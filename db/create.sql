1dade99677f686336c653ab71a2287f81ce1b99a
giancarlo_buenaflor@yahoo.com <giancarlo_buenaflor@yahoo.com>
2023-01-14 17:08:58 +0100

    use german translation for first version

--- db/create.sql
+++ db/create.sql
@@ -1,23 +1,23 @@
 CREATE TYPE tag AS ENUM (
-  'Leaf crops',  -- German: Blattgemüse
-  'Fruit crops', -- German: Fruchtpflanzen
-  'Root crops', -- German: Wurzelpflanzen
-  'Flowering crops', -- German: Blütenpflanzen
-  'Herbs', -- German: Kräuter
-  'Other' -- German: Sonstiges
+  'Blattgemüse',  -- English: Leaf crops
+  'Fruchtpflanzen', -- English: Fruit crops
+  'Wurzelpflanzen', -- English: Root crops
+  'Blütenpflanzen', -- English: Flowering crops
+  'Kräuter', -- English: Herbs
+  'Sonstiges' -- English: Other
 );
 
 CREATE TYPE quality AS ENUM (
-  'Organic', -- German: Bio
-  'Not organic', -- German: Nicht-Bio
-  'Unknown' -- German: unbekannt
+  'Bio', -- English: Organic
+  'Nicht-Bio', -- English: Not organic
+  'Unbekannt' -- English: Unknown
 );
 
 CREATE TYPE quantity AS ENUM (
-  'Nothing', -- German: Nichts
-  'Not enough', -- German: Nicht Genug
-  'Enough' -- German: Genug
-  'More than enough' -- Mehr als genug
+  'Nichts', -- English: Nothing
+  'Nicht Genug', -- English: Not enough
+  'Genug' -- English: Enough
+  'Mehr als genug' -- More than enough
 );
 
 CREATE TABLE variety_synonyms (
@@ -26,7 +26,7 @@ CREATE TABLE variety_synonyms (
   variety_id INTEGER REFERENCES varieties(id) NOT NULL
 )
 
-CREATE TABLE varieties ( -- German: Sorten
+CREATE TABLE varieties ( -- English: Sorten
   id SERIAL PRIMARY KEY,
   tags tag[] NOT NULL, -- if not given (via inserting an empty array: ARRAY[]::tag[]), plants take preference.
   species VARCHAR(255) NOT NULL,
@@ -50,7 +50,7 @@ CREATE TABLE varieties ( -- German: Sorten
 CREATE TABLE seeds (
   id SERIAL PRIMARY KEY,
   tags tag[] NOT NULL,
-  species VARCHAR(255) NOT NULL, -- German: Art
+  species VARCHAR(255) NOT NULL, -- English: Art
   variety_id INTEGER REFERENCES varieties(id) NOT NULL,
   harvest_year SMALLINT NOT NULL,
   use_by DATE,
