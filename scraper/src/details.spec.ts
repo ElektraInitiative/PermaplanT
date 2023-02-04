b2a026f4a7a0e8dba0a163f7947691c4cbe4edf3
Nursultan <nursultan_imanov@mail.ru>
2023-02-04 15:22:49 +0100

    fetch details of every plant from the list

--- scraper/src/details.spec.ts
+++ scraper/src/details.spec.ts
@@ -1,35 +1,54 @@
 import { test, expect } from '@playwright/test';
+const csv = require('csvtojson');
 
 import { chromium } from '@playwright/test';
 import { parse } from 'json2csv';
 import { writeFileSync } from 'fs';
 
+const BASE_URL = 'https://practicalplants.org';
+
+let jsonArray: any[] = [];
+
 test.describe('Fetch plant list', () => {
+    test.beforeEach(async ({}) => {
+        jsonArray = await csv().fromFile('data/plants.csv');
+    });
+
     test('Fetch list', async ({}) => {
         const browser = await chromium.launch({
             headless: true,
         });
-        const data: Object = {};
         const context = await browser.newContext();
-        const page = await context.newPage();
-        await page.goto('https://practicalplants.org/wiki/solanum_lycopersicum/');
-        const plantDatatable = await page.$('div#plant-datatable');
-        const sections = await plantDatatable.$$('div.infobox-section');
-        for (const section of sections) {
-            // const title = await section.$('.infobox-title');
-            const content = await section.$('div.infobox-content');
-            const subSections = await content.$$('div.infobox-subsection');
-            const contentText = await Promise.all(
-                subSections.map(async (subSection) => {
-                    const subSectionTitle = await subSection.$('.infobox-title');
-                    const subSectionTitleText = await subSectionTitle.innerText();
-                    const subSectionContent = await subSection.$('.infobox-content');
-                    const subSectionContentText = await subSectionContent.innerText();
-                    data[subSectionTitleText] = subSectionContentText;
-                }),
-            );
-        }
-        const csv = parse(data);
+        let detailsArray = await Promise.all(
+            jsonArray.map(async (plant) => {
+                const page = await context.newPage();
+                const details: Object = {};
+                await page.goto(`${BASE_URL}${plant['url']}`);
+
+                const plantDatatable = await page.$('div#plant-datatable');
+                if (!plantDatatable) {
+                    return;
+                }
+                const sections = await plantDatatable.$$('div.infobox-section');
+                for (const section of sections) {
+                    // const title = await section.$('.infobox-title');
+                    const content = await section.$('div.infobox-content');
+                    const subSections = await content.$$('div.infobox-subsection');
+                    const contentText = await Promise.all(
+                        subSections.map(async (subSection) => {
+                            const subSectionTitle = await subSection.$('.infobox-title');
+                            const subSectionTitleText = await subSectionTitle.innerText();
+                            const subSectionContent = await subSection.$('.infobox-content');
+                            const subSectionContentText = await subSectionContent.innerText();
+                            details[subSectionTitleText] = subSectionContentText;
+                        }),
+                    );
+                }
+                return details;
+            }),
+        );
+        detailsArray = detailsArray.filter((item) => item !== undefined);
+        const csv = parse(detailsArray);
         writeFileSync('data/detail.csv', csv);
     });
 });
