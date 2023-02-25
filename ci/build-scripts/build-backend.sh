60a284db871fa2b74f44cde6469ffa8c1dcc974f
Lukas Hartl <git@lukashartl.at>
2023-02-25 21:47:44 +0100

    ci: move db reset and env to Jenkinsfile

--- ci/build-scripts/build-backend.sh
+++ ci/build-scripts/build-backend.sh
@@ -5,13 +5,8 @@ cd ./backend/
 
 source /opt/rust.env
 
-export DATABASE_URL=postgres://cidb:cidb@127.0.0.1/cidb
-
 # Echo on and fail fast
 set -ex
-
-# Reset Database
-sudo /usr/local/bin/permaplant-reset-ci.sh
 
 # Download Cargo Modules
 cargo fetch
