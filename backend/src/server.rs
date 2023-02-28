12dd28ff28e4c60603ac6c11ea776e8d0e0f5a0f
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-02-28 12:35:30 +0100

    move cors config to function, remove clone from pool

--- backend/src/server.rs
+++ backend/src/server.rs
@@ -10,24 +10,26 @@ use dotenvy::dotenv;
 pub async fn start(app_config: Config) -> std::io::Result<()> {
     dotenv().ok();
 
-    let pool = db::config(&app_config.database_url);
-    let pool = Data::new(pool.clone());
-
     HttpServer::new(move || {
+        let pool = db::config(&app_config.database_url);
+        let data = Data::new(pool);
+
         App::new()
-            .wrap(
-                Cors::default() // allowed_origin return access-control-allow-origin: * by default
-                    .allowed_origin("http://127.0.0.1:5173")
-                    .allowed_origin("http://localhost:5173")
-                    .send_wildcard()
-                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
-                    .allowed_header(http::header::CONTENT_TYPE)
-                    .max_age(3600),
-            )
-            .app_data(Data::clone(&pool))
+            .wrap(cors_configuration())
+            .app_data(data)
             .configure(routes::config)
     })
     .bind(app_config.bind_address)?
     .run()
     .await
+}
+
+fn cors_configuration() -> Cors {
+    Cors::default() // allowed_origin return access-control-allow-origin: * by default
+        .allowed_origin("http://127.0.0.1:5173")
+        .allowed_origin("http://localhost:5173")
+        .send_wildcard()
+        .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+        .allowed_header(http::header::CONTENT_TYPE)
+        .max_age(3600)
 }
