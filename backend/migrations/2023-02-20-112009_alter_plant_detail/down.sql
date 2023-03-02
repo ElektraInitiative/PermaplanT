77591d931c6fb3c62ca3a10e7812e5d3439524f5
Nursultan <nursultan_imanov@mail.ru>
2023-03-02 18:06:46 +0100

    rename varieties in database

--- backend/migrations/2023-02-20-112009_alter_plant_detail/down.sql
+++ backend/migrations/2023-02-20-112009_alter_plant_detail/down.sql
@@ -1,2 +1,10 @@
 -- This file should undo anything in `up.sql`
 ALTER TABLE plant_detail DROP COLUMN article_last_modified_at;
+ALTER TABLE seeds
+  RENAME COLUMN plant_id TO variety_id;
+ALTER TABLE seeds
+  RENAME CONSTRAINT seeds_plant_id_fkey TO seeds_variety_id_fkey;
+ALTER TABLE plants
+  RENAME CONSTRAINT plants_pkey TO varieties_pkey;
+ALTER TABLE plants
+  RENAME COLUMN plant TO variety;
--- backend/migrations/2023-02-20-112009_alter_plant_detail/up.sql
+++ backend/migrations/2023-02-20-112009_alter_plant_detail/up.sql
@@ -1,3 +1,11 @@
 -- Your SQL goes here
 ALTER TABLE plant_detail
 ADD COLUMN article_last_modified_at TIMESTAMP;
+ALTER TABLE seeds
+  RENAME COLUMN variety_id TO plant_id;
+ALTER TABLE seeds
+  RENAME CONSTRAINT seeds_variety_id_fkey TO seeds_plant_id_fkey;
+ALTER TABLE plants
+  RENAME CONSTRAINT varieties_pkey TO plants_pkey;
+ALTER TABLE plants
+  RENAME COLUMN variety TO plant;
