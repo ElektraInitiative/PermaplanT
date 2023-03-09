438b8cb016b44c4ba6dd0b9d7f9c47e12b22db26
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-03-09 17:36:39 +0100

    Fix build issues related to new migration scripts from a different pull request.

--- backend/migrations/2023-03-03-140707_convert_variety_to_text/down.sql
+++ backend/migrations/2023-03-03-140707_convert_variety_to_text/down.sql
@@ -1,17 +1,1 @@
--- This file should undo anything in `up.sql`
 ALTER TABLE seeds DROP COLUMN variety;
-
-CREATE TABLE varieties ( 
-  id SERIAL PRIMARY KEY,
-  tags TEXT[] NOT NULL, -- if not given (via inserting an empty array: ARRAY[]::tag[]), plants take preference.
-  species TEXT NOT NULL, -- German: Art
-  variety TEXT -- German: Sorte
-);
-
--- Add column without not null constraint.
-ALTER TABLE seeds ADD COLUMN variety_id INTEGER REFERENCES varieties(id);
--- Then set all varieties to some default value .... 
-INSERT INTO varieties (id, tags, species) VALUES (1, ARRAY['Default']::TEXT[], 'Default');
-UPDATE seeds SET variety_id = 1;
--- ... before setting not null constraint.
-ALTER TABLE seeds ALTER COLUMN variety_id SET NOT NULL;
--- backend/migrations/2023-03-03-140707_convert_variety_to_text/up.sql
+++ backend/migrations/2023-03-03-140707_convert_variety_to_text/up.sql
@@ -1,5 +1,2 @@
 -- Your SQL goes here
-ALTER TABLE seeds DROP COLUMN variety_id;
-DROP TABLE varieties;
-
 ALTER TABLE seeds ADD COLUMN variety TEXT;
