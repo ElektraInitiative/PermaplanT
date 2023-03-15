dcc35e2d8b37418899d08159ebbeb363302f9734
Nursultan <nursultan_imanov@mail.ru>
2023-03-15 14:05:03 +0100

    remove an example table

--- backend/migrations/2023-03-14-153713_enable_postgis/up.sql
+++ backend/migrations/2023-03-14-153713_enable_postgis/up.sql
@@ -1,7 +1,2 @@
 -- Your SQL goes here
 CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE TABLE geometry_samples (
-  id SERIAL PRIMARY KEY,
-  point geometry(Point, 4326) NOT NULL,
-  linestring geometry(Linestring, 4326) NOT NULL
-);
