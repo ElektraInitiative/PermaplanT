cc6c6924bcb1c3ae98adf23b34b481c449b0edc8
Samuel <absurd.turtl3@gmail.com>
2023-03-26 18:14:35 +0200

    added dark mode switcher

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,14 @@ import { Fragment } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  document.documentElement.classList.add('dark');
+  if (localStorage.getItem('darkMode') === 'true') {
+    localStorage.setItem('darkMode', 'false');
+    document.documentElement.classList.remove('dark');
+  } else {
+    localStorage.setItem('darkMode', 'true');
+    document.documentElement.classList.add('dark');
+  }
+
   return (
     <div>
       <Fragment>
