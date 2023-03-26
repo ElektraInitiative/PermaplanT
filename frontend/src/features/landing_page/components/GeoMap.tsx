f7f594a71738b60be06356d025d72fa116a71a31
Samuel <absurd.turtl3@gmail.com>
2023-03-26 18:13:32 +0200

    changed marker location to Unterfrauenhaid

--- frontend/src/features/landing_page/components/GeoMap.tsx
+++ frontend/src/features/landing_page/components/GeoMap.tsx
@@ -2,12 +2,12 @@ import 'leaflet/dist/leaflet.css';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
 export const GeoMap = () => (
-  <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <MapContainer center={[47.57, 16.496]} zoom={8} scrollWheelZoom={false}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[51.505, -0.09]}>
+    <Marker position={[47.57, 16.496]}>
       <Popup>
         This is a PermaplanT site. Click <a href="/sites/1">here</a> to visit.
       </Popup>
