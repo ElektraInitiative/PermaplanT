5240e963de15161e116c25f27f3cf519d9c50d27
Samuel <absurd.turtl3@gmail.com>
2023-03-26 18:13:58 +0200

    added gallery and map to navbar

--- frontend/src/features/landing_page/components/Navbar.tsx
+++ frontend/src/features/landing_page/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import SimpleButton from '@/components/Button/SimpleButton';
+import SimpleButton, { ButtonVariant } from '@/components/Button/SimpleButton';
+import { DarkModeSwitcher } from './DarkModeSwitcher';
 
 export const Navbar = () => (
   <header className="flex items-center gap-10 bg-background-200-dark p-4">
@@ -6,8 +7,15 @@ export const Navbar = () => (
     <h2 className="text-white">PermaPlanT</h2>
     <div className="flex flex-grow justify-end gap-10">
       <a href="seeds">
-        <SimpleButton>seeds</SimpleButton>
+        <SimpleButton variant={ButtonVariant.neutral}>seeds</SimpleButton>
       </a>
+      <a className="border-b border-background-700" href="/#gallery">
+        <SimpleButton variant={ButtonVariant.neutral}>gallery</SimpleButton>
+      </a>
+      <a className="border-b border-background-700" href="/#map">
+        <SimpleButton variant={ButtonVariant.neutral}>map</SimpleButton>
+      </a>
+      <DarkModeSwitcher/>
     </div>
     <a href="https://github.com/ElektraInitiative/PermaplanT" className="w-8">
       <img src="/github-mark/github-mark-white.svg"></img>
