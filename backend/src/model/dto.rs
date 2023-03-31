bcc779d4d6f4aff7832716a00996e2865970ae71
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-03-31 15:52:11 +0200

    fix: clippy error

--- backend/src/model/dto.rs
+++ backend/src/model/dto.rs
@@ -53,7 +53,7 @@ pub struct NewSeedDto {
 
 #[allow(clippy::missing_docs_in_private_items)] // TODO: See #97.
 #[typeshare]
-#[derive(Debug, Serialize, PartialEq, Deserialize, ToSchema)]
+#[derive(Debug, Serialize, PartialEq, Eq, Deserialize, ToSchema)]
 pub struct PlantsSearchDto {
     pub id: i32,
     pub binomial_name: String,
