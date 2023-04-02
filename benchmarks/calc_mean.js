c463c4918449b5afdb7db95e3f972f49bb585802
Nursultan <nursultan_imanov@mail.ru>
2023-04-02 23:05:40 +0200

    fix mean calculations

--- benchmarks/calc_mean.js
+++ benchmarks/calc_mean.js
@@ -4,14 +4,7 @@ const csv = require('csv-parser');
 
 const reportFolderPath = './report/';
 
-const output = [
-    { testcase: '100 circles', firstContentfulPaint: null, interactive: null },
-    { testcase: '100 stars', firstContentfulPaint: null, interactive: null },
-    { testcase: '100 rectangles', firstContentfulPaint: null, interactive: null },
-    { testcase: '100 polygons', firstContentfulPaint: null, interactive: null },
-];
-
-const calculateMean = async (fileName, files) => {
+const calculateMean = async (fileName, files, output) => {
     // Filter out non-CSV files and only keep files that end with "-lighthouse-results-shapes.csv"
     const csvFiles = files.filter((file) => file.endsWith('-' + fileName));
     let numFilesProcessed = 0;
@@ -24,28 +17,42 @@ const calculateMean = async (fileName, files) => {
         fileStream
             .pipe(csv())
             .on('data', (row) => {
-                const foundTestcase = output.find((testcase) => testcase.testcase === row.testname);
-                if (foundTestcase) {
-                    foundTestcase.firstContentfulPaint += Number(row.firstContentfulPaint);
-                    foundTestcase.interactive += Number(row.interactive);
-                }
+                output.push({
+                    testname: row.testname,
+                    testrun: numFilesProcessed,
+                    firstContentfulPaint: parseFloat(row.firstContentfulPaint),
+                    interactive: parseFloat(row.interactive),
+                });
             })
             .on('end', () => {
                 numFilesProcessed++;
                 if (numFilesProcessed === csvFiles.length) {
-                    // Divide the sum of each value by the number of CSV files to get the mean value
                     const numFiles = csvFiles.length;
-                    output.forEach((row) => {
-                        row.firstContentfulPaint = (row.firstContentfulPaint /= numFiles).toFixed(
-                            2,
-                        );
-                        row.interactive = (row.interactive /= numFiles).toFixed(2);
-                    });
+                    const groupedOutput = output.reduce((acc, row) => {
+                        if (!acc[row.testname]) {
+                            acc[row.testname] = {
+                                testname: row.testname,
+                                firstContentfulPaint: row.firstContentfulPaint,
+                                interactive: row.interactive,
+                            };
+                        } else {
+                            acc[row.testname].firstContentfulPaint += row.firstContentfulPaint;
+                            acc[row.testname].interactive += row.interactive;
+                        }
+                        return acc;
+                    }, {});
+
+                    // Calculate the mean
+                    const averageOutput = Object.values(groupedOutput).map((row) => ({
+                        testname: row.testname,
+                        firstContentfulPaint: row.firstContentfulPaint / numFiles,
+                        interactive: row.interactive / numFiles,
+                    }));
 
                     // Write the output to a new CSV file
                     const outputPath = path.join(reportFolderPath, fileName);
-                    const outputCsv = ['testname,firstContentfulPaint,interactive']
-                        .concat(output.map((row) => Object.values(row).join(',')))
+                    const outputCsv = ['testname,testrun,firstContentfulPaint,interactive']
+                        .concat(averageOutput.map((row) => Object.values(row).join(',')))
                         .join('\n');
                     fs.writeFileSync(outputPath, outputCsv);
                 }
@@ -55,8 +62,9 @@ const calculateMean = async (fileName, files) => {
 
 fs.readdir(reportFolderPath, async (err, files) => {
     if (err) throw err;
+    const output = [];
+    const outputShapes = [];
+    await calculateMean('lighthouse-results-shapes.csv', files, outputShapes);
 
-    await calculateMean('lighthouse-results-shapes.csv', files);
-
-    await calculateMean('lighthouse-results.csv', files);
+    await calculateMean('lighthouse-results.csv', files, output);
 });
