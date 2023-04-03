9c0225fed486cb0ea290f3bf7216c36052ce6136
Thorben Julian Staufer <e11809935@student.tuwien.ac.at>
2023-04-03 16:56:22 +0200

    Added accent color definitions
    Frontend now uses the two color themes discussed in the focus group.

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -136,6 +136,65 @@ const yellow = {
   '100': '#fffbff',
 }
 
+/*
+* Example color roles for light/dark mode:
+*
+* 500/100: main color (e.g. buttons)
+* 50/800: on main color (e.g. text on buttons)
+* 100/500: supporting main color
+* 800/50: on supporting main color
+*/
+const accentColors = {
+  asparagus: {
+    '50': '#f2f6ef',
+    '100': '#d8e3cf',
+    '200': '#bfd0af',
+    '300': '#a5bd8e',
+    '400': '#8baa6e',
+    '500': '#719155',
+    '600': '#587142',
+    '700': '#3f502f',
+    '800': '#26301c',
+    '900': '#0d1009'
+  },
+  rackley: {
+    '50': '#eff2f6',
+    '100': '#cfd8e3',
+    '200': '#afbfd0',
+    '300': '#8ea5bd',
+    '400': '#6e8baa',
+    '500': '#557191',
+    '600': '#425871',
+    '700': '#2f3f50',
+    '800': '#1c2630',
+    '900': '#090d10'
+  },
+  emerald: {
+    '50': '#ecfdf5',
+    '100': '#d1fae5',
+    '200': '#a7f3d0',
+    '300': '#6ee7b7',
+    '400': '#34d399',
+    '500': '#10b981',
+    '600': '#059669',
+    '700': '#047857',
+    '800': '#065f46',
+    '900': '#064e3b'
+  },
+  bondi: {
+    '50': '#e5fcff',
+    '100': '#b3f5ff',
+    '200': '#80eeff',
+    '300': '#4de7ff',
+    '400': '#1ae0ff',
+    '500': '#00c7e6',
+    '600': '#009bb3',
+    '700': '#006e80',
+    '800': '#00424d',
+    '900': '#00161a'
+  }
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
@@ -145,24 +204,13 @@ module.exports = {
       border: ['focus'],
       colors: {
         // variant 1
-        // primary: colors.emerald,
-        // secondary: blue,
-        // neutral: brown,
-
-        // variant 2
-        // primary: colors.emerald,
-        // secondary: blue,
+        // primary: accentColors.emerald,
+        // secondary: accentColors.bondi,
         // neutral: gray,
 
-        // variant 3
-        // primary: green,
-        // secondary: yellow,
-        // neutral: brown,
-
-        // variant 4
-        background: gray,
-        primary: green,
-        secondary: yellow,
+        // variant 2
+        primary: accentColors.asparagus,
+        secondary: accentColors.rackley,
         neutral: gray,
       },
     },
