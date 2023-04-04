fb1439ba36b56fbee78052e18aafa924b563a859
Samuel <absurd.turtl3@gmail.com>
2023-04-04 12:43:36 +0200

    added mailto to contact form, removed name + email

--- frontend/src/features/landing_page/components/ContactUs.tsx
+++ frontend/src/features/landing_page/components/ContactUs.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 const ContactUs = () => {
+  const [message, setMessage] = useState("")
+  const email = "office@permaplant.net"
+
   return (
     <section className="body-font relative w-full">
       <div className="container mx-auto py-24">
@@ -10,32 +15,6 @@ const ContactUs = () => {
         </div>
         <div className="mx-auto w-full">
           <div className="-m-2 flex flex-wrap">
-            <div className="w-1/2 p-2">
-              <div className="relative">
-                <label htmlFor="name" className="text-sm leading-7 text-gray-600">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full rounded border border-gray-300 bg-gray-100 bg-opacity-50 py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                />
-              </div>
-            </div>
-            <div className="w-1/2 p-2">
-              <div className="relative">
-                <label htmlFor="email" className="text-sm leading-7 text-gray-600">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full rounded border border-gray-300 bg-gray-100 bg-opacity-50 py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
-                />
-              </div>
-            </div>
             <div className="w-full p-2">
               <div className="relative">
                 <label htmlFor="message" className="text-sm leading-7 text-gray-600">
@@ -45,13 +24,17 @@ const ContactUs = () => {
                   id="message"
                   name="message"
                   className="h-32 w-full resize-none rounded border border-gray-300 bg-gray-100 bg-opacity-50 py-1 px-3 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200"
+                  value={message}
+                  onChange={(event) => setMessage(event.target.value)}
                 ></textarea>
               </div>
             </div>
             <div className="w-full p-2">
-              <button className="mx-auto flex rounded border-0 bg-primary-500 py-2 px-8 text-primary-50 hover:bg-primary-600 focus:outline-none dark:bg-primary-300 dark:text-primary-700 dark:hover:bg-primary-200">
-                Button
-              </button>
+              <a href={`mailto:${email}?subject=first%20contact&body=${encodeURIComponent(message)}`}>
+                <button className="mx-auto flex rounded border-0 bg-primary-500 py-2 px-8 text-primary-50 hover:bg-primary-600 focus:outline-none dark:bg-primary-300 dark:text-primary-700 dark:hover:bg-primary-200">
+                  Compose email
+                </button>
+              </a>
             </div>
           </div>
         </div>
