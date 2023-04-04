a62cb2f3cfe63e4441ecf0754fe0aef15c9c5b05
Nursultan <nursultan_imanov@mail.ru>
2023-04-04 22:18:45 +0200

    extend the reinsaat scraper

--- scraper/src/fetch_reinsaat.js
+++ scraper/src/fetch_reinsaat.js
@@ -2,37 +2,88 @@ import { chromium } from 'playwright';
 import fs from 'fs';
 import { parse as json2csv } from 'json2csv';
 
-const fetchAllPlants = async () => {
-  const browser = await chromium.launch();
+const results = [];
+
+const fetchPlant = async (browser, { name, url }) => {
   const page = await browser.newPage();
-  await page.goto('https://www.reinsaat.at/shop/DE/gurken/rs-gu-01.22_nishiki/');
-  const results = [];
-  const plant = {};
+  await page.goto(url);
+  const plant = {
+    name,
+    url,
+  };
 
   const rows = await page.$$('.rs-growing-time table tr');
-  for (const row of rows) {
-    const typeLabelTd = await row.$('td.type-lable');
-    if (!typeLabelTd) {
-      continue;
-    }
-    const typeLabel = await typeLabelTd.innerText();
-    plant[typeLabel] = [];
+  const plantInfo = await page.$('.fce_shop_inhalt_right_artikelnummer');
 
-    const tds = await row.$$('td');
-    for (let i = 0; i < tds.length; i++) {
-      const td = tds[i];
-      const backgroundColor = await td.getAttribute('style');
-      if (backgroundColor && backgroundColor !== 'background-color: none;') {
-        plant[typeLabel].push(i);
+  if (rows.length === 0 && !plantInfo) {
+    return;
+  }
+
+  if (rows.length > 0) {
+    for (const row of rows) {
+      const typeLabelTd = await row.$('td.type-lable');
+      if (!typeLabelTd) {
+        continue;
+      }
+      const typeLabel = await typeLabelTd.innerText();
+      plant[typeLabel] = [];
+
+      const tds = await row.$$('td');
+      for (let i = 0; i < tds.length; i++) {
+        const td = tds[i];
+        const backgroundColor = await td.getAttribute('style');
+        if (backgroundColor && backgroundColor !== 'background-color: none;') {
+          plant[typeLabel].push(i);
+        }
       }
     }
   }
 
+  if (plantInfo) {
+    const innerText = await plantInfo.innerText();
+
+    const regex = /(\w+[^:]*):\s*([^\n]*)/g;
+    let matches;
+    while ((matches = regex.exec(innerText)) !== null) {
+      plant[matches[1].trim()] = matches[2].trim();
+    }
+  }
+
   results.push(plant);
+};
+
+const fetchSublinks = async (browser, url) => {
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const sublinks = await page.$$eval('.s_subnavi .s_subnavi_active .s_sub_subnavi a', (links) =>
+    links.map((link) => {
+      return {
+        name: link.innerText,
+        url: link.href,
+      };
+    }),
+  );
+
+  console.log(sublinks);
+
+  await Promise.all(sublinks.map((sublink) => fetchPlant(browser, sublink)));
+};
+
+const fetchAllPlants = async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.reinsaat.at/shop/EN/');
+
+  const superlinks = await (
+    await page.$$eval('.s_subnavi a', (links) => links.map((link) => link.href))
+  ).slice(0, 10);
+
+  await Promise.all(superlinks.map((superlink) => fetchSublinks(browser, superlink)));
 
   await browser.close();
 
-  return results;
+  writePlantsToCsv(results);
 };
 
 function writePlantsToCsv(plants) {
@@ -41,7 +92,9 @@ function writePlantsToCsv(plants) {
   }
   const sanitizedPlants = plants.map((plant) => {
     for (const [key, value] of Object.entries(plant)) {
-      plant[key] = value.map((v) => v.toString()).join(',');
+      if (typeof key === 'object' && !Array.isArray(key) && key !== null) {
+        plant[key] = value.map((v) => v.toString()).join(',');
+      }
     }
     return plant;
   });
@@ -50,6 +103,4 @@ function writePlantsToCsv(plants) {
   fs.writeFileSync('data/reinsaatRawData.csv', csv);
 }
 
-fetchAllPlants()
-  .then((plants) => writePlantsToCsv(plants))
-  .catch((error) => console.error(error));
+fetchAllPlants();
