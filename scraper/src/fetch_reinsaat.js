f732aa4169d0f94c10c4d53b4df4e5071d9f7ca1
Nursultan <nursultan_imanov@mail.ru>
2023-04-05 13:30:08 +0200

    scrape sublinks

--- scraper/src/fetch_reinsaat.js
+++ scraper/src/fetch_reinsaat.js
@@ -3,90 +3,167 @@ import fs from 'fs';
 import { parse as json2csv } from 'json2csv';
 
 const results = [];
+const errors = [];
 
-const fetchPlant = async (browser, { name, url }) => {
-  const page = await browser.newPage();
-  await page.goto(url);
-  const plant = {
-    name,
-    url,
-  };
+const fetchPlant = async (context, { name, subcategory = null, category, url }) => {
+  const page = await context.newPage();
 
-  const rows = await page.$$('.rs-growing-time table tr');
-  const plantInfo = await page.$('.fce_shop_inhalt_right_artikelnummer');
+  try {
+    await page.goto(url);
+    console.log('fetching', name, category, subcategory, url);
 
-  if (rows.length === 0 && !plantInfo) {
-    return;
-  }
+    const plant = {
+      name,
+      category,
+      subcategory,
+      url,
+    };
 
-  if (rows.length > 0) {
-    for (const row of rows) {
-      const typeLabelTd = await row.$('td.type-lable');
-      if (!typeLabelTd) {
-        continue;
-      }
-      const typeLabel = await typeLabelTd.innerText();
-      plant[typeLabel] = [];
+    const rows = await page.$$('.rs-growing-time table tr');
+    const plantInfo = await page.$('.fce_shop_inhalt_right_artikelnummer');
+    const growingInfo = await page.$('.growingInfos');
 
-      const tds = await row.$$('td');
-      for (let i = 0; i < tds.length; i++) {
-        const td = tds[i];
-        const backgroundColor = await td.getAttribute('style');
-        if (backgroundColor && backgroundColor !== 'background-color: none;') {
-          plant[typeLabel].push(i);
+    if (rows.length === 0) {
+      return;
+    }
+
+    if (rows.length > 0) {
+      for (const row of rows) {
+        const typeLabelTd = await row.$('td.type-lable');
+        if (!typeLabelTd) {
+          continue;
+        }
+        const typeLabel = await typeLabelTd.innerText();
+        plant[typeLabel] = [];
+
+        const tds = await row.$$('td');
+        for (let i = 0; i < tds.length; i++) {
+          const td = tds[i];
+          const backgroundColor = await td.getAttribute('style');
+          if (backgroundColor && backgroundColor !== 'background-color: none;') {
+            plant[typeLabel].push(i);
+          }
         }
       }
     }
-  }
 
-  if (plantInfo) {
-    const innerText = await plantInfo.innerText();
+    if (plantInfo) {
+      const innerText = await plantInfo.innerText();
 
-    const regex = /(\w+[^:]*):\s*([^\n]*)/g;
-    let matches;
-    while ((matches = regex.exec(innerText)) !== null) {
-      plant[matches[1].trim()] = matches[2].trim();
+      const regex = /(\w+[^:]*):\s*([^\n]*)/g;
+      let matches;
+      while ((matches = regex.exec(innerText)) !== null) {
+        plant[matches[1].trim()] = matches[2].trim();
+      }
     }
-  }
 
-  results.push(plant);
+    if (growingInfo) {
+      const paragraphs = await growingInfo.$$('p');
+      for (const paragraph of paragraphs) {
+        const strong = await paragraph.$('strong');
+        if (strong) {
+          let key = await strong.innerText();
+          key = key.replace(':', '');
+          const text = await paragraph.innerText();
+          const value = text.replace(key, '').trim();
+          plant[key] = value;
+        }
+      }
+    }
+    results.push(plant);
+  } catch (error) {
+    console.error('[ERROR] Error fetching plant', name, category, subcategory, url, error);
+    errors.push({ name, category, subcategory, url, error });
+  }
 };
 
-const fetchSublinks = async (browser, url) => {
-  const page = await browser.newPage();
-  await page.goto(url);
+const fetchSubSublinks = async (context, { name, category, url }) => {
+  const pageSubSublinks = await context.newPage();
+  await pageSubSublinks.goto(url);
 
-  const sublinks = await page.$$eval('.s_subnavi .s_subnavi_active .s_sub_subnavi a', (links) =>
-    links.map((link) => {
-      return {
-        name: link.innerText,
-        url: link.href,
-      };
-    }),
+  const subsublinks = await pageSubSublinks.$$eval(
+    '.s_subnavi .s_subnavi_active .s_sub_subnavi .s_sub_sub_subnavi a',
+    (links) =>
+      links.map((link) => {
+        console.log('link', link.innerText, link.href);
+        return {
+          name: link.innerText,
+          url: link.href,
+        };
+      }),
   );
 
-  console.log(sublinks);
+  await SubSublinks.close();
 
-  await Promise.all(sublinks.map((sublink) => fetchPlant(browser, sublink)));
+  if (subsublinks.length > 0) {
+    await Promise.all(
+      subsublinks.map((subsublinks) =>
+        fetchPlant(context, { ...subsublinks, subcategory: name, category }),
+      ),
+    );
+  } else {
+    await fetchPlant(context, { name, category, url });
+  }
+};
+
+const fetchSublinks = async (browser, { category, url }) => {
+  const context = await browser.newContext();
+  const pageSublinks = await context.newPage();
+
+  await pageSublinks.goto(url);
+
+  const sublinks = await pageSublinks.$$eval(
+    '.s_subnavi .s_subnavi_active .s_sub_subnavi a',
+    (links) =>
+      links.map((link) => {
+        return {
+          name: link.innerText,
+          url: link.href,
+        };
+      }),
+  );
+
+  await pageSublinks.close();
+  await Promise.all(sublinks.map((sublink) => fetchSubSublinks(context, { ...sublink, category })));
+  await context.close();
 };
 
 const fetchAllPlants = async () => {
+  console.log('[INFO] Fetching plants');
   const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.reinsaat.at/shop/EN/');
+  try {
+    const page = await browser.newPage();
+    await page.goto('https://www.reinsaat.at/shop/EN/');
 
-  const superlinks = await (
-    await page.$$eval('.s_subnavi a', (links) => links.map((link) => link.href))
-  ).slice(0, 10);
+    const superlinks = await await page.$$eval('.s_subnavi a', (links) =>
+      links.map((link) => {
+        return {
+          category: link.innerText,
+          url: link.href,
+        };
+      }),
+    );
 
-  await Promise.all(superlinks.map((superlink) => fetchSublinks(browser, superlink)));
-
-  await browser.close();
-
-  writePlantsToCsv(results);
+    await Promise.all(superlinks.map((superlink) => fetchSublinks(browser, superlink)));
+    await browser.close();
+    console.log('[INFO] Done fetching plants');
+    console.log('[INFO] Writing to CSV. Length:', results.length);
+    writeToCsv(results, 'data/reinsaatRawData.csv');
+    console.log('[INFO] Done writing to CSV');
+  } catch (error) {
+    console.error(error);
+    await browser.close();
+    console.log('[INFO] browser closed');
+  } finally {
+    if (errors.length > 0) {
+      console.log('[INFO] Writing errors to CSV. Length:', errors.length);
+      writeToCsv(errors, 'data/reinsaatErrors.csv');
+      console.log('[INFO] Done writing errors to CSV');
+    }
+  }
 };
 
-function writePlantsToCsv(plants) {
+function writeToCsv(plants, path) {
   if (!fs.existsSync('data')) {
     fs.mkdirSync('data');
   }
@@ -100,7 +177,20 @@ function writePlantsToCsv(plants) {
   });
 
   const csv = json2csv(sanitizedPlants);
-  fs.writeFileSync('data/reinsaatRawData.csv', csv);
+  fs.writeFileSync(path, csv);
 }
 
 fetchAllPlants();
+
+// const test = async () => {
+//   const browser = await chromium.launch();
+//   await fetchSublinks(browser, {
+//     category: 'Cucumbers',
+//     // url: 'https://www.reinsaat.at/shop/EN/wild_flowers_seeds/',
+//     url: 'https://www.reinsaat.at/shop/EN/cucumbers/',
+//   });
+//   console.log('results', results);
+//   await browser.close();
+// };
+
+// test();
