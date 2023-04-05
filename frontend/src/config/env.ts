5896e3ccb6447ea77851cd8beab677f665d0a12c
Paul Bushuo Buschmann <me@paulbuschmann.com>
2023-04-05 07:30:23 +0200

    fix: weird order of exports

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,7 +1,7 @@
-export const baseApiUrl = import.meta.env.VITE_BASE_API_URL;
-export const isDev = import.meta.env.DEV;
-export const isProd = import.meta.env.PROD;
-
 if (typeof import.meta.env.VITE_BASE_API_URL !== 'string') {
   throw new Error('VITE_BASE_API_URL not set');
 }
+
+export const baseApiUrl = import.meta.env.VITE_BASE_API_URL;
+export const isDev = import.meta.env.DEV;
+export const isProd = import.meta.env.PROD;
