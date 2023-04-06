d725f568312fc6a375686bf7058a4fb8abd4716b
Benjamin Zinschitz <benjamin.zinschitz@student.tuwien.ac.at>
2023-04-06 14:36:37 +0200

    database: add varities to override specific plant attributes

--- doc/decisions/example_migrations/2023-04-04-220921_plant_relationships/up.sql
+++ doc/decisions/example_migrations/2023-04-04-220921_plant_relationships/up.sql
@@ -22,6 +22,18 @@ CREATE TABLE taxon_relationships (
     right_taxon_id INTEGER REFERENCES taxons (id) NOT NULL
 );
 
+--
+-- This illustrates how column overrides could be done for plant varieties.
+-- The example only shows how it would work for two columns.
+-- It could easily be extended to other columns.
+--
+CREATE TABLE varieties (
+    id SERIAL PRIMARY KEY NOT NULL,
+    plant_id INTEGER REFERENCES plants (id),
+    mature_size_height VARCHAR,
+    flower_colour VARCHAR
+);
+
 -- Example
 --
 -- For this example we use the following notation
@@ -258,3 +270,53 @@ LEFT JOIN
 LEFT JOIN
     taxons AS family
     ON (family.rank = 'family' AND family.id = plants.family_id);
+
+-- Let's say carrots have a height of 0.3m and a white flower colour.
+UPDATE plants
+SET
+    mature_size_height = '0.3',
+    flower_colour = 'white'
+WHERE binomial_name = 'Daucus carota';
+
+-- There is a variety of it which grows higher but has the same flower_colour.
+INSERT INTO varieties (plant_id, mature_size_height)
+VALUES ((SELECT id FROM plants WHERE binomial_name = 'Daucus carota'), '0.5');
+
+-- And another variety which has the same height but yellow flowers.
+INSERT INTO varieties (plant_id, flower_colour)
+VALUES ((SELECT id FROM plants WHERE binomial_name = 'Daucus carota'), 'yellow');
+
+-- Get all plants without varieties.
+SELECT
+    id,
+    binomial_name,
+    mature_size_height,
+    flower_colour
+FROM plants;
+
+-- Get all varities.
+SELECT
+    v.id,
+    p.binomial_name,
+    COALESCE(v.mature_size_height, p.mature_size_height),
+    COALESCE(v.flower_colour, p.flower_colour)
+FROM varieties AS v
+LEFT JOIN plants p
+    ON v.plant_id = p.id;
+
+-- Get all plants and varieties.
+SELECT
+    p.id,
+    p.binomial_name,
+    p.mature_size_height,
+    p.flower_colour
+FROM plants AS p
+UNION
+SELECT
+    v.id,
+    p.binomial_name,
+    COALESCE(v.mature_size_height, p.mature_size_height),
+    COALESCE(v.flower_colour, p.flower_colour)
+FROM varieties AS v
+LEFT JOIN plants p
+    ON v.plant_id = p.id;
