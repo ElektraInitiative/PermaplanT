8f921b3f6f02f64e786bbda3ea1ed91c2eea2cff
Benjamin Zinschitz <benjamin.zinschitz@student.tuwien.ac.at>
2023-04-07 11:37:12 +0200

    database: extract taxons into separate migration

--- doc/decisions/example_migrations/2023-04-04-220921_plant_relationships/up.sql
+++ doc/decisions/example_migrations/2023-04-04-220921_plant_relationships/up.sql
@@ -1,18 +1,3 @@
-CREATE TYPE taxonomic_rank AS ENUM ('family', 'subfamily', 'genus', 'species');
-CREATE TABLE taxons (
-    id SERIAL PRIMARY KEY NOT NULL,
-    rank TAXONOMIC_RANK NOT NULL,
-    name VARCHAR NOT NULL,
-    parent_id INTEGER REFERENCES taxons (id) NULL
-);
-
--- TODO: drop taxonomic names in plants (family, subfamily, genus, species)
-ALTER TABLE plants
-    ADD COLUMN family_id INTEGER REFERENCES taxons (id) NOT NULL,
-    ADD COLUMN subfamily_id INTEGER REFERENCES taxons (id) NULL,
-    ADD COLUMN genus_id INTEGER REFERENCES taxons (id) NOT NULL,
-    ADD COLUMN species_id INTEGER REFERENCES taxons (id) NOT NULL;
-
 CREATE TYPE relationship_kind AS ENUM ('companion', 'antagonist');
 CREATE TABLE taxon_relationships (
     id SERIAL PRIMARY KEY NOT NULL,
