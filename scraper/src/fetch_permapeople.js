f7aa9ecd558bbcf865499065a2a661733ac64636
Nursultan <nursultan_imanov@mail.ru>
2023-04-11 22:06:52 +0200

    documentation in the code

--- scraper/src/fetch_permapeople.js
+++ scraper/src/fetch_permapeople.js
@@ -11,14 +11,26 @@ const headers = {
   'x-permapeople-key-secret': process.env.PERMAPEOPLE_KEY_SECRET,
 };
 
+/**
+ * Fetch all plants from permapeople API.
+ * The API returns 100 plants per page.
+ * This function fetches all the pages sequentially.
+ * The API has a rate limit, so we add a sleep of 1 second between each request.
+ * The information related to each plant is stored in a nested array called data.
+ * This function additionally flattens the data array to make it convenient to work with in the next step when writing to csv.
+ *
+ * @returns Array of plants
+ */
 async function fetchAllPlants() {
   const allPlants = [];
+
   const response = await axios.get(apiUrl, { headers });
   let plants = response.data.plants;
-  let i = 0;
+  let i = 0; // Page number just for logging
+
   while (plants.length > 0) {
-    // if (i == 1) break;
     console.log(`Fetching page ${i + 1}`);
+
     plants.forEach((plant) => {
       plant = plant.data.reduce((acc, curr) => {
         acc[curr.key] = curr.value;
@@ -27,19 +39,26 @@ async function fetchAllPlants() {
       delete plant.data;
       allPlants.push(plant);
     });
+
     const plant = plants.pop();
     const plantId = plant.id;
+
     const plantResponse = await axios.get(`${apiUrl}?last_id=${plantId}`, {
       headers,
     });
     plants = plantResponse.data.plants;
     i++;
+
     await new Promise((resolve) => setTimeout(resolve, 1000)); // Add a sleep of 1 second
   }
 
   return allPlants;
 }
 
+/**
+ * Write the plants to a csv file
+ * @param {*} plants Array of plants
+ */
 function writePlantsToCsv(plants) {
   if (!fs.existsSync('data')) {
     fs.mkdirSync('data');
