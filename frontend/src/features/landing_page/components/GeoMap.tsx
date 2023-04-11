90e582fb47ba890f857281f3f20cb11ce91708fc
Samuel <absurd.turtl3@gmail.com>
2023-04-11 15:42:06 +0200

    removed dark tiles because they won't load without subscription on pr.permaplant.net

--- frontend/src/features/landing_page/components/GeoMap.tsx
+++ frontend/src/features/landing_page/components/GeoMap.tsx
@@ -30,8 +30,8 @@ export const GeoMap = () => {
     <MapContainer center={[47.57, 16.496]} zoom={7} scrollWheelZoom={false}>
       {darkMode ? (
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       ) : (
         <TileLayer
