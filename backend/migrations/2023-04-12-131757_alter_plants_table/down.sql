e39db4b90d078177fbe478278084aaefba0f05e4
Nursultan <nursultan_imanov@mail.ru>
2023-04-12 15:31:45 +0200

    migration

---
+++ backend/migrations/2023-04-12-131757_alter_plants_table/down.sql
@@ -0,0 +1,56 @@
+-- This file should undo anything in `up.sql`
+-- Your SQL goes here
+ALTER TABLE plants
+    RENAME COLUMN scientific_name TO binomial_name;
+ALTER TABLE plants
+    RENAME CONSTRAINT plant_detail_scientific_name_key TO plant_detail_binomial_name_key;
+ALTER TABLE plants
+ALTER COLUMN binomial_name TYPE VARCHAR USING binomial_name::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN family TYPE VARCHAR USING family::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN subfamily TYPE VARCHAR USING subfamily::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN genus TYPE VARCHAR USING genus::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN edible_uses TYPE VARCHAR USING edible_uses::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN medicinal_uses TYPE VARCHAR USING medicinal_uses::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN material_uses_and_functions TYPE VARCHAR USING material_uses_and_functions::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN botanic TYPE VARCHAR USING botanic::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN propagation TYPE VARCHAR USING propagation::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN cultivation TYPE VARCHAR USING cultivation::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN environment TYPE VARCHAR USING environment::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN material_uses TYPE VARCHAR USING material_uses::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN functions TYPE VARCHAR USING functions::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN provides_forage_for TYPE VARCHAR USING provides_forage_for::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN provides_shelter_for TYPE VARCHAR USING provides_shelter_for::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN shade TYPE VARCHAR USING shade::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN native_climate_zones TYPE VARCHAR USING native_climate_zones::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN adapted_climate_zones TYPE VARCHAR USING adapted_climate_zones::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN native_geographical_range TYPE VARCHAR USING native_geographical_range::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN native_environment TYPE VARCHAR USING native_environment::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN ecosystem_niche TYPE VARCHAR USING ecosystem_niche::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN pollinators TYPE VARCHAR USING pollinators::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN flower_colour TYPE VARCHAR USING flower_colour::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN mature_size_height TYPE VARCHAR USING mature_size_height::VARCHAR;
+ALTER TABLE plants
+ALTER COLUMN mature_size_width TYPE VARCHAR USING mature_size_width::VARCHAR;
