cce405b12b9a070bca05ee8929c864f4b790d74d
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-04-14 13:59:33 +0200

    feat: add test for ButtonLink

---
+++ frontend/src/components/tests/ButtonLink.test.tsx
@@ -0,0 +1,10 @@
+import renderer from 'react-test-renderer';
+import ButtonLink from '../Button/ButtonLink';
+import {MemoryRouter, MemoryRouter as Router} from 'react-router-dom';
+
+it('renders correctly', () => {
+    const tree = renderer
+        .create(<MemoryRouter><ButtonLink to="https://dev.permaplant.net" title="Permaplant" /></MemoryRouter>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
