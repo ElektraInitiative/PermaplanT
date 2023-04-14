73884c8f83cd985f8811459d6b93f0785cbfca45
Benjamin Zinschitz <benjamin.zinschitz@student.tuwien.ac.at>
2023-04-14 16:40:55 +0200

    frontend: fix import oder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import NavContainer from './components/Layout/NavContainer';
 import Pages from './routes/Pages';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Fragment } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 function App() {
--- frontend/src/features/seeds/routes/ViewSeeds.tsx
+++ frontend/src/features/seeds/routes/ViewSeeds.tsx
@@ -6,11 +6,11 @@ import SearchInput from '@/components/Form/SearchInput';
 import PageTitle from '@/components/Header/PageTitle';
 import PageLayout from '@/components/Layout/PageLayout';
 import SimpleModal from '@/components/Modals/SimpleModal';
+import useDebounce from '@/utils/use-debounced-value';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { Suspense, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
-import { useInfiniteQuery } from '@tanstack/react-query';
-import useDebounce from '@/utils/use-debounced-value';
 
 export const ViewSeeds = () => {
   const navigate = useNavigate();
