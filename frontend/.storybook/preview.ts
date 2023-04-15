71610e5e6d838d468c402c6a8df34686e901e200
Paul Bushuo Buschmann <me@paulbuschmann.com>
2023-04-15 20:11:18 +0200

    chore(storybook): better sorting of stories

---
+++ frontend/.storybook/preview.ts
@@ -0,0 +1,23 @@
+import '../src/styles/globals.css';
+import { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    options: {
+      storySort: {
+        // careful this is case sensitive
+        order: ['docs', ['Readme', '*'], 'Components', '*'],
+        locales: 'en-US',
+      },
+    },
+  },
+};
+
+export default preview;
