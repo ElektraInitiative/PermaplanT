ed01c1a8b3b72a340aa455b2f9afa10b0e405d16
Thorben Julian Staufer <e11809935@student.tuwien.ac.at>
2023-04-15 12:50:26 +0200

    Added translation to imprint page
    Imprint page can now be viewed in german and english.

--- frontend/src/config/i18n/de/index.ts
+++ frontend/src/config/i18n/de/index.ts
@@ -1,11 +1,13 @@
 import common from './common.json';
 import enums from './enums.json';
+import imprint from './imprint.json';
 import seeds from './seeds.json';
 
 const de = {
   common,
   seeds,
   enums,
+  imprint,
 };
 
 export default de;
--- frontend/src/features/imprint_page/routes/ImprintPage.tsx
+++ frontend/src/features/imprint_page/routes/ImprintPage.tsx
@@ -1,20 +1,23 @@
 import Footer from '@/components/Layout/Footer';
 import PageLayout from '@/components/Layout/PageLayout';
 import ContactUs from '@/features/landing_page/components/ContactUs';
+import { useTranslation } from 'react-i18next';
 
 export function ImprintPage() {
+  const { t } = useTranslation(['imprint']);
+
   return (
     <PageLayout styleNames="body-font relative w-full text-center">
       <h1 className="title-font mb-8 text-3xl font-medium text-primary-500 dark:text-primary-300 sm:text-4xl">
-        Imprint
+        {t('imprint:title')}
       </h1>
       <section
         id="general"
         className="mx-auto mb-4 flex flex-col text-base leading-relaxed lg:w-2/3"
       >
         <p>PermaplanT</p>
-        <p>Chairman: Markus Raab</p>
-        <p>ZVR Nr.: not yet registered</p>
+        <p>{t('imprint:chairman')}: Markus Raab</p>
+        <p>ZVR Nr.: {t('imprint:zvr_placeholder')}</p>
         <p>
           E-Mail: <a href="mailto://contact@permaplant.net">contact@permaplant.net</a>
         </p>
@@ -26,11 +29,10 @@ export function ImprintPage() {
         <b className="title-font text-1xl mb-4 font-medium sm:text-2xl">Address</b>
         <p>Feldgasse 19</p>
         <p>7321 Unterfrauenhaid</p>
-        <p>Austria</p>
+        <p>{t('imprint:country')}</p>
       </section>
       <i className="mx-auto mb-4 flex flex-col text-base leading-relaxed lg:w-2/3">
-        This website is purely non-commercial, this website does not sell anything. It only offers
-        you various forms of memberships.
+        {t('imprint:legal_notice')}
       </i>
       <ContactUs />
       <Footer />
