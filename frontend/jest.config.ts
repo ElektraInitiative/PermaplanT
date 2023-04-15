e0b756bf363b414951625dcec9f5951303a6c7d4
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-04-15 11:42:29 +0200

    feat: configure jest for dom tests

---
+++ frontend/jest.config.ts
@@ -0,0 +1,8 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  verbose: true,
+  testEnvironment: 'jsdom',
+};
+
+export default config;
--- frontend/src/components/Form/SearchInput.test.tsx
+++ frontend/src/components/Form/SearchInput.test.tsx
@@ -1,10 +1,27 @@
+import {render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderer from 'react-test-renderer';
 import SearchInput from './SearchInput';
-import {MemoryRouter, MemoryRouter as Router} from 'react-router-dom';
+import {MemoryRouter} from 'react-router-dom';
+
+/**
+ * @jest-environment jsdom
+ */
 
 it('renders correctly', () => {
     const tree = renderer
         .create(<MemoryRouter><SearchInput placeholder='Test' handleSearch={() => {}}/></MemoryRouter>)
         .toJSON();
     expect(tree).toMatchSnapshot();
+});
+
+it('calls handleSearch on input change', async () => {
+    const {getByPlaceholderText} = render(
+        <MemoryRouter>
+            <SearchInput placeholder='Test' handleSearch={(event) => {expect(event.target.value).toBe('Hello World!')}}/>
+        </MemoryRouter>
+    );
+    
+    await userEvent.click(getByPlaceholderText('Test'));
+    await userEvent.paste('Hello World!');
 });
