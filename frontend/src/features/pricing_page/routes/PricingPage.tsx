6c639b35e96d52f3b46f099d017d9f73e49701d7
Thorben Julian Staufer <e11809935@student.tuwien.ac.at>
2023-04-17 21:02:31 +0200

    Add additional content to pricing page
    Page now contains an opening text and the footer. Also added a button to submit the selected option.

--- frontend/src/features/pricing_page/routes/PricingPage.tsx
+++ frontend/src/features/pricing_page/routes/PricingPage.tsx
@@ -1,5 +1,7 @@
 import PricingOption from '../components/PricingOption';
+import SimpleButton from '@/components/Button/SimpleButton';
 import PageTitle from '@/components/Header/PageTitle';
+import Footer from '@/components/Layout/Footer';
 import PageLayout from '@/components/Layout/PageLayout';
 import { useState } from 'react';
 
@@ -19,6 +21,7 @@ export default function PricingPage() {
     },
   ];
   const [pricingOptions, setPricingOptions] = useState(initialOptions);
+  const selected = pricingOptions.find((option) => option.isSelected) !== undefined;
 
   const options = pricingOptions.map((option) => {
     return <PricingOption key={option.title} {...option} onClickHandler={selectPricingOption} />;
@@ -37,9 +40,17 @@ export default function PricingPage() {
   }
 
   return (
-    <PageLayout styleNames="flex flex-col space-y-4">
+    <PageLayout styleNames="flex flex-col gap-12 space-y-4">
       <PageTitle title="Pricing" />
+      <span className="text-center">This text tells you a little about our pricing strategy!</span>
       <div className="grid grid-cols-2 gap-8">{options}</div>
+      <SimpleButton
+        disabled={!selected}
+        className={!selected ? 'opacity-50 hover:bg-primary-500 dark:hover:bg-primary-300' : ''}
+      >
+        Apply Now!
+      </SimpleButton>
+      <Footer />
     </PageLayout>
   );
 }
