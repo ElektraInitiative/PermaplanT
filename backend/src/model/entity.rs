6aa8f9f76cabe2bc273fab93b8eda66d54fbe1c0
Nursultan <nursultan_imanov@mail.ru>
2023-04-22 11:47:11 +0200

    update plants struct documentation

--- backend/src/model/entity.rs
+++ backend/src/model/entity.rs
@@ -25,13 +25,13 @@ use super::r#enum::{
 #[derive(Identifiable, Queryable)]
 #[diesel(table_name = plants)]
 pub struct Plants {
-    /// The primary key.
+    /// The internal id of the plant.
     pub id: i32,
     /// The unique name of the plant.
     pub unique_name: String,
-    /// The name of the plant in English.
+    /// The list of the common names of the plant.
     pub common_name_en: Option<Vec<Option<String>>>,
-    /// The name of the plant in German.
+    /// The list of the common names of the plant in german. Additionally fetched from Wikidata API.
     pub common_name_de: Option<Vec<Option<String>>>,
     /// Family of the plant.
     pub family: Option<String>,
@@ -39,109 +39,213 @@ pub struct Plants {
     pub subfamily: Option<String>,
     /// Genus of the plant.
     pub genus: Option<String>,
+    ///
     pub edible_uses_en: Option<String>,
+    ///
     pub medicinal_uses: Option<String>,
+    ///
     pub material_uses_and_functions: Option<String>,
+    ///
     pub botanic: Option<String>,
+    ///
     pub cultivation: Option<String>,
+    ///
     pub material_uses: Option<String>,
+    ///
     pub functions: Option<String>,
+    ///
     pub provides_forage_for: Option<String>,
+    ///
     pub provides_shelter_for: Option<String>,
+    ///
     pub heat_zone: Option<i16>,
+    ///
     pub shade: Option<String>,
+    /// The list of enum values.
     pub soil_ph: Option<Vec<Option<SoilPh>>>,
+    /// The list of enum values.
     pub soil_texture: Option<Vec<Option<SoilTexture>>>,
+    /// The list of enum values.
     pub soil_water_retention: Option<Vec<Option<SoilWaterRetention>>>,
+    ///
     pub environmental_tolerances: Option<Vec<Option<String>>>,
+    ///
     pub native_climate_zones: Option<String>,
+    ///
     pub adapted_climate_zones: Option<String>,
+    ///
     pub native_geographical_range: Option<String>,
+    ///
     pub native_environment: Option<String>,
+    ///
     pub ecosystem_niche: Option<String>,
+    /// Enum value.
     pub deciduous_or_evergreen: Option<DeciduousOrEvergreen>,
+    /// Enum value.
     pub herbaceous_or_woody: Option<HerbaceousOrWoody>,
+    /// The list of enum values.
     pub life_cycle: Option<Vec<Option<LifeCycle>>>,
+    /// The list of enum values.
     pub growth_rate: Option<Vec<Option<GrowthRate>>>,
+    ///
     pub height: Option<String>,
+    ///
     pub width: Option<String>,
+    /// The list of enum values.
     pub fertility: Option<Vec<Option<Fertility>>>,
+    ///
     pub pollinators: Option<String>,
+    ///
     pub flower_colour: Option<String>,
+    /// Enum value.
     pub flower_type: Option<FlowerType>,
+    /// The creation date of the entry.
     pub created_at: NaiveDateTime,
+    /// The last update date of the entry.
     pub updated_at: NaiveDateTime,
+    ///
     pub has_drought_tolerance: Option<bool>,
+    ///
     pub tolerates_wind: Option<bool>,
+    /// The list of the references of the plant.
     pub plant_references: Option<Vec<Option<String>>>,
+    /// Boolean value indicating whether the plant is a tree. The initial value is to `True` if  herbaceous_or_woody (woody) and life_cycle (perennial)
     pub is_tree: Option<bool>,
+    /// Enum value. The initial value is to `light feeder` if "Nutritionally poor soil" in `environmental_tolerances` is present.
     pub nutrition_demand: Option<NutritionDemand>,
+    /// Number value between -1..6 (-1 should be printed as 00)
     pub preferable_permaculture_zone: Option<i16>,
+    /// Date value fetched from PracticalPlants page showing the last modification date of the plant.
     pub article_last_modified_at: Option<NaiveDateTime>,
+    ///
     pub hardiness_zone: Option<String>,
+    /// The list of enum values.
     pub light_requirement: Option<Vec<Option<LightRequirement>>>,
+    /// The list of enum values.
     pub water_requirement: Option<Vec<Option<WaterRequirement>>>,
+    ///
     pub propagation_method: Option<Vec<Option<PropagationMethod>>>,
+    ///
     pub alternate_name: Option<String>,
+    ///
     pub diseases: Option<String>,
+    ///
     pub edible: Option<bool>,
+    ///
     pub edible_parts: Option<Vec<Option<String>>>,
+    ///
     pub germination_temperature: Option<String>,
+    ///
     pub introduced_into: Option<String>,
+    ///
     pub layer: Option<String>,
+    ///
     pub leaves: Option<String>,
+    ///
     pub medicinal_parts: Option<String>,
+    ///
     pub native_to: Option<String>,
+    ///
     pub plants_for_a_future: Option<String>,
+    ///
     pub plants_of_the_world_online_link: Option<String>,
+    ///
     pub plants_of_the_world_online_link_synonym: Option<String>,
+    ///
     pub pollination: Option<String>,
+    ///
     pub propagation_transplanting: Option<String>,
+    ///
     pub resistance: Option<String>,
+    ///
     pub root_type: Option<String>,
+    ///
     pub seed_planting_depth_en: Option<String>,
+    ///
     pub seed_viability: Option<String>,
+    ///
     pub slug: Option<String>,
+    ///
     pub spread: Option<String>,
+    ///
     pub thining: Option<String>,
+    ///
     pub utility: Option<String>,
+    ///
     pub warning: Option<String>,
+    ///
     pub when_to_plant_cuttings: Option<String>,
+    ///
     pub when_to_plant_division: Option<String>,
+    ///
     pub when_to_plant_transplant: Option<String>,
+    ///
     pub when_to_sow_indoors: Option<String>,
+    ///
     pub sowing_outdoors_en: Option<String>,
+    ///
     pub when_to_start_indoors_weeks: Option<String>,
+    ///
     pub when_to_start_outdoors_weeks: Option<String>,
+    ///
     pub cold_stratification_temperature: Option<String>,
+    ///
     pub cold_stratification_time: Option<String>,
+    ///
     pub days_to_harvest: Option<String>,
+    ///
     pub habitat: Option<String>,
+    ///
     pub spacing_en: Option<String>,
+    ///
     pub wikipedia: Option<String>,
+    ///
     pub days_to_maturity: Option<String>,
+    ///
     pub pests: Option<String>,
+    ///
     pub version: Option<String>,
+    ///
     pub germination_time: Option<String>,
+    ///
     pub description: Option<String>,
+    /// The permapeople id of the parent entry pointing to the `external_id` column.
     pub parent_id: Option<String>,
+    /// Enum value indicating the source of the entry.
     pub external_source: Option<ExternalSource>,
+    /// The external id of the entry used in combination with the `external_source` column.
     pub external_id: Option<String>,
+    /// The external url provided by the origin source.
     pub external_url: Option<String>,
+    ///
     pub root_depth: Option<String>,
+    ///
     pub external_article_number: Option<String>,
+    ///
     pub external_portion_content: Option<String>,
+    ///
     pub sowing_outdoors_de: Option<String>,
+    ///
     pub sowing_outdoors: Option<String>,
+    ///
     pub harvest_time: Option<String>,
+    ///
     pub spacing_de: Option<String>,
+    ///
     pub required_quantity_of_seeds_de: Option<String>,
+    ///
     pub required_quantity_of_seeds_en: Option<String>,
+    ///
     pub seed_planting_depth_de: Option<String>,
+    ///
     pub seed_weight_1000_de: Option<String>,
+    ///
     pub seed_weight_1000_en: Option<String>,
+    ///
     pub seed_weight_1000: Option<String>,
+    ///
     pub machine_cultivation_possible: Option<String>,
+    ///
     pub edible_uses_de: Option<String>,
 }
 /// The `Seed` entity.
