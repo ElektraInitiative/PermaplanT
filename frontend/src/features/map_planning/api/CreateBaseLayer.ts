890e0cb1287114a653d4242fc2119f02d6ebc265
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-05-01 21:14:51 +0200

    chore: run prettier

--- frontend/src/features/map_planning/api/CreateBaseLayer.ts
+++ frontend/src/features/map_planning/api/CreateBaseLayer.ts
@@ -1,11 +1,11 @@
-import {NewBaseLayerDto} from "../../../bindings/definitions";
-import axios from "axios";
-import {baseApiUrl} from "../../../config";
+import { NewBaseLayerDto } from '../../../bindings/definitions';
+import { baseApiUrl } from '../../../config';
+import axios from 'axios';
 
 export const createBaseLayer = async (baseLayer: NewBaseLayerDto) => {
-    try {
-        await axios.post<NewBaseLayerDto>(`${baseApiUrl}/api/base_layers`, baseLayer);
-    } catch (error) {
-        throw error as Error;
-    }
-}
+  try {
+    await axios.post<NewBaseLayerDto>(`${baseApiUrl}/api/base_layers`, baseLayer);
+  } catch (error) {
+    throw error as Error;
+  }
+};
--- frontend/src/features/map_planning/routes/CreateMap.tsx
+++ frontend/src/features/map_planning/routes/CreateMap.tsx
@@ -1,26 +1,25 @@
-import PageTitle from "@/components/Header/PageTitle";
-import PageLayout from "@/components/Layout/PageLayout";
-import BaseLayerConfigurator from "../components/BaseLayerConfigurator";
-import {createBaseLayer} from "@/features/map_planning/api/CreateBaseLayer";
-import {NewBaseLayerDto} from "@/bindings/definitions";
-import {useNavigate} from "react-router-dom";
+import BaseLayerConfigurator from '../components/BaseLayerConfigurator';
+import { NewBaseLayerDto } from '@/bindings/definitions';
+import PageTitle from '@/components/Header/PageTitle';
+import PageLayout from '@/components/Layout/PageLayout';
+import { createBaseLayer } from '@/features/map_planning/api/CreateBaseLayer';
+import { useNavigate } from 'react-router-dom';
 
 export const CreateMap = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const onSubmit = async (baseLayer: NewBaseLayerDto) => {
+    await createBaseLayer(baseLayer);
 
-    const onSubmit = async (baseLayer: NewBaseLayerDto) => {
-        await createBaseLayer(baseLayer);
+    // redirect to the map page
+    // TODO: redirect to the newly created map
+    navigate('/map');
+  };
 
-        // redirect to the map page
-        // TODO: redirect to the newly created map
-        navigate('/map');
-    }
-
-    return (
-        <PageLayout>
-            <PageTitle title={"Create a new Map"} />
-            <BaseLayerConfigurator onSubmit={onSubmit}></BaseLayerConfigurator>
-        </PageLayout>
-    );
-}
+  return (
+    <PageLayout>
+      <PageTitle title={'Create a new Map'} />
+      <BaseLayerConfigurator onSubmit={onSubmit}></BaseLayerConfigurator>
+    </PageLayout>
+  );
+};
