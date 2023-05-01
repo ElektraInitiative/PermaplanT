d016ce5810ba9c08d4d91bef92330549854b1c2d
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-05-01 21:14:14 +0200

    feat: submit created base layer

--- frontend/src/features/map_planning/routes/CreateMap.tsx
+++ frontend/src/features/map_planning/routes/CreateMap.tsx
@@ -1,12 +1,26 @@
 import PageTitle from "@/components/Header/PageTitle";
 import PageLayout from "@/components/Layout/PageLayout";
 import BaseLayerConfigurator from "../components/BaseLayerConfigurator";
+import {createBaseLayer} from "@/features/map_planning/api/CreateBaseLayer";
+import {NewBaseLayerDto} from "@/bindings/definitions";
+import {useNavigate} from "react-router-dom";
 
 export const CreateMap = () => {
+
+    const navigate = useNavigate();
+
+    const onSubmit = async (baseLayer: NewBaseLayerDto) => {
+        await createBaseLayer(baseLayer);
+
+        // redirect to the map page
+        // TODO: redirect to the newly created map
+        navigate('/map');
+    }
+
     return (
         <PageLayout>
             <PageTitle title={"Create a new Map"} />
-            <BaseLayerConfigurator></BaseLayerConfigurator>
+            <BaseLayerConfigurator onSubmit={onSubmit}></BaseLayerConfigurator>
         </PageLayout>
     );
 }
