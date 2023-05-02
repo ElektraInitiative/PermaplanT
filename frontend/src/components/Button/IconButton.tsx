40f0eecefc74f58968e4946546a7f9cafa789a2a
Samuel <absurd.turtl3@gmail.com>
2023-05-02 19:56:36 +0200

    added colors for fill

--- frontend/src/components/Button/IconButton.tsx
+++ frontend/src/components/Button/IconButton.tsx
@@ -14,11 +14,11 @@ export enum ButtonVariant {
  */
 export default function IconButton({ variant = ButtonVariant.primary, ...props }: IconButtonProps) {
   const colors = [
-    'hover:text-primary-500 dark:hover:text-primary-500 hover:stroke-primary-500 dark:hover:stroke-primary-500 active:stroke-primary-200 dark:active:stroke-primary-200',
+    'hover:text-primary-500 dark:hover:text-primary-500 hover:stroke-primary-500 dark:hover:stroke-primary-500 active:stroke-primary-200 dark:active:stroke-primary-200  hover:fill-primary-500 dark:hover:fill-primary-500 active:fill-primary-200 dark:active:fill-primary-200',
     'hover:text-secondary-500 dark:hover:text-secondary-500 active:stroke-secondary-900 dark:active:stroke-secondary-900',
   ];
   const className =
-    'inline-flex h-6 w-6 justify-center rounded-lg items-center text-sm font-medium focus:outline-none focus:ring-4 focus:ring-blue-300 stroke-neutral-800 dark:stroke-neutral-800-dark' +
+    'inline-flex h-6 w-6 justify-center rounded-lg items-center text-sm font-medium focus:outline-none focus:ring-4 focus:ring-blue-300 stroke-neutral-800 dark:stroke-neutral-800-dark fill-neutral-800 dark:fill-neutral-800-dark' +
     ' ' +
     colors[variant];
   return (
