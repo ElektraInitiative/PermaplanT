366c87c036f94fc32a6dd99f9764957790cf5dec
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-05-02 10:58:41 +0200

    #329 handle invalid tokens

--- backend/src/config/auth/jwt_claims.rs
+++ backend/src/config/auth/jwt_claims.rs
@@ -1,16 +1,12 @@
 use actix_web::http::{header, StatusCode};
-use jsonwebtoken::{decode, decode_header, jwk::Jwk, DecodingKey, Validation};
+use jsonwebtoken::{
+    decode, decode_header,
+    jwk::{Jwk, JwkSet},
+    DecodingKey, Validation,
+};
 use serde::Deserialize;
 
 use crate::error::ServiceError;
-
-/// The URL where Keycloak's public keys for JWT verification can be found.
-const JKWS_URL: &str = "http://localhost:8081/realms/PermaplanT/protocol/openid-connect/certs";
-
-#[derive(Debug, Clone, Deserialize)]
-struct JwkSetHelper {
-    keys: Vec<serde_json::Value>,
-}
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Claims {
@@ -21,32 +17,59 @@ pub struct Claims {
 impl Claims {
     pub async fn validate(token: &str) -> Result<Self, ServiceError> {
         let header = decode_header(token).unwrap();
+        let kid = header.kid.as_ref().unwrap();
 
         // TODO: use extensions or middleware. Otherwise this function gets called twice
         // [`JwkSet`] doesn't work as Keycloak uses algorithms jsonwebtoken doesn't support. This mean deserialization would fail.
-        let jwk = reqwest::get(JKWS_URL)
-            .await
-            .unwrap()
-            .json::<JwkSetHelper>()
-            .await
-            .unwrap()
-            .keys
-            .into_iter()
-            .filter_map(|key| serde_json::from_value::<Jwk>(key).ok())
-            .find(|a| a.common.key_id.as_ref().unwrap() == header.kid.as_ref().unwrap())
-            .unwrap();
+        let jwks = fetch_keys().await?;
 
-        println!("{jwk:?}");
+        let jwk = jwks.find(kid).ok_or(ServiceError::new(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "no valid key found".to_owned(),
+        ))?;
 
         let claims = decode(
             token,
             &DecodingKey::from_jwk(&jwk).unwrap(),
             &Validation::new(header.alg),
         )
-        .unwrap()
+        .map_err(|_| ServiceError::new(StatusCode::UNAUTHORIZED, "invalid token".to_owned()))?
         .claims;
         Ok(claims)
     }
+}
+
+async fn fetch_keys() -> Result<JwkSet, ServiceError> {
+    #[derive(Debug, Clone, Deserialize)]
+    struct JwkSetHelper {
+        keys: Vec<serde_json::Value>,
+    }
+
+    /// The URL where Keycloak's public keys for JWT verification can be found.
+    const JKWS_URL: &str = "http://localhost:8081/realms/PermaplanT/protocol/openid-connect/certs";
+
+    let keys = reqwest::get(JKWS_URL)
+        .await
+        .map_err(|_| {
+            ServiceError::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "unable to fetch keys".to_owned(),
+            )
+        })?
+        .json::<JwkSetHelper>()
+        .await
+        .map_err(|_| {
+            ServiceError::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "invalid response".to_owned(),
+            )
+        })?
+        .keys
+        .into_iter()
+        .filter_map(|key| serde_json::from_value::<Jwk>(key).ok())
+        .collect::<Vec<_>>();
+    let jwks = JwkSet { keys };
+    Ok(jwks)
 }
 
 pub fn get_token_from_request(req: &actix_web::HttpRequest) -> Result<String, ServiceError> {
