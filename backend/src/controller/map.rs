282123c1e2f0dd42100ac300aebcc6ae7ada912a
Thorben Julian Staufer <e11809935@student.tuwien.ac.at>
2023-05-05 15:46:45 +0200

    Added endpoints for map versions
    Extended the map controller with endpoints for loading and saving snapshots.

--- backend/src/controller/map.rs
+++ backend/src/controller/map.rs
@@ -7,7 +7,9 @@ use actix_web::{
     HttpResponse, Result,
 };
 
-use crate::model::dto::{MapSearchParameters, PageParameters};
+use crate::model::dto::{
+    MapSearchParameters, MapVersionSearchParameters, NewMapVersionDto, PageParameters,
+};
 use crate::{db::connection::Pool, model::dto::NewMapDto, service};
 
 /// Endpoint for fetching or searching all [`Map`](crate::model::entity::Map).
@@ -67,5 +69,53 @@ pub async fn find_by_id(map_id: Path<i32>, pool: Data<Pool>) -> Result<HttpRespo
 #[post("")]
 pub async fn create(new_map_json: Json<NewMapDto>, pool: Data<Pool>) -> Result<HttpResponse> {
     let response = service::map::create(new_map_json.0, &pool).await?;
+    Ok(HttpResponse::Created().json(response))
+}
+
+/// Endpoint for fetching or searching all [`MapVersion`](crate::model::entity::MapVersion).
+/// Search parameters are taken from the URLs query string (e.g. .../api/maps?map_id=1&per_page=5).
+/// If no page parameters are provided, the first page is returned.
+///
+/// # Errors
+/// * If the connection to the database could not be established.
+#[utoipa::path(
+    context_path = "/api/users/{user_id}/maps",
+    params(
+        MapVersionSearchParameters,
+        PageParameters
+    ),
+    responses(
+        (status = 200, description = "Fetch or search all map versions", body = PageMapVersionDto)
+    )
+)]
+#[get("/{map_id}/versions")]
+pub async fn show_versions(
+    search_query: Query<MapVersionSearchParameters>,
+    page_query: Query<PageParameters>,
+    pool: Data<Pool>,
+) -> Result<HttpResponse> {
+    let response =
+        service::map::show_versions(search_query.into_inner(), page_query.into_inner(), &pool)
+            .await?;
+    Ok(HttpResponse::Ok().json(response))
+}
+
+/// Endpoint for saving a new [`MapVersion`](crate::model::entity::MapVersion).
+///
+/// # Errors
+/// * If the connection to the database could not be established.
+#[utoipa::path(
+    context_path = "/api/users/{user_id}/maps",
+    request_body = NewMapVersionDto,
+    responses(
+        (status = 201, description = "Save a new map version", body = MapVersionDto)
+    )
+)]
+#[post("/{map_id}/versions")]
+pub async fn save_snapshot(
+    new_map_version_json: Json<NewMapVersionDto>,
+    pool: Data<Pool>,
+) -> Result<HttpResponse> {
+    let response = service::map::save_snapshot(new_map_version_json.0, &pool).await?;
     Ok(HttpResponse::Created().json(response))
 }
