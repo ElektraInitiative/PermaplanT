1317b52d89287b5ee7e19a28c505b641b44081fa
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-05-08 17:11:23 +0200

    fix: translations of createMap component

--- frontend/src/config/i18n/en/index.ts
+++ frontend/src/config/i18n/en/index.ts
@@ -2,6 +2,7 @@ import auth from './auth.json';
 import blog from './blog.json';
 import common from './common.json';
 import contact from './contact.json';
+import createMap from './createMap.json';
 import enums from './enums.json';
 import featureDescriptions from './featureDescriptions.json';
 import geomap from './geomap.json';
@@ -21,6 +22,7 @@ const en = {
   blog,
   common,
   contact,
+  createMap,
   enums,
   featureDescriptions,
   geomap,
