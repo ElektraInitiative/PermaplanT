a6b0202b6b4c678eed76c5227ded22664781b297
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-05-09 15:52:17 +0200

    fix: SimpleFormInput in Createseed component.

--- frontend/src/components/Form/SimpleFormInput.tsx
+++ frontend/src/components/Form/SimpleFormInput.tsx
@@ -1,14 +1,22 @@
 import { HTMLInputTypeAttribute } from 'react';
 import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 
-interface SimpleFormInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface SimpleFormInputProps<T extends FieldValues> extends React.InputHTMLAttributes<HTMLInputElement> {
+  id: Path<T>, 
   labelText: string;
+  register?: UseFormRegister<T>;
+  valueAsNumber?: boolean;
+  errorTitle?: string;
 }
 
-export default function SimpleFormInput ({
+export default function SimpleFormInput<T extends FieldValues> ({
+  id,
   labelText = '',
+  register,
+  valueAsNumber = false,
+  errorTitle,
   ...props
-}: SimpleFormInputProps) {
+}: SimpleFormInputProps<T>) {
   // Extract the input fields value from the respective Events before calling onChange.
 /*  const callOnChange = function <E>(event: React.ChangeEvent<E> | React.KeyboardEvent<E>) {
     if (onChange == null) return;
@@ -25,12 +33,18 @@ export default function SimpleFormInput ({
 
   return (
     <div className="dark:text-white">
-      <label htmlFor={props.id} className="mb-2 block text-sm font-medium">
+      <label htmlFor={id} className="mb-2 block text-sm font-medium">
         {labelText}
         {props.required ? <span className="text-red-800"> *</span> : <></>}
       </label>
       <input
-         {...props} 
+        id={id}
+        title={errorTitle}
+        {...props}
+        {...register?.(id, {
+            valueAsNumber: props.type === 'number' || valueAsNumber,
+        })}
+        className="block h-11 w-full rounded-lg border border-neutral-500 bg-neutral-100 p-2.5 text-sm placeholder-neutral-500 focus:border-primary-500 focus:outline-none dark:border-neutral-400-dark dark:bg-neutral-50-dark dark:focus:border-primary-300"
       />
     </div>
   );
