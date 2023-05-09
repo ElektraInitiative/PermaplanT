1e804278b9d638e7f06ba7f83f9913709e27395b
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-05-09 19:08:13 +0200

    fix: linter errors after merge.

--- frontend/src/components/Form/SimpleFormInput.tsx
+++ frontend/src/components/Form/SimpleFormInput.tsx
@@ -1,15 +1,15 @@
-import { HTMLInputTypeAttribute } from 'react';
 import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 
-interface SimpleFormInputProps<T extends FieldValues> extends React.InputHTMLAttributes<HTMLInputElement> {
-  id: Path<T>, 
+interface SimpleFormInputProps<T extends FieldValues>
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  id: Path<T>;
   labelText: string;
   register?: UseFormRegister<T>;
   valueAsNumber?: boolean;
   errorTitle?: string;
 }
 
-export default function SimpleFormInput<T extends FieldValues> ({
+export default function SimpleFormInput<T extends FieldValues>({
   id,
   labelText = '',
   register,
@@ -18,7 +18,7 @@ export default function SimpleFormInput<T extends FieldValues> ({
   ...props
 }: SimpleFormInputProps<T>) {
   // Extract the input fields value from the respective Events before calling onChange.
-/*  const callOnChange = function <E>(event: React.ChangeEvent<E> | React.KeyboardEvent<E>) {
+  /*  const callOnChange = function <E>(event: React.ChangeEvent<E> | React.KeyboardEvent<E>) {
     if (onChange == null) return;
 
     // If somebody finds a way of fixing the next line, please do!
@@ -42,7 +42,7 @@ export default function SimpleFormInput<T extends FieldValues> ({
         title={errorTitle}
         {...props}
         {...register?.(id, {
-            valueAsNumber: props.type === 'number' || valueAsNumber,
+          valueAsNumber: props.type === 'number' || valueAsNumber,
         })}
         className="block h-11 w-full rounded-lg border border-neutral-500 bg-neutral-100 p-2.5 text-sm placeholder-neutral-500 focus:border-primary-500 focus:outline-none dark:border-neutral-400-dark dark:bg-neutral-50-dark dark:focus:border-primary-300"
       />
