3be4d2a53d374ee30fcbac77ec31efc10fd69446
giancarlo_buenaflor@yahoo.com <giancarlo_buenaflor@yahoo.com>
2023-05-14 18:40:44 +0200

    change from drag and drop to click and add

--- frontend/src/features/map_planning/components/toolbar/PlantSearch.tsx
+++ frontend/src/features/map_planning/components/toolbar/PlantSearch.tsx
@@ -1,5 +1,6 @@
 import IconButton from '@/components/Button/IconButton';
 import SearchInput from '@/components/Form/SearchInput';
+import useMapStore from '@/features/undo_redo';
 import { ReactComponent as PlantIcon } from '@/icons/plant.svg';
 import { ReactComponent as SearchIcon } from '@/icons/search.svg';
 import { AnimatePresence, motion } from 'framer-motion';
@@ -113,16 +114,45 @@ const allPlants = [
   'Zelkova',
 ];
 
-/** UI component intended for searching plants that can be drag and dropped to the plant layer */
+/** UI component intended for searching plants that can be added to the plant layer */
 export const PlantSearch = () => {
   const [plants, setPlants] = useState(allPlants);
   const [searchVisible, setSearchVisible] = useState(false);
   const searchInputRef = useRef<HTMLInputElement>(null);
   const { t } = useTranslation(['plantSearch']);
+  const dispatch = useMapStore((map) => map.dispatch);
 
   useEffect(() => {
     searchInputRef.current?.focus();
   }, [searchVisible]);
+
+  /**
+   * This adds the chosen plant to the stage, specifically the Plant's layer.
+   *
+   * NOTE: this function is not finished yet.
+   *
+   * @param plant the plant to add to the stage
+   */
+  const addPlantToStage = (plant: string) => {
+    // Plant is currently unused as the object creation is still in works,
+    // but this should illustrate how to add a plant from the plants toolbar section.
+    // Todo: The final results should put the plant in the plant layer.
+    dispatch({
+      type: 'OBJECT_ADD',
+      payload: {
+        index: 'plant',
+        id: Math.random().toString(36).slice(2, 9),
+        x: 300,
+        y: 300,
+        width: 100,
+        height: 100,
+        type: 'rect',
+        rotation: 0,
+        scaleX: 1,
+        scaleY: 1,
+      },
+    });
+  };
 
   return (
     <div className="flex flex-col gap-4 p-2">
@@ -160,7 +190,6 @@ export const PlantSearch = () => {
                 setPlants(allPlants.filter((plant) => exp.test(plant)));
               }}
               ref={searchInputRef}
-              onBlur={() => setSearchVisible(false)}
               onKeyDown={(e) => {
                 if (e.key === 'Escape') setSearchVisible(false);
               }}
@@ -170,6 +199,9 @@ export const PlantSearch = () => {
                 <li
                   className="flex items-center gap-4 stroke-neutral-700 hover:stroke-primary-500 hover:text-primary-500 dark:stroke-neutral-700-dark"
                   key={plant}
+                  onClick={() => {
+                    addPlantToStage(plant);
+                  }}
                 >
                   <PlantIcon />
                   {plant}
