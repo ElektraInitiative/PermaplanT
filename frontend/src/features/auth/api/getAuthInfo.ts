467a7f581cf624e044f8b6afa5952866f960121e
Samuel <absurd.turtl3@gmail.com>
2023-05-16 18:35:12 +0200

    used Dto from shared types

--- frontend/src/features/auth/api/getAuthInfo.ts
+++ frontend/src/features/auth/api/getAuthInfo.ts
@@ -1,14 +1,10 @@
+import { ConfigDto } from '@/bindings/definitions';
 import { baseApiUrl } from '@/config';
 import axios from 'axios';
 
-interface AuthInfoDto {
-  issuer_uri: string;
-  client_id: string;
-}
-
-export const getAuthInfo = async (): Promise<AuthInfoDto> => {
+export const getAuthInfo = async (): Promise<ConfigDto> => {
   try {
-    const response = await axios.get<AuthInfoDto>(`${baseApiUrl}/api/config`);
+    const response = await axios.get<ConfigDto>(`${baseApiUrl}/api/config`);
     return response.data;
   } catch (error) {
     throw error as Error;
