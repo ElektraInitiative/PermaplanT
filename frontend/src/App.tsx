7e2c99536c45eeeb44bc569936a949d7a75fba98
Samuel <absurd.turtl3@gmail.com>
2023-05-16 12:08:15 +0200

    refactor: extract custom hook 'init dark mode'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,21 @@ import { useAuth } from 'react-oidc-context';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+const useInitDarkMode = () => {
+  const darkMode = localStorage.getItem('darkMode');
+
+  useEffect(() => {
+    if (darkMode === 'true') {
+      document.documentElement.classList.add('dark');
+    } else {
+      localStorage.setItem('darkMode', 'false');
+    }
+  }, [darkMode]);
+};
+
 function App() {
   const auth = useAuth();
-
-  if (localStorage.getItem('darkMode') === 'true') {
-    document.documentElement.classList.add('dark');
-  } else {
-    localStorage.setItem('darkMode', 'false');
-  }
+  useInitDarkMode();
 
   // authentication notifications
   useEffect(() => {
@@ -46,7 +53,7 @@ function App() {
   }, [auth.isAuthenticated, auth.user]);
 
   return (
-    <div>
+    <>
       <NavContainer>
         <Pages />
       </NavContainer>
@@ -56,7 +63,7 @@ function App() {
           'Toastify__progress-bar--animated bottom-0 left-0 origin-left absolute h-1 w-full bg-primary-500 dark:bg-primary-300'
         }
       />
-    </div>
+    </>
   );
 }
 
