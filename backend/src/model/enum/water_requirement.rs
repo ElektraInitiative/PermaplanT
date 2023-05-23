03f525d0ba1f8096eea66861660668a4022b8a0f
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-05-23 14:49:37 +0200

    #258 remove unneeded variants from water_requirements

--- backend/src/model/enum/water_requirement.rs
+++ backend/src/model/enum/water_requirement.rs
@@ -5,39 +5,28 @@ use serde::{Deserialize, Serialize};
 use typeshare::typeshare;
 use utoipa::ToSchema;
 
-#[allow(clippy::missing_docs_in_private_items)] // TODO: See #97.
+/// *Used* in hydrology layer.
+/// *Fetched from* PracticalPlants and Permapeople (merged with `water` of PracticalPlants).
 #[typeshare]
 #[derive(Serialize, Deserialize, DbEnum, Debug, ToSchema)]
 #[ExistingTypePath = "crate::schema::sql_types::WaterRequirement"]
 pub enum WaterRequirement {
-    #[serde(rename = "low")]
-    #[db_rename = "low"]
-    Low,
-
-    #[serde(rename = "moderate")]
-    #[db_rename = "moderate"]
-    Moderate,
-
-    #[serde(rename = "high")]
-    #[db_rename = "high"]
-    High,
-
-    #[serde(rename = "aquatic")]
-    #[db_rename = "aquatic"]
-    Aquatic,
-
+    /// well drained, little water input
     #[serde(rename = "dry")]
     #[db_rename = "dry"]
     Dry,
 
+    /// regular water supply, e.g. flat bed with humus
     #[serde(rename = "moist")]
     #[db_rename = "moist"]
     Moist,
 
+    /// drowned, (often) flooded or in general very moist, e.g. swamp
     #[serde(rename = "wet")]
     #[db_rename = "wet"]
     Wet,
 
+    /// completely aquatic
     #[serde(rename = "water")]
     #[db_rename = "water"]
     Water,
