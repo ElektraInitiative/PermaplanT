169a601df7ee5d27485b717465b8d99970c92c84
Samuel <absurd.turtl3@gmail.com>
2023-05-26 09:34:51 +0200

    WIP: added createConversation and getMessages

--- frontend/src/features/nextcloud_integration/api/chat.ts
+++ frontend/src/features/nextcloud_integration/api/chat.ts
@@ -1,3 +1,5 @@
+// Documentation is from https://nextcloud-talk.readthedocs.io/en/latest/chat/
+
 import { createNextcloudAPI } from "@/config/axios";
 
 export const sendMessage = (chatToken: string, message: string) => {
@@ -11,10 +13,134 @@ export const sendMessage = (chatToken: string, message: string) => {
   })
 }
 
-export const getConversations = async () => {
+
+export type TalkConversation = {
+    id: number;
+    token: string;
+    type: number;
+    name: string;
+    displayName: string;
+    objectType: string;
+    objectId: string;
+    participantType: number;
+    participantFlags: number;
+    readOnly: number;
+    hasPassword: boolean;
+    hasCall: boolean;
+    canStartCall: boolean;
+    lastActivity: number;
+    lastReadMessage: number;
+    unreadMessages: number;
+    unreadMention: boolean;
+    unreadMentionDirect: boolean;
+    isFavorite: boolean;
+    canLeaveConversation: boolean;
+    canDeleteConversation: boolean;
+    notificationLevel: number;
+    notificationCalls: number;
+    lobbyState: number;
+    lobbyTimer: number;
+    lastPing: number;
+    sessionId: string;
+    lastMessage: {
+        id: number;
+        token: string;
+        actorType: string;
+        actorId: string;
+        actorDisplayName: string;
+        timestamp: number;
+        message: string;
+        messageParameters: any[];
+        systemMessage: string;
+        messageType: string;
+        isReplyable: boolean;
+        referenceId: string;
+        reactions: Record<string, unknown>;
+        expirationTimestamp: number;
+    };
+    sipEnabled: number;
+    actorType: string;
+    actorId: string;
+    attendeeId: number;
+    permissions: number;
+    attendeePermissions: number;
+    callPermissions: number;
+    defaultPermissions: number;
+    canEnableSIP: boolean;
+    attendeePin: string;
+    description: string;
+    lastCommonReadMessage: number;
+    listable: number;
+    callFlag: number;
+    messageExpiration: number;
+};
+export const getConversations = async (): Promise<Array<TalkConversation>> => {
   let http = createNextcloudAPI()
   http.defaults.headers['OCS-APIRequest'] = true
 
   const data = await http.get("/ocs/v2.php/apps/spreed/api/v4/room")
-  console.log(data)
+  return data.data.ocs.data
+}
+
+export enum ConversationType {
+  OneToOne,
+  Group,
+  Public,
+  Changelog,
+  //When a user is deleted from the server or removed from all their conversations, 1 "One to one" rooms are converted to this type)
+  FormerOneToOne,
+}
+export type CreateConversationOptions = {
+  roomType: ConversationType;
+  // User ID (roomType = 1), group ID (roomType = 2 - optional), circle ID (roomType = 2, source = 'circles'], only available with circles-support capability)
+  invite?: string;
+  // The source for the invite, only supported on roomType = 2 for groups and circles (only available with circles-support capability)
+  source?: string;
+  // Conversation name up to 255 characters (Not available for roomType = 1)
+  roomName?: string;
+  // Type of an object this room references, currently only allowed value is 'room' to indicate the parent of a breakout room (See Object types)
+  objectType?: string;
+  // ID of an object this room references, room token is used for the parent of a breakout room
+  objectId?: string;
+}
+export const createConversation = async (options: CreateConversationOptions) => {
+  let http = createNextcloudAPI()
+  http.defaults.headers['OCS-APIRequest'] = true
+  return http.post("/ocs/v2.php/apps/spreed/api/v4/room", {...options, roomType: options.roomType + 1 })
+}
+
+
+enum LookIntoFuture {
+  Poll = 1, // Poll and wait for new message
+  GetHistory = 0, // Get history of a conversation
+}
+enum SetReadMarker {
+  Auto = 1, // Automatically set the read timer after fetching the messages
+  Manual = 0, // use Manual when your client calls 'Mark chat as read manually'. (Default: Auto)
+}
+type GetChatMessagesOptions = {
+  lookIntoFuture?: LookIntoFuture;
+  // Number of chat messages to receive (100 by default, 200 at most)
+  limit?: number;
+  // Serves as an offset for the query. The lastKnownMessageId for the next page is available in the X-Chat-Last-Given header.
+  lastKnownMessageId?: number;
+  // Send the last X-Chat-Last-Common-Read header you got if you are interested in updates of the common read value.
+  // A 304 response does not allow custom headers and otherwise the server cannot know if your value is modified or not.
+  lastCommonReadId?: number;
+  // Only applicable when lookIntoFuture = 1. Number of seconds to wait for new messages (30 by default, 60 at most)
+  timeout?: number;
+  // Auto to automatically set the read timer after fetching the messages
+  // use Manual when your client calls 'Mark chat as read manually'. (Default: Auto)
+  setReadMarker?: SetReadMarker;
+  // 1 to include the last known message as well (Default: 0)
+  includeLastKnown?: number;
+  // Set to 1 when the user status should not be automatically set to online (default: 0)
+  noStatusUpdate?: number;
+  // 0 to not mark notifications as read (Default: 1, only available with chat-keep-notifications capability)
+  markNotificationsAsRead?: number;
+};
+export const getChatMessages = async (token: string, options: GetChatMessagesOptions) => {
+  let http = createNextcloudAPI()
+  http.defaults.headers['OCS-APIRequest'] = true
+  return http.get("/ocs/v2.php/apps/spreed/api/v1/chat/" + token, options)
 }
--- frontend/src/features/nextcloud_integration/components/chat-test.tsx
+++ frontend/src/features/nextcloud_integration/components/chat-test.tsx
@@ -1,30 +1,76 @@
 import { useEffect, useState } from "react"
 import SimpleFormInput from "@/components/Form/SimpleFormInput"
 import SimpleButton from "@/components/Button/SimpleButton"
-import { getConversations, sendMessage } from "../api/chat"
+import { getChatMessages, getConversations, sendMessage, TalkConversation } from "../api/chat"
+import { toast } from "react-toastify"
+import ConversationForm from "../api/ConversationForm"
+
 
 /**
  * component used for testing different webdav api call
  */
 export const ChatTest = () => {
   const [message, setMessage] = useState<string>("")
-  const chatToken = ""
+  const [messages, setMessages] = useState<any>()
+  const [conversations, setConversations] = useState<Array<TalkConversation>>([])
+  const [selectedConversation, setSelectedConversation] = useState<TalkConversation>()
+
+  async function fetchConversations(){
+    const conversations = await getConversations()
+    console.log(conversations)
+    setConversations(conversations)
+  }
+
+  async function fetchMessages(){
+    if (selectedConversation){
+      const messages = await getChatMessages(selectedConversation?.token, {})
+      console.log(messages)
+      setMessages(messages)
+    } else {
+      toast("no convo selected", {type: 'error'})
+    }
+  }
+
+  function send(){
+    if(selectedConversation){
+      sendMessage(selectedConversation.token, message)
+      toast(("message: '" + message + "' sent"), {type: 'success'})
+    } else {
+      toast("no convo selected", {type: 'error'})
+    }
+  }
 
   useEffect(() => {
-    getConversations()
+    fetchMessages()
+  }, [selectedConversation])
+
+  useEffect(() => {
+    fetchConversations()
   }, [])
 
   return <div className="flex flex-col justify-center items-center gap-4 mt-8">
-    {/* upload an image to path */}
+    <h2>conversations</h2>
+    <ul>
+      {conversations.map(item => {
+        return <li className="cursor-pointer hover:text-primary-400" key={item.id} onClick={() => setSelectedConversation(item)}>{item.displayName}</li>
+      })}
+    </ul>
+    <div className="h-[1px] w-[70%] bg-neutral-500"></div>
+    <h2>send message</h2>
     <div className="flex flex-col  gap-4 w-[50%]">
       <SimpleFormInput
         id="message"
-        labelText="write a message"
+        labelText={"write a message to " + (selectedConversation ? selectedConversation.displayName : "")}
         placeHolder="message..."
         onChange={(e) => setMessage(e.target.value)}
         isArea={true}
       ></SimpleFormInput>
-      <SimpleButton onClick={() => sendMessage(chatToken, message)}>send</SimpleButton>
+      <SimpleButton onClick={send}>send</SimpleButton>
+    </div>
+    <div className="h-[1px] w-[70%] bg-neutral-500"></div>
+    <div>
+      <h2 className="mb-2">Create conversation</h2>
+      <ConversationForm></ConversationForm>
     </div>
   </div>
 
