ecb7b380f76a7955d70555f945eb2bcc5260ea14
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-05-28 12:50:45 +0200

    base layer: remove useImage.

--- frontend/src/features/map_planning/layers/BaseLayer.tsx
+++ frontend/src/features/map_planning/layers/BaseLayer.tsx
@@ -1,18 +1,15 @@
 import { MAP_PIXELS_PER_METER } from '../utils/Constants';
 import { Layer, Image } from 'react-konva';
-import useImage from 'use-image';
 
 interface BaseLayerProps {
   opacity: number;
   visible: boolean;
-  imageUrl: string;
+  image: HTMLImageElement | undefined;
   pixels_per_meter: number;
   rotation: number;
 }
 
-const BaseLayer = ({ visible, opacity, imageUrl, pixels_per_meter, rotation }: BaseLayerProps) => {
-  const [image] = useImage(imageUrl);
-
+const BaseLayer = ({ visible, opacity, image, pixels_per_meter, rotation }: BaseLayerProps) => {
   const scale = pixels_per_meter / MAP_PIXELS_PER_METER;
   const width = image?.width ?? 0;
   const height = image?.height ?? 0;
