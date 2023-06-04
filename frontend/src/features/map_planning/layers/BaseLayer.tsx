c347f05e8e40752fadd01689318e56d247e94777
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-06-04 18:43:09 +0200

    base layer: use nextcloud instead of arbitrary image sources

--- frontend/src/features/map_planning/layers/BaseLayer.tsx
+++ frontend/src/features/map_planning/layers/BaseLayer.tsx
@@ -1,22 +1,29 @@
 import { MAP_PIXELS_PER_METER } from '../utils/Constants';
 import { Layer, Image } from 'react-konva';
-import useImage from "use-image";
+import { createNextcloudWebDavClient } from '@/config/nextcloud_client';
+import { useQuery } from '@tanstack/react-query';
+import {ImageBlob} from '../../nextcloud_integration/components/ImageBlob';
 
 interface BaseLayerProps {
   opacity: number;
   visible: boolean;
-  imageURL: string;
+  nextcloudImagePath: string;
   pixels_per_meter: number;
   rotation: number;
 }
 
-const BaseLayer = ({ visible, opacity, imageURL, pixels_per_meter, rotation }: BaseLayerProps) => {
-  const [image] = useImage(imageURL);
+const BaseLayer = ({ visible, opacity, nextcloudImagePath, pixels_per_meter, rotation }: BaseLayerProps) => {
+  const webdav = createNextcloudWebDavClient();
+  const imagepath = `/remote.php/webdav/${nextcloudImagePath};` 
+  const imageData = useQuery(['files', imagepath], () => webdav.getFileContents(imagepath));
+  const image = new HTMLImageElement();
+  image.src = URL.createObjectURL(new Blob([imageData?.data as BlobPart]));
+
   const scale = pixels_per_meter / MAP_PIXELS_PER_METER;
   const width = image?.width ?? 0;
   const height = image?.height ?? 0;
-
-  return (
+  
+    return (
     <Layer listening={false} visible={visible} opacity={opacity} offset={{ x: -width, y: -height }}>
       <Image
         image={image}
