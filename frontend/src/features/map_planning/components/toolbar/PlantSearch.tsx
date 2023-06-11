3cce294c630d43c93e23f74a894edb3c24f0f858
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-06-11 15:15:27 +0200

    plant search: refactor hook calls due to linter warnings.

--- frontend/src/features/map_planning/components/toolbar/PlantSearch.tsx
+++ frontend/src/features/map_planning/components/toolbar/PlantSearch.tsx
@@ -4,7 +4,7 @@ import { searchPlants } from '@/features/seeds/api/searchPlants';
 import { ReactComponent as PlantIcon } from '@/icons/plant.svg';
 import { ReactComponent as SearchIcon } from '@/icons/search.svg';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 /** UI component intended for searching plants that can be drag and dropped to the plants layer */
@@ -12,38 +12,53 @@ export const PlantSearch = () => {
   const { t } = useTranslation(['plantSearch']);
 
   const [plants, setPlants] = useState<string[]>([]);
-  const [searchTerm, setSearchTerm]   = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   // Keep track of which search page should be requested next.
   // NOTE: pages start at index 1.
-  const [nextPage, setNextPage]       = useState(1);
+  const [nextPage, setNextPage] = useState(1);
   // How far the user has scrolled in the search result list.
-  const [searchScrollPos, setSearchScrollPos]   = useState(1);
+  const [searchScrollPos, setSearchScrollPos] = useState(1);
   const [searchVisible, setSearchVisible] = useState(false);
-  
+
   const searchResultsRef = useRef<HTMLUListElement>(null);
   const searchInputRef = useRef<HTMLInputElement>(null);
 
+  const loadPage = useCallback(
+    async (pageNum: number) => {
+      const page = await searchPlants(searchTerm, pageNum);
+
+      if (pageNum > page.total_pages) {
+        return;
+      }
+
+      // Display plants using the following format:
+      // <unique name> (<common name>)
+      const newPlants: string[] = page.results.map((plant) => {
+        const common_name_en =
+          plant.common_name_en != null ? ' (' + plant.common_name_en[0] + ')' : '';
+
+        return plant.unique_name + common_name_en;
+      });
+
+      // Loading the first page of a search query indicates, that the entire
+      // results array has to be overwritten.
+      // All other pages should just be appended.
+      setPlants((currPlants) => (pageNum == 1 ? newPlants : [...currPlants, ...newPlants]));
+    },
+    [searchTerm],
+  );
+
   useEffect(() => {
     searchInputRef.current?.focus();
-    
-    // Load plants even if the user didn't make any inputs.
-    const loadInitalPage = setTimeout(() => {
-      loadPage(1);
-      setNextPage(2);
-    }, 300);
 
-    return () => clearTimeout(loadInitalPage);
-  }, [searchVisible]);
-  
+    // Load plants even if the user didn't make any inputs.
+    setSearchTerm('');
+    setNextPage(1);
+  }, [searchVisible, loadPage]);
+
   // If the user inputs a new search term, the frontend should display the first page of the search request.
   useEffect(() => {
-    // Debounce plant search.
-    const loadInitalPage = setTimeout(() => {
-      loadPage(1);
-      setNextPage(2);
-    }, 300);
-
-    return () => clearTimeout(loadInitalPage);
+    setNextPage(1);
   }, [searchTerm]);
 
   // When the user scrolls past a certain threshold with a new page is supposed to be loaded.
@@ -52,61 +67,36 @@ export const PlantSearch = () => {
     const offsetHeight = searchResultsRef.current?.offsetHeight;
 
     // Values may be undefined before the user clicked the search button.
-    if (scrollHeight === undefined || offsetHeight === undefined)
-      return;
+    if (scrollHeight === undefined || offsetHeight === undefined) return;
 
     // Start loading the next page 100 pixels before the actual bottom of the page
     // to allow for smoother page transitions.
     const pageLoadThreshold = scrollHeight - offsetHeight - 100;
-    if (searchScrollPos < pageLoadThreshold)
-      return;
+    if (searchScrollPos < pageLoadThreshold) return;
 
+    setNextPage((curr) => curr + 1);
+  }, [searchScrollPos]);
+
+  useEffect(() => {
     // Debounce page fetching
-    const loadInitalPage = setTimeout(() => {
-      loadPage(nextPage + 1);
+    const loadInitialPage = setTimeout(() => {
+      loadPage(nextPage);
     }, 300);
 
-    return () => {
-      clearTimeout(loadInitalPage);
-      // Only load the next page if the current page has been loaded successfully.
-      setNextPage(nextPage + 1);
-    };
-  }, [searchScrollPos]);
-  
-  // Callback that recalculates the scroll position after the 
+    return () => clearTimeout(loadInitialPage);
+  }, [nextPage, loadPage]);
+
+  // Callback that recalculates the scroll position after the
   // user made a scroll input.
   const onSearchResultsScroll = () => {
     const scrollOffset = searchResultsRef.current?.scrollTop;
-    if (scrollOffset === undefined)
-      throw new Error('Undefined search result list properties.'); 
+    if (scrollOffset === undefined) throw new Error('Undefined search result list properties.');
 
     setSearchScrollPos(scrollOffset);
   };
-   
-  const loadPage = async (pageNum: number) => {
-    const page = await searchPlants(searchTerm, nextPage);
-    
-    if (nextPage > page.total_pages) {
-        return;
-    }
 
-    // Display plants using the following format:
-    // <unique name> (<common name>)
-    const newPlants: string[] = page.results.map((plant) => {
-      const common_name_en =
-        plant.common_name_en != null ? ' (' + plant.common_name_en[0] + ')' : '';
-
-      return plant.unique_name + common_name_en;
-    });
-    
-    // Loading the first page of a search query indicates, that the entire
-    // results array has to be overwritten.
-    // All other pages should just be appended.
-    setPlants(pageNum == 1 ? newPlants : [...plants, ...newPlants]);
-  }
-  
   return (
-    <div className="flex flex-col gap-4 p-2 h-full overflow-clip fixed">
+    <div className="fixed flex h-full flex-col gap-4 overflow-clip p-2">
       <div className="flex items-center justify-between">
         <h2>{t('plantSearch:dnd')}</h2>
         {!searchVisible && (
@@ -143,9 +133,10 @@ export const PlantSearch = () => {
                 if (e.key === 'Escape') setSearchVisible(false);
               }}
             ></SearchInput>
-            <ul onScroll={onSearchResultsScroll}
-                ref={searchResultsRef}
-                className="overflow-y-scroll h-full absolute"
+            <ul
+              onScroll={onSearchResultsScroll}
+              ref={searchResultsRef}
+              className="absolute h-full overflow-y-scroll"
             >
               {plants.map((plant) => (
                 <li
