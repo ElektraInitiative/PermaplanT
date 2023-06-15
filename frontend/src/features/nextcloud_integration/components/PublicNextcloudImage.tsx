89e7e8e344d88d761b8226ff23f62f59cdc2c6b7
Samuel <absurd.turtl3@gmail.com>
2023-06-15 15:50:51 +0200

    added docs

--- frontend/src/features/nextcloud_integration/components/PublicNextcloudImage.tsx
+++ frontend/src/features/nextcloud_integration/components/PublicNextcloudImage.tsx
@@ -16,6 +16,8 @@ interface PublicNextcloudImageProps
 
 /**
  * Component used for fetching and displaying images from Nextcloud public share directories
+  @param props.path: relative path starting at the public share directory to the image in Nextcloud
+  @param props.shareToken: token which identifies the public share directory
  */
 export const PublicNextcloudImage = (props: PublicNextcloudImageProps) => {
   const { path, shareToken, ...imageProps } = props;
