a736833481947a303443c6edb943745963b395d7
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-06-19 17:11:06 +0200

    base layer: use separate module for loading images.

--- frontend/src/features/map_planning/layers/base/BaseLayer.tsx
+++ frontend/src/features/map_planning/layers/base/BaseLayer.tsx
@@ -1,12 +1,9 @@
 import { MAP_PIXELS_PER_METER } from '../../utils/Constants';
-import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { NextcloudKonvaImage } from '@/features/map_planning/components/NextcloudKonvaImage';
 import Konva from 'konva';
-import { useRef } from 'react';
-import { useTranslation } from 'react-i18next';
-import { Layer, Image } from 'react-konva';
-import { toast } from 'react-toastify';
-import { FileStat, ResponseDataDetailed, WebDAVClient } from 'webdav';
-import Vector2d = Konva.Vector2d;
+import { useState } from 'react';
+import { Layer } from 'react-konva';
+import { WebDAVClient } from 'webdav';
 
 interface BaseLayerProps extends Konva.LayerConfig {
   /**
@@ -33,6 +30,7 @@ interface BaseLayerProps extends Konva.LayerConfig {
  *
  * @param imageStat file stats returned by nextcloud.
  */
+/*
 const checkFileIsImage = (
   imageStat: UseQueryResult<FileStat | ResponseDataDetailed<FileStat>, unknown>,
 ): boolean => {
@@ -41,76 +39,43 @@ const checkFileIsImage = (
   const stat = imageStat.data as FileStat;
   return stat.type === 'file' && (stat.mime?.startsWith('image') ?? false);
 };
+*/
 
 const BaseLayer = ({
-  nextcloudClient,
   visible,
   opacity,
   nextcloudImagePath,
   pixelsPerMeter,
   rotation,
 }: BaseLayerProps) => {
-  const { t } = useTranslation(['baseLayer']);
-
   // It shouldn't matter whether the image path starts with a slash or not.
   let cleanImagePath = nextcloudImagePath;
   if (cleanImagePath.startsWith('/')) {
     cleanImagePath = cleanImagePath.substring(1);
   }
 
-  const imageURLPath = `/remote.php/webdav/${cleanImagePath ?? ''}`;
-
-  // TODO: remove after debugging Nextcloud issues
-  console.log('Nextcloud URI: ', imageURLPath);
-
   // Make sure that the image is centered on, and rotates around, the origin.
-  const imageOffset = useRef<Vector2d>({x: 0, y: 0});
-  const layerOffset = useRef<Vector2d>({x: 0, y: 0});
+  const [imageOffset, setImageOffset] = useState({ x: 0, y: 0 });
 
-  // Hooks have to be called an equal number of times on each render.
-  // We therefore have to check whether a file is a valid image after loading it.
-  const fileStat = useQuery(['stat', imageURLPath], () =>
-    nextcloudClient.stat(imageURLPath, { details: false }),
-  );
-  const imageData = useQuery(['files', imageURLPath], () =>
-    nextcloudClient.getFileContents(imageURLPath),
-  );
-
-  if (imageData.isLoading) {
-    return <Layer></Layer>;
-  }
-
-  if (imageData.isError) {
-    console.log('Data undefined.', nextcloudImagePath);
-    if (nextcloudImagePath != '') toast.error(t('baseLayer:loadingFailed') + imageData.error);
-    return <Layer></Layer>;
-  }
-
-  if (!checkFileIsImage(fileStat)) {
-    console.log('Not an image', nextcloudImagePath);
-    if (nextcloudImagePath != '') toast.error(t('baseLayer:notAnImage'));
-    return <Layer></Layer>;
-  }
-
-  const image = new window.Image();
-  image.src = URL.createObjectURL(new Blob([imageData.data as BlobPart]));
-  // Width and height are only defined after the browser has finished loading the image.
-  image.onload = () => {
-    imageOffset.current = {x: image.width / 2, y: image.height / 2};
-    layerOffset.current = {x: -image.width / 2, y: -image.height / 2};
+  const onload = (image: HTMLImageElement) => {
+    if (imageOffset.x !== image.width / 2 || imageOffset.y !== image.height / 2)
+      setImageOffset({ x: image.width / 2, y: image.height / 2 });
   };
 
   const scale = pixelsPerMeter / MAP_PIXELS_PER_METER;
 
   return (
-    <Layer listening={false} visible={visible} opacity={opacity} offset={layerOffset.current}>
-      <Image
-        image={image}
-        rotation={rotation}
-        scaleX={scale}
-        scaleY={scale}
-        offset={imageOffset.current}
-      />
+    <Layer listening={false} visible={visible} opacity={opacity}>
+      {cleanImagePath && (
+        <NextcloudKonvaImage
+          path={cleanImagePath}
+          onload={onload}
+          rotation={rotation ?? 0}
+          scaleX={scale}
+          scaleY={scale}
+          offset={imageOffset}
+        />
+      )}
     </Layer>
   );
 };
