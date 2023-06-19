2a648f7a6631bda0c80a4f3a193e39961859c605
Samuel <absurd.turtl3@gmail.com>
2023-06-19 18:20:58 +0200

    added docs

--- frontend/src/features/map_planning/components/NextcloudKonvaImage.tsx
+++ frontend/src/features/map_planning/components/NextcloudKonvaImage.tsx
@@ -9,12 +9,24 @@ import { toast } from 'react-toastify';
 interface NextcloudKonvaImageProps extends ShapeConfig {
   // relative path starting at the users Nextcloud root directory
   path: string;
+  // crop the image with given rectangle
   crop?: IRect;
+  // corner radius of the image
   cornerRadius?: number | number[];
+  // Fires immediately after the browser loads the image object.
   onload?: (ncImage: HTMLImageElement) => void;
 }
 
 const WEBDAV_PATH = '/remote.php/webdav/';
+
+
+/** loads an image from Nextcloud and returns a KonvaImage shape on success
+  * @param props.path relative path starting at the users Nextcloud root directory
+  * @param props.crop crop the image with given rectangle
+  * @param props.cornerRadius corner radius of the image
+  * @param props.onload Fires immediately after the browser loads the image object.
+  * @returns a KonvaNodeComponent which is either the requested image or a rectangle of size 0 in case an error occurs
+  **/
 export const NextcloudKonvaImage = (props: NextcloudKonvaImageProps) => {
   const { t } = useTranslation(['nextcloudIntegration']);
   const { path, ...imageProps } = props;
