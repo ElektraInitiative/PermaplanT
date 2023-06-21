9c0a41706da8fa26c13117c94d875ec7930c61ed
Samuel <absurd.turtl3@gmail.com>
2023-06-21 19:30:08 +0200

    added description

--- frontend/src/features/landing_page/components/PhotoGallery.tsx
+++ frontend/src/features/landing_page/components/PhotoGallery.tsx
@@ -9,6 +9,8 @@ import { ReactComponent as ChevronRightIcon } from '@/icons/chevron-right.svg';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
+
+/** Component used for the landing page to show photos from the PermaplanT public Nextcloud directory */
 export const PhotoGallery = () => {
   // Nextcloud public share token for the gallery
   // used to fetch ressources from 'https://cloud.permaplant.net/s/qo6mZwPg6kFTmmj'
