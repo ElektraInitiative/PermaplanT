a9ba546011bf772d98d3439f621fd331b9a50f8f
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-06-22 22:27:28 +0200

    base layer: enable scaling

--- frontend/src/features/map_planning/layers/base/components/BaseLayerRightToolbar.tsx
+++ frontend/src/features/map_planning/layers/base/components/BaseLayerRightToolbar.tsx
@@ -4,6 +4,20 @@ import SimpleFormInput from '@/components/Form/SimpleFormInput';
 import useMapStore from '@/features/map_planning/store/MapStore';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import ModalContainer from '@/components/Modals/ModalContainer';
+import { Vector2d } from 'konva/lib/types';
+import { toast } from 'react-toastify';
+import { MAP_PIXELS_PER_METER } from '@/features/map_planning/utils/Constants';
+
+const calculateScale = (measuredDistancePixels: number, actualDistanceCentimeters: number): number => {
+  return measuredDistancePixels / actualDistanceCentimeters * MAP_PIXELS_PER_METER;
+};
+
+const calculateMeasredDistance = (point1: Vector2d, point2: Vector2d) => {
+  const lengthX = Math.abs(point2.x - point1.x); 
+  const lengthY = Math.abs(point2.y - point1.y); 
+  return Math.sqrt(lengthX * lengthX + lengthY * lengthY);
+};
 
 const BaseLayerRightToolbar = () => {
   const trackedState = useMapStore((state) => state.trackedState.layers.Base);
@@ -15,15 +29,73 @@ const BaseLayerRightToolbar = () => {
   const { t } = useTranslation(['common', 'baseLayerForm']);
 
   const [rotationInput, setRotationInput] = useState(trackedState.rotation);
+  const [scaleInput, setScaleInput] = useState(trackedState.scale);
   const [pathInput, setPathInput] = useState(trackedState.nextcloudImagePath);
 
   useEffect(() => {
     setRotationInput(trackedState.rotation);
+    setScaleInput(trackedState.scale);
     setPathInput(trackedState.nextcloudImagePath);
-  }, [trackedState.nextcloudImagePath, trackedState.rotation]);
+  }, [trackedState.nextcloudImagePath, trackedState.scale, trackedState.rotation]);
+
+  const [distMeters, setDistMeters] = useState(0);
+  const [distCentimeters, setDistCentimeters] = useState(0);
+
+  const onDistModalSubmit = () => {
+    console.assert(untrackedState.measurePoint1 !== null);
+    console.assert(untrackedState.measurePoint2 !== null);
+    
+    const point1 = untrackedState.measurePoint1 ?? {x: 0, y: 0};
+    const point2 = untrackedState.measurePoint2 ?? {x: 0, y: 0};
+     
+    const measuredDistance = calculateMeasredDistance(point1, point2);
+    const actualDistance = distMeters * 100 + distCentimeters;
+    if (actualDistance === 0) {
+      toast.error(t('baseLayerForm:error_actual_distance_zero'));
+      return;
+    } 
+    setScaleInput(calculateScale(measuredDistance, actualDistance));
+    deactivateMeasurement();
+  } 
 
   return (
     <div className="flex flex-col gap-2 p-2">
+      <ModalContainer show={untrackedState.measureStep === 'both selected'}>
+        <div className="flex flex-col gap-2 p-2">
+          <h3>{t('baseLayerForm:distance_modal_title')}</h3>
+          <div className="flex items-end flex-row gap-2">
+            <SimpleFormInput
+              id="dist_meters"
+              labelText={t('common:meters')}
+              onChange={(e) => setDistMeters(parseInt(e.target.value))}
+              type="number"
+              value={distMeters}
+              min="0"
+            />
+            <SimpleFormInput
+              id="dist_centimeters"
+              labelText={t('common:meters')}
+              onChange={(e) => setDistCentimeters(parseInt(e.target.value))}
+              type="number"
+              value={distCentimeters}
+              min="0"
+              max="99"
+            />
+          </div>
+          <div className="flex items-end flex-row gap-2">
+            <SimpleButton
+              onClick={() => deactivateMeasurement()}
+            >
+              {t('common:cancel')}
+            </SimpleButton>
+            <SimpleButton
+              onClick={() => onDistModalSubmit()}
+            >
+              {t('common:ok')}
+            </SimpleButton>
+          </div>
+        </div> 
+      </ModalContainer>
       <h2>{t('baseLayerForm:title')}</h2>
       <SimpleFormInput
         id="file"
@@ -40,20 +112,30 @@ const BaseLayerRightToolbar = () => {
         min="0"
         max="359"
       />
+      <div className="flex items-end flex-row gap-2">
+        <SimpleFormInput
+          id="scale"
+          labelText={t('baseLayerForm:scale')}
+          onChange={(e) => setScaleInput(parseInt(e.target.value))}
+          type="number"
+          value={scaleInput}
+          min="0"
+        />
+        <SimpleButton
+          onClick={() =>
+            untrackedState.measureStep === 'inactive'
+              ? activateMeasurement()
+              : deactivateMeasurement()
+          }
+        >
+          {untrackedState.measureStep === 'inactive'
+            ? t('baseLayerForm:set_scale')
+            : t('common:cancel')}
+        </SimpleButton>
+      </div>
       <SimpleButton
         onClick={() =>
-          untrackedState.measureStep === 'inactive'
-            ? activateMeasurement()
-            : deactivateMeasurement()
-        }
-      >
-        {untrackedState.measureStep === 'inactive'
-          ? t('baseLayerForm:set_scale')
-          : t('common:cancel')}
-      </SimpleButton>
-      <SimpleButton
-        onClick={() =>
-          executeAction(new UpdateBaseLayerAction(rotationInput, trackedState.scale, pathInput))
+          executeAction(new UpdateBaseLayerAction(rotationInput, scaleInput, pathInput))
         }
       >
         {t('common:apply')}
