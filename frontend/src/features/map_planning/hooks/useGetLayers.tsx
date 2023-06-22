59b57e6cf2bb22732923915508f17ae5734f1dd8
Bushuo <me@paulbuschmann.com>
2023-06-22 14:49:31 +0200

    refactor: add constant

--- frontend/src/features/map_planning/hooks/useGetLayers.tsx
+++ frontend/src/features/map_planning/hooks/useGetLayers.tsx
@@ -1,14 +1,16 @@
 import { getLayers } from '../api/getLayers';
 import { useQuery } from '@tanstack/react-query';
 
+const TEN_MINUTES = 1000 * 60 * 10;
+
 /**
  * Gets all layers for the given map id.
  * Re-fetches data only after a certain amount of time to reduce network stress.
  */
 export function useGetLayers(mapId: number) {
-  const data = useQuery(['layers', mapId] as const, {
-    queryFn: ({ queryKey: [, mapId] }) => getLayers(mapId),
-    staleTime: 1000 * 60 * 10, // 10 minutes
+  const data = useQuery(['layers', mapId], {
+    queryFn: () => getLayers(mapId),
+    staleTime: TEN_MINUTES,
   });
 
   return data;
