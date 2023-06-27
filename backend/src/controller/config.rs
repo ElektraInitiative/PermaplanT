96c5c18c96b57c2696e2bfe77d4faa73a1279895
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-06-27 10:44:21 +0200

    return cargo.toml version number from config endpoint

--- backend/src/controller/config.rs
+++ backend/src/controller/config.rs
@@ -19,7 +19,7 @@ pub async fn get() -> HttpResponse {
     let response = ConfigDto {
         issuer_uri: config.openid_configuration.issuer.clone(),
         client_id: config.client_id.clone(),
-        version: 0,
+        version: env!("CARGO_PKG_VERSION").to_owned(),
     };
     HttpResponse::Ok().json(response)
 }
