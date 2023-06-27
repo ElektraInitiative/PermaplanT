95caab57105a483287d853398742e446421cd486
Bushuo <me@paulbuschmann.com>
2023-06-27 14:14:34 +0200

    fix: linting error in webdav client hook

--- frontend/src/config/nextcloud_client/index.ts
+++ frontend/src/config/nextcloud_client/index.ts
@@ -1,27 +1,31 @@
 import { nextcloudUri } from '../env';
-import { AuthType, createClient, WebDAVClient } from 'webdav';
 import { useSafeAuth } from '@/hooks/useSafeAuth';
 import { useEffect, useState } from 'react';
+import { AuthType, createClient, WebDAVClient } from 'webdav';
 
 /**
  * create a webdav client configured for the PermaplanT Nextcloud instance
  * the configuration includes the baseUrl and authorization token if available
  */
 export function useNextcloudWebDavClient() {
-  const auth = useSafeAuth()
-  const [webdav, setWebdav] = useState<WebDAVClient | null>(null)
+  const auth = useSafeAuth();
+  const [webdav, setWebdav] = useState<WebDAVClient | null>(null);
+
   useEffect(() => {
-    if(!auth || !auth.user){
-      return
+    if (!auth || !auth.user) {
+      return;
     }
-    setWebdav(createClient(nextcloudUri, {
-      authType: AuthType.Token,
-      token: {
-        access_token: auth.user?.access_token,
-        token_type: auth.user?.token_type,
-        refresh_token: auth.user?.refresh_token,
-      },
-    }));
-  }, [auth.user])
-  return webdav
+
+    setWebdav(
+      createClient(nextcloudUri, {
+        authType: AuthType.Token,
+        token: {
+          access_token: auth.user?.access_token,
+          token_type: auth.user?.token_type,
+          refresh_token: auth.user?.refresh_token,
+        },
+      }),
+    );
+  }, [auth, auth.user]);
+  return webdav;
 }
