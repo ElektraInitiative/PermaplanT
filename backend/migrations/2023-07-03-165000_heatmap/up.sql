c839a35ed5d10010b23e024ee6ca4d8cee34f9f7
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-07-08 20:32:06 +0200

    refactor SQL queries and add better doc

--- backend/migrations/2023-07-03-165000_heatmap/up.sql
+++ backend/migrations/2023-07-03-165000_heatmap/up.sql
@@ -1,62 +1,91 @@
 -- Your SQL goes here
 ALTER TABLE maps ADD COLUMN geometry GEOMETRY(POLYGON, 4326) NOT NULL;
 
--- Scales the input to values 0-1.
-CREATE OR REPLACE FUNCTION scale_input(input REAL)
-RETURNS REAL AS $$
-BEGIN
-    IF input > 1 THEN
-        RETURN 1;
-    ELSIF input < 0 THEN
-        RETURN 0;
-    ELSE
-        RETURN input;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
 
 -- Calculate the bounding box of the map geometry.
 CREATE OR REPLACE FUNCTION calculate_bbox(map_id INTEGER)
 RETURNS TABLE(x_min REAL, y_min REAL, x_max REAL, y_max REAL) AS $$
-DECLARE
-    map_geometry GEOMETRY(POLYGON, 4326);
-    bbox GEOMETRY;
 BEGIN
-    SELECT geometry FROM maps WHERE id = map_id INTO STRICT map_geometry;
-
-    -- Get the bounding box of the map's geometry
-    bbox := ST_Envelope(map_geometry);
-
-    -- Return bounding box information
-    x_min := ST_XMin(bbox);
-    y_min := ST_YMin(bbox);
-    x_max := ST_XMax(bbox);
-    y_max := ST_YMax(bbox);
-
-    RETURN NEXT;
+    RETURN QUERY
+    SELECT
+        CAST(ST_XMin(bbox) AS REAL) AS x_min,
+        CAST(ST_YMin(bbox) AS REAL) AS y_min,
+        CAST(ST_XMax(bbox) AS REAL) AS x_max,
+        CAST(ST_YMax(bbox) AS REAL) AS y_max
+    FROM (
+        SELECT
+            ST_Envelope(geometry) AS bbox
+        FROM maps
+        WHERE id = map_id
+    ) AS subquery;
 END;
 $$ LANGUAGE plpgsql;
 
--- Get all relations for the plant on the specified layer.
-CREATE OR REPLACE FUNCTION get_plant_relations(p_layer_id INTEGER, p_plant_id INTEGER)
-RETURNS TABLE(x INTEGER, y INTEGER, relation relation_type) AS $$
+
+-- Returns scores from 0-1 for each pixel of the map. It should never return values greater than 1.
+-- Values where the plant should not be placed are close to 0.
+-- Values where the plant should be placed are close to 1.
+-- The score at position x=0,y=0 is at the position x=x_min,y=y_min.
+CREATE OR REPLACE FUNCTION calculate_score(p_map_id INTEGER, p_layer_id INTEGER, p_plant_id INTEGER, granularity REAL, x_min REAL, y_min REAL, x_max REAL, y_max REAL)
+RETURNS TABLE(score REAL, x INTEGER, y INTEGER) AS $$
+DECLARE
+    map_geometry GEOMETRY(POLYGON, 4326);
+    cell GEOMETRY;
+    bbox GEOMETRY;
+    num_cols INTEGER;
+    num_rows INTEGER;
+    x_pos REAL;
+    y_pos REAL;
+    plant_relation RECORD;
 BEGIN
-    RETURN QUERY
-    SELECT plantings.x, plantings.y, relations.relation
-    FROM plantings
-    JOIN plants ON plantings.plant_id = plants.id
-    JOIN (
-        SELECT plant1 AS plant, r1.relation
-        FROM relations r1
-        WHERE plant2 = p_plant_id
-            AND r1.relation != 'neutral'
-        UNION
-        SELECT plant2 AS plant, r2.relation
-        FROM relations r2
-        WHERE plant1 = p_plant_id
-            AND r2.relation != 'neutral'
-    ) relations ON plants.id = relations.plant
-    WHERE plantings.layer_id = p_layer_id;
+    -- Makes sure the plant layer exists and fits to the map
+    IF NOT EXISTS (SELECT 1 FROM layers WHERE id = p_layer_id AND type = 'plants' AND map_id = p_map_id) THEN
+        RAISE EXCEPTION 'Plant Layer with id % not found', p_layer_id;
+    END IF;
+    -- Makes sure the plant exists
+    IF NOT EXISTS (SELECT 1 FROM plants WHERE id = p_plant_id) THEN
+        RAISE EXCEPTION 'Plant with id % not found', p_plant_id;
+    END IF;
+
+    -- INTO STRICT makes sure the map exists. Does have to be explicitly checked as bounding box calculation would error anyways.
+    SELECT geometry FROM maps WHERE id = p_map_id INTO STRICT map_geometry;
+
+    -- Calculate the number of rows and columns based on the map's size and granularity
+    num_cols := CEIL((x_max - x_min) / granularity); -- Adjusted for granularity
+    num_rows := CEIL((y_max - y_min) / granularity); -- Adjusted for granularity
+
+    -- Calculate the score for each pixel on the heatmap
+    FOR i IN 0..num_cols-1 LOOP
+        FOR j IN 0..num_rows-1 LOOP
+            -- i and j do not represent coordinates. We need to adjust them to actual coordinates.
+            x_pos := x_min + (i * granularity);
+            y_pos := y_min + (j * granularity);
+
+            -- Make a square the same size as the granularity
+            cell := ST_Translate(ST_GeomFromEWKT('SRID=4326;POLYGON((0 0, ' || granularity || ' 0, ' || granularity || ' ' || granularity || ', 0 ' || granularity || ', 0 0))'), x_pos, y_pos);
+
+            -- If the square is on the map calculate a score; otherwise set score to 0.
+            IF ST_Intersects(cell, map_geometry) THEN
+                score := 0.5 + calculate_score_from_relations(p_layer_id, p_plant_id, x_pos, y_pos);
+                score := scale_score(score);  -- scale the score to be between 0 and 1
+            ELSE
+                score := 0.0;
+            END IF;
+
+            x := i;
+            y := j;
+
+            RETURN NEXT;
+        END LOOP;
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Scales the score to values 0-1.
+CREATE OR REPLACE FUNCTION scale_score(input REAL)
+RETURNS REAL AS $$
+BEGIN
+    RETURN LEAST(GREATEST(input, 0), 1);
 END;
 $$ LANGUAGE plpgsql;
 
@@ -88,49 +117,27 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
--- Returns scores from 0-1 for each pixel of the map. It should never return values greater than 1.
--- Values where the plant should not be placed are close to 0.
--- Values where the plant should be placed are close to 1.
--- The score at position x=0,y=0 is at the position x=x_min,y=y_min.
-CREATE OR REPLACE FUNCTION calculate_score(map_id INTEGER, p_layer_id INTEGER, p_plant_id INTEGER, granularity REAL, x_min REAL, y_min REAL, x_max REAL, y_max REAL)
-RETURNS TABLE(score REAL, x INTEGER, y INTEGER) AS $$
-DECLARE
-    map_geometry GEOMETRY(POLYGON, 4326);
-    cell GEOMETRY;
-    bbox GEOMETRY;
-    num_cols INTEGER;
-    num_rows INTEGER;
-    x_pos REAL;
-    y_pos REAL;
-    plant_relation RECORD;
+-- Get all relations for the plant on the specified layer.
+CREATE OR REPLACE FUNCTION get_plant_relations(p_layer_id INTEGER, p_plant_id INTEGER)
+RETURNS TABLE(x INTEGER, y INTEGER, relation relation_type) AS $$
 BEGIN
-    SELECT geometry FROM maps WHERE id = map_id INTO STRICT map_geometry;
-
-    -- Calculate the number of rows and columns based on the map's size and granularity
-    num_cols := CEIL((x_max - x_min) / granularity); -- Adjusted for granularity
-    num_rows := CEIL((y_max - y_min) / granularity); -- Adjusted for granularity
-
-    FOR i IN 0..num_cols-1 LOOP
-        FOR j IN 0..num_rows-1 LOOP
-            -- i and j do not represent coordinates. We still need to adjust them to actual coordinates.
-            x_pos := x_min + (i * granularity);
-            y_pos := y_min + (j * granularity);
-
-            cell := ST_Translate(ST_GeomFromEWKT('SRID=4326;POLYGON((0 0, ' || granularity || ' 0, ' || granularity || ' ' || granularity || ', 0 ' || granularity || ', 0 0))'), x_pos, y_pos);
-
-            IF ST_Intersects(cell, map_geometry) THEN
-                score := 0.5 + calculate_score_from_relations(p_layer_id, p_plant_id, x_pos, y_pos);
-                score := scale_input(score);  -- scale the score to be between 0 and 1
-            ELSE
-                score := 0.0;
-            END IF;
-
-            x := i;
-            y := j;
-
-            RETURN NEXT;
-        END LOOP;
-    END LOOP;
+    RETURN QUERY
+        -- We only need x,y and type of relation to calculate a score.
+        SELECT plantings.x, plantings.y, relations.relation
+        FROM plantings
+        JOIN plants ON plantings.plant_id = plants.id
+        JOIN (
+            -- We need UNION as the relation is bidirectional.
+            SELECT plant1 AS plant, r1.relation
+            FROM relations r1
+            WHERE plant2 = p_plant_id
+                AND r1.relation != 'neutral'
+            UNION
+            SELECT plant2 AS plant, r2.relation
+            FROM relations r2
+            WHERE plant1 = p_plant_id
+                AND r2.relation != 'neutral'
+        ) relations ON plants.id = relations.plant
+        WHERE plantings.layer_id = p_layer_id;
 END;
 $$ LANGUAGE plpgsql;
