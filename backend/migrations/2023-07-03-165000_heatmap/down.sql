fd28fba7475fe1627baf78f3f8df21209c19e0dd
kitzbergerg <gabriel.kitzberger@gmail.com>
2023-07-09 13:06:57 +0200

    adjust heatmap down.sql

--- backend/migrations/2023-07-03-165000_heatmap/down.sql
+++ backend/migrations/2023-07-03-165000_heatmap/down.sql
@@ -1,3 +1,7 @@
 -- This file should undo anything in `up.sql`
+DROP FUNCTION get_plant_relations;
+DROP FUNCTION calculate_score_from_relations;
+DROP FUNCTION scale_score;
 DROP FUNCTION calculate_score;
-ALTER TABLE maps DROP COLUMN map_geom;
+DROP FUNCTION calculate_bbox;
+ALTER TABLE maps DROP COLUMN geometry;
