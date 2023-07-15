83fcc75adfe267dc48034a4b8e2fcb7b833d1dbc
Chris <e11776854@student.tuwien.ac.at>
2023-07-16 01:13:04 +0200

    close #311 enums are all lowercase now

--- backend/src/model/enum/quantity.rs
+++ backend/src/model/enum/quantity.rs
@@ -10,16 +10,16 @@ use utoipa::ToSchema;
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, DbEnum, ToSchema)]
 #[ExistingTypePath = "crate::schema::sql_types::Quantity"]
 pub enum Quantity {
-    #[serde(rename = "Nothing")]
-    #[db_rename = "Nothing"]
+    #[serde(rename = "nothing")]
+    #[db_rename = "nothing"]
     Nothing,
-    #[serde(rename = "Not enough")]
-    #[db_rename = "Not enough"]
+    #[serde(rename = "not enough")]
+    #[db_rename = "not enough"]
     NotEnough,
-    #[serde(rename = "Enough")]
-    #[db_rename = "Enough"]
+    #[serde(rename = "enough")]
+    #[db_rename = "enough"]
     Enough,
-    #[serde(rename = "More than enough")]
-    #[db_rename = "More than enough"]
+    #[serde(rename = "more than enough")]
+    #[db_rename = "more than enough"]
     MoreThanEnough,
 }
