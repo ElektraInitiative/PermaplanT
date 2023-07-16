dd55933daa4106916c54753780f687adc6e4d01a
Chris <e11776854@student.tuwien.ac.at>
2023-07-16 02:38:52 +0200

    cleaned up down.sql

--- backend/migrations/2023-07-15-152628_squashed_setup/down.sql
+++ backend/migrations/2023-07-15-152628_squashed_setup/down.sql
@@ -1,65 +1,66 @@
--- Drop all triggers
+---- Drop foreign keys
+ALTER TABLE seeds DROP CONSTRAINT seeds_plant_id_fkey;
+ALTER TABLE relations DROP CONSTRAINT relations_plant2_fkey;
+ALTER TABLE relations DROP CONSTRAINT relations_plant1_fkey;
+ALTER TABLE plantings DROP CONSTRAINT plantings_plant_id_fkey;
+ALTER TABLE plantings DROP CONSTRAINT plantings_layer_id_fkey;
+ALTER TABLE layers DROP CONSTRAINT layers_map_id_fkey;
+--
+-- Drop triggers
+DROP TRIGGER check_layer_type_before_insert_or_update ON plantings;
 
-DROP TRIGGER IF EXISTS check_layer_type_before_insert_or_update ON plantings;
+-- Drop primary keys
+ALTER TABLE seeds DROP CONSTRAINT seeds_pkey;
+ALTER TABLE relations DROP CONSTRAINT relations_pkey;
+ALTER TABLE plantings DROP CONSTRAINT plantings_pkey;
+ALTER TABLE plants DROP CONSTRAINT plant_detail_pkey;
+ALTER TABLE plants DROP CONSTRAINT plant_unique_name_key;
+ALTER TABLE maps DROP CONSTRAINT maps_pkey;
+ALTER TABLE maps DROP CONSTRAINT map_unique_name;
+ALTER TABLE layers DROP CONSTRAINT layers_pkey;
 
--- Drop all foreign keys
--- Note: you may need to drop these if your SQL server doesn't drop them automatically when you drop tables.
+-- Drop tables
+DROP TABLE plantings;
+DROP TABLE layers;
+DROP TABLE maps;
+DROP TABLE seeds;
+DROP TABLE relations;
+DROP TABLE plants;
 
-ALTER TABLE IF EXISTS layers DROP CONSTRAINT IF EXISTS layers_map_id_fkey;
-ALTER TABLE IF EXISTS plantings DROP CONSTRAINT IF EXISTS plantings_layer_id_fkey;
-ALTER TABLE IF EXISTS plantings DROP CONSTRAINT IF EXISTS plantings_plant_id_fkey;
-ALTER TABLE IF EXISTS relations DROP CONSTRAINT IF EXISTS relations_plant1_fkey;
-ALTER TABLE IF EXISTS relations DROP CONSTRAINT IF EXISTS relations_plant2_fkey;
-ALTER TABLE IF EXISTS seeds DROP CONSTRAINT IF EXISTS seeds_plant_id_fkey;
 
--- Drop all tables
+-- Drop sequences
+DROP SEQUENCE seeds_id_seq;
+DROP SEQUENCE plant_detail_id_seq;
+DROP SEQUENCE maps_id_seq;
+DROP SEQUENCE layers_id_seq;
 
-DROP TABLE IF EXISTS layers;
-DROP TABLE IF EXISTS maps;
-DROP TABLE IF EXISTS persons;
-DROP TABLE IF EXISTS plants;
-DROP TABLE IF EXISTS plantings;
-DROP TABLE IF EXISTS relations;
-DROP TABLE IF EXISTS seeds;
+-- Drop functions
+DROP FUNCTION check_layer_type();
 
--- Drop all sequences
+-- Drop types
+DROP TYPE water_requirement;
+DROP TYPE soil_water_retention;
+DROP TYPE soil_texture;
+DROP TYPE soil_ph;
+DROP TYPE shade;
+DROP TYPE relation_type;
+DROP TYPE quantity;
+DROP TYPE quality;
+DROP TYPE propagation_method;
+DROP TYPE privacy_options;
+DROP TYPE plant_spread;
+DROP TYPE plant_height;
+DROP TYPE nutrition_demand;
+DROP TYPE light_requirement;
+DROP TYPE life_cycle;
+DROP TYPE layer_type;
+DROP TYPE herbaceous_or_woody;
+DROP TYPE growth_rate;
+DROP TYPE flower_type;
+DROP TYPE fertility;
+DROP TYPE external_source;
+DROP TYPE deciduous_or_evergreen;
 
-DROP SEQUENCE IF EXISTS layers_id_seq;
-DROP SEQUENCE IF EXISTS maps_id_seq;
-DROP SEQUENCE IF EXISTS plant_detail_id_seq;
-DROP SEQUENCE IF EXISTS seeds_id_seq;
-
--- Drop all functions
-
-DROP FUNCTION IF EXISTS check_layer_type();
-
--- Drop all types
-
-DROP TYPE IF EXISTS deciduous_or_evergreen;
-DROP TYPE IF EXISTS external_source;
-DROP TYPE IF EXISTS fertility;
-DROP TYPE IF EXISTS flower_type;
-DROP TYPE IF EXISTS growth_rate;
-DROP TYPE IF EXISTS herbaceous_or_woody;
-DROP TYPE IF EXISTS layer_type;
-DROP TYPE IF EXISTS life_cycle;
-DROP TYPE IF EXISTS light_requirement;
-DROP TYPE IF EXISTS nutrition_demand;
-DROP TYPE IF EXISTS plant_height;
-DROP TYPE IF EXISTS plant_spread;
-DROP TYPE IF EXISTS privacy_options;
-DROP TYPE IF EXISTS propagation_method;
-DROP TYPE IF EXISTS quality;
-DROP TYPE IF EXISTS quantity;
-DROP TYPE IF EXISTS relation_type;
-DROP TYPE IF EXISTS shade;
-DROP TYPE IF EXISTS soil_ph;
-DROP TYPE IF EXISTS soil_texture;
-DROP TYPE IF EXISTS soil_water_retention;
-DROP TYPE IF EXISTS water_requirement;
-
--- Drop all extensions
-
-DROP EXTENSION IF EXISTS pg_trgm;
-
-DROP EXTENSION IF EXISTS postgis;
+-- Drop extensions
+DROP EXTENSION postgis;
+DROP EXTENSION pg_trgm;
