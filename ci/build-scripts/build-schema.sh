3b7ddcb239c75ac7b653ff763755cdedcac45d2c
Aydan Ghazani <ghazani.aydan@gmail.com>
2023-07-20 20:04:43 +0000

    Restore script

---
+++ ci/build-scripts/build-schema.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "building schema only"
+
+# Echo on and fail fast
+set -ex
+
+cd ./backend/
+
+if [[ -f /opt/rust.env ]]
+then
+    source /opt/rust.env
+fi
+
+# Download Cargo Modules
+cargo fetch
+
+# Show Diesel Version
+diesel --version
+
+# Migrate DB and create schema.rs
+LC_ALL=C diesel migration run
+
+# Create Bindings
+typeshare ./ --lang=typescript --output-file=../frontend/src/bindings/definitions.ts
