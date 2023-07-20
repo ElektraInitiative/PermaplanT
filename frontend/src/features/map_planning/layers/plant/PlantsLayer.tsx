bc1039b688b8eda3354338c14f0105cc27270241
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-07-20 16:01:11 +0200

    chore: lint and format.

--- frontend/src/features/map_planning/layers/plant/PlantsLayer.tsx
+++ frontend/src/features/map_planning/layers/plant/PlantsLayer.tsx
@@ -53,7 +53,7 @@ function usePlantLayerListeners(listening: boolean) {
         }),
       );
     },
-    [executeAction, selectedPlant, timelineDate],
+    [getSelectedLayerId, executeAction, selectedPlant, timelineDate],
   );
 
   /**
