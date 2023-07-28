cd47a8d2b9dab0e63367646bd5a8cb4208ea9fa9
Aydan Ghazani <ghazani.aydan@gmail.com>
2023-07-28 16:48:56 +0000

    Pre-commit fixes

--- doc/setups/nextcloud/README.md
+++ doc/setups/nextcloud/README.md
@@ -7,4 +7,4 @@ You can use the `docker-compose.yml` in this folder to start a local Nextcloud i
 The PermaplanT app requires to have following plugins installed in Nextcloud:
 
 1. [user_oidc](https://github.com/nextcloud/user_oidc)
-  This plugin allows to configure OpenID Connect providers for authentication
+   This plugin allows to configure OpenID Connect providers for authentication
--- scraper/src/insert_plants.js
+++ scraper/src/insert_plants.js
@@ -1,14 +1,14 @@
-import pgPromise from 'pg-promise';
-import csv from 'csvtojson';
-import dbPlantsColumns from './helpers/dp_plants_columns.js';
-import { sanitizeColumnNames } from './helpers/helpers.js';
+import pgPromise from "pg-promise";
+import csv from "csvtojson";
+import dbPlantsColumns from "./helpers/dp_plants_columns.js";
+import { sanitizeColumnNames } from "./helpers/helpers.js";
 
-import { config } from 'dotenv';
+import { config } from "dotenv";
 config();
 const pgp = pgPromise({});
 
 const db = pgp(process.env.DATABASE_URL);
-const plantsFilePath = process.argv[2] || 'data/mergedDatasets.csv';
+const plantsFilePath = process.argv[2] || "data/mergedDatasets.csv";
 
 /**
  * Sanitizes the values of the json array
@@ -22,49 +22,49 @@ function sanitizeValues(jsonArray) {
     Object.assign(obj, {});
     keys.forEach((newKey) => {
       obj[newKey] = obj[newKey].trim();
-      if (obj[newKey] === '') {
+      if (obj[newKey] === "") {
         obj[newKey] = null;
       }
 
       if (
-        (newKey === 'soil_ph' && obj[newKey] !== null) ||
-        (newKey === 'soil_texture' && obj[newKey] !== null) ||
-        (newKey === 'soil_water_retention' && obj[newKey] !== null) ||
-        (newKey === 'fertility' && obj[newKey] !== null) ||
-        (newKey === 'life_cycle' && obj[newKey] !== null) ||
-        (newKey === 'common_name' && obj[newKey] !== null) ||
-        (newKey === 'common_name_de' && obj[newKey] !== null) ||
-        (newKey === 'light_requirement' && obj[newKey] !== null) ||
-        (newKey === 'water_requirement' && obj[newKey] !== null) ||
-        (newKey === 'growth_rate' && obj[newKey] !== null) ||
-        (newKey === 'propagation_method' && obj[newKey] !== null) ||
-        (newKey === 'common_name_en' && obj[newKey] !== null) ||
-        (newKey === 'edible_parts' && obj[newKey] !== null)
+        (newKey === "soil_ph" && obj[newKey] !== null) ||
+        (newKey === "soil_texture" && obj[newKey] !== null) ||
+        (newKey === "soil_water_retention" && obj[newKey] !== null) ||
+        (newKey === "fertility" && obj[newKey] !== null) ||
+        (newKey === "life_cycle" && obj[newKey] !== null) ||
+        (newKey === "common_name" && obj[newKey] !== null) ||
+        (newKey === "common_name_de" && obj[newKey] !== null) ||
+        (newKey === "light_requirement" && obj[newKey] !== null) ||
+        (newKey === "water_requirement" && obj[newKey] !== null) ||
+        (newKey === "growth_rate" && obj[newKey] !== null) ||
+        (newKey === "propagation_method" && obj[newKey] !== null) ||
+        (newKey === "common_name_en" && obj[newKey] !== null) ||
+        (newKey === "edible_parts" && obj[newKey] !== null)
       ) {
-        obj[newKey] = obj[newKey].split(',');
+        obj[newKey] = obj[newKey].split(",");
         obj[newKey] = obj[newKey].map((item) => {
           return item.toLowerCase().trim();
         });
       }
 
-      if (newKey === 'plant_references' && obj[newKey] !== null) {
-        obj[newKey] = obj[newKey].split('///ref///');
+      if (newKey === "plant_references" && obj[newKey] !== null) {
+        obj[newKey] = obj[newKey].split("///ref///");
         obj[newKey] = obj[newKey].map((item) => {
           return item.trim();
         });
       }
 
-      if (newKey === 'environmental_tolerances' && obj[newKey] !== null) {
-        obj[newKey] = obj[newKey].split('\n');
+      if (newKey === "environmental_tolerances" && obj[newKey] !== null) {
+        obj[newKey] = obj[newKey].split("\n");
         obj[newKey] = obj[newKey].map((item) => {
           return item.toLowerCase().trim();
         });
       }
 
-      if (newKey === 'hardiness_zone' && obj[newKey] !== null) {
-        obj[newKey] = obj[newKey].replace(/[\u2013-\u2014]/g, '-');
-        if (obj[newKey].includes('-')) {
-          obj[newKey] = obj[newKey].split('-');
+      if (newKey === "hardiness_zone" && obj[newKey] !== null) {
+        obj[newKey] = obj[newKey].replace(/[\u2013-\u2014]/g, "-");
+        if (obj[newKey].includes("-")) {
+          obj[newKey] = obj[newKey].split("-");
           obj[newKey] = `[${obj[newKey][0]},${obj[newKey][1]})`;
         } else {
           obj[newKey] = `[${obj[newKey]},${obj[newKey]})`;
@@ -72,8 +72,8 @@ function sanitizeValues(jsonArray) {
       }
 
       if (
-        (newKey === 'harvest_time' && obj[newKey] !== null) ||
-        (newKey === 'sowing_outdoors' && obj[newKey] !== null)
+        (newKey === "harvest_time" && obj[newKey] !== null) ||
+        (newKey === "sowing_outdoors" && obj[newKey] !== null)
       ) {
         obj[newKey] = JSON.parse(obj[newKey]);
         obj[newKey] = obj[newKey].map((item) => {
@@ -92,7 +92,7 @@ function sanitizeValues(jsonArray) {
  * @param {*} fileName - The file name of the csv file
  */
 async function insertPlants(fileName) {
-  console.log('[INFO] Starting the insertion of plants into database.');
+  console.log("[INFO] Starting the insertion of plants into database.");
 
   const jsonArray = await csv().fromFile(fileName);
 
@@ -101,21 +101,23 @@ async function insertPlants(fileName) {
   sanitizeValues(jsonArray);
 
   const cs = new pgp.helpers.ColumnSet(dbPlantsColumns, {
-    table: 'plants',
+    table: "plants",
   });
 
   const query =
     pgp.helpers.insert(jsonArray, cs) +
-    ` ON CONFLICT ON CONSTRAINT plant_unique_name_key DO UPDATE SET ${cs.assignColumns({
-      from: 'EXCLUDED',
-      skip: 'unique_name',
-    })}, updated_at = NOW()`;
+    ` ON CONFLICT ON CONSTRAINT plant_unique_name_key DO UPDATE SET ${cs.assignColumns(
+      {
+        from: "EXCLUDED",
+        skip: "unique_name",
+      }
+    )}, updated_at = NOW()`;
 
-  console.log('[INFO] Inserting plant details into database.');
+  console.log("[INFO] Inserting plant details into database.");
 
   db.none(query);
 
-  console.log('[INFO] Plant details inserted into database.');
+  console.log("[INFO] Plant details inserted into database.");
 }
 
 insertPlants(plantsFilePath);
