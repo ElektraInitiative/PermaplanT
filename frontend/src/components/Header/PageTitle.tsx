995aa8c06e41e7b1cb5cdb0bfa60da04640c2abd
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-08-01 10:20:06 +0200

    doc: document undocumented UI elements.

--- frontend/src/components/Header/PageTitle.tsx
+++ frontend/src/components/Header/PageTitle.tsx
@@ -1,7 +1,9 @@
 interface PageTitleProps {
+  /** Page title text content. */
   title: string;
 }
 
+/** A simple custom-styled page header. */
 const PageTitle = ({ title }: PageTitleProps) => {
   return <h1 className="mb-8 text-2xl">{title}</h1>;
 };
