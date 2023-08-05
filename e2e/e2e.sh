8686cbe65a569416d591dadd866f02e73fdf7cad
Aydan Ghazani <ghazani.aydan@gmail.com>
2023-08-05 12:08:16 +0000

    Remove env file, switch to rerun

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the pytest command
-python3 -m pytest -n auto --retries 2 --video retain-on-failure --html=test-reports/report.html --self-contained-html --cucumberjson=test-reports/cucumber.json
+python3 -m pytest -n auto --reruns 2 --reruns-delay 5 --rerun-except AssertionError --video retain-on-failure --html=test-reports/report.html --self-contained-html --cucumberjson=test-reports/cucumber.json
