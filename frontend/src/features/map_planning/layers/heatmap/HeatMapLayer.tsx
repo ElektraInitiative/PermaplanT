bf56bbd95824d9839b394bad8d70ddd89721ea0c
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-08-06 16:22:15 +0200

    heatmap: fix broken image.

--- frontend/src/features/map_planning/layers/heatmap/HeatMapLayer.tsx
+++ frontend/src/features/map_planning/layers/heatmap/HeatMapLayer.tsx
@@ -1,9 +1,8 @@
 import { getHeatMap } from '@/features/map_planning/layers/heatmap/api/getHeatMap';
 import useMapStore from '@/features/map_planning/store/MapStore';
 import { useImageFromBlob } from '@/features/nextcloud_integration/hooks/useImageFromBlob';
-import { AxiosResponse } from 'axios';
+import { useQuery } from '@tanstack/react-query';
 import Konva from 'konva';
-import { useEffect, useState } from 'react';
 import { Layer, Image } from 'react-konva';
 
 type HeatMapLayerProps = Konva.LayerConfig;
@@ -12,34 +11,34 @@ export const HeatMapLayer = (props: HeatMapLayerProps) => {
   const { ...layerProps } = props;
 
   const mapId = useMapStore((state) => state.untrackedState.mapId);
-  const layerId = useMapStore((state) => state.trackedState.layers.plants.id);
+  const plantLayerId = useMapStore((state) => state.trackedState.layers.plants.id);
+  const shadeLayerId = useMapStore((state) => state.trackedState.layers.shade.id);
   const selectedPlantId = useMapStore(
     (state) => state.untrackedState.layers.plants.selectedPlantForPlanting?.id,
   );
 
-  const [imageResponse, setImageResponse] = useState<AxiosResponse | null>();
-
-  useEffect(() => {
-    async function loadHeatmapImage() {
-      const response = await getHeatMap(mapId, layerId, selectedPlantId);
-      setImageResponse(response);
-    }
-
-    if (!imageResponse) {
-      loadHeatmapImage();
-    }
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['heatmap', selectedPlantId],
+    queryFn: () => getHeatMap(mapId, plantLayerId, shadeLayerId, selectedPlantId),
+    cacheTime: 0,
+    staleTime: 0,
+    enabled: !!mapId && !!plantLayerId && !!shadeLayerId && !!selectedPlantId,
   });
 
   const image = useImageFromBlob({
-    isLoading: false,
-    isError: imageResponse === null || imageResponse?.status !== 200,
-    data: imageResponse?.data,
+    isLoading,
+    isError,
+    data: data?.data,
     fallbackImageSource: '',
   });
 
+  if (selectedPlantId === undefined || data === undefined || isLoading || isError) {
+    return <Layer listening={false} />;
+  }
+
   return (
     <Layer {...layerProps} listening={false}>
-      <Image image={image}></Image>
+      <Image image={image} x={0} y={0} strokeWidth={1}></Image>
     </Layer>
   );
 };
--- frontend/src/features/map_planning/layers/heatmap/api/getHeatMap.ts
+++ frontend/src/features/map_planning/layers/heatmap/api/getHeatMap.ts
@@ -4,10 +4,16 @@ import { createAPI } from '@/config/axios';
  * Tells the backend to generate a heatmap.
  *
  * @param mapId The map for which the heatmap is generated.
- * @param layerId Which plant layer should be considered.
+ * @param plantLayerId Which plant layer should be considered?
+ * @param shadeLayerId Which shade layer should be used?
  * @param plantId The plant that is supposed to be added to the plant layer.
  */
-export async function getHeatMap(mapId: number, layerId: number, plantId: number | undefined) {
+export async function getHeatMap(
+  mapId: number,
+  plantLayerId: number,
+  shadeLayerId: number,
+  plantId: number | undefined,
+) {
   if (!plantId) {
     return null;
   }
@@ -16,7 +22,8 @@ export async function getHeatMap(mapId: number, layerId: number, plantId: number
 
   try {
     return await http.get(`api/maps/${mapId}/layers/plants/heatmap`, {
-      params: { layer_id: layerId, plant_id: plantId },
+      params: { plant_layer_id: plantLayerId, plant_id: plantId, shade_layer_id: shadeLayerId },
+      responseType: 'blob',
     });
   } catch (error) {
     throw error as Error;
