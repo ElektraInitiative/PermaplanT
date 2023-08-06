3057e6ca2ab4a018bec680b764795e18f5ad253b
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-08-06 18:02:15 +0200

    heatmap: set scale according to map geometry.

--- frontend/src/features/map_planning/layers/heatmap/HeatMapLayer.tsx
+++ frontend/src/features/map_planning/layers/heatmap/HeatMapLayer.tsx
@@ -1,11 +1,17 @@
 import { getHeatMap } from '@/features/map_planning/layers/heatmap/api/getHeatMap';
 import useMapStore from '@/features/map_planning/store/MapStore';
+import { findMapById } from '@/features/maps/api/findMapById';
 import { useImageFromBlob } from '@/features/nextcloud_integration/hooks/useImageFromBlob';
 import { useQuery } from '@tanstack/react-query';
 import Konva from 'konva';
 import { Layer, Image } from 'react-konva';
 
 type HeatMapLayerProps = Konva.LayerConfig;
+
+type Geomerty = {
+  rings: Array<Array<{ x: number; y: number }>>;
+  srid: string;
+};
 
 export const HeatMapLayer = (props: HeatMapLayerProps) => {
   const { ...layerProps } = props;
@@ -17,7 +23,11 @@ export const HeatMapLayer = (props: HeatMapLayerProps) => {
     (state) => state.untrackedState.layers.plants.selectedPlantForPlanting?.id,
   );
 
-  const { isLoading, isError, data } = useQuery({
+  const {
+    isLoading: heatmapIsLoading,
+    isError: heatmapIsError,
+    data: heatmapData,
+  } = useQuery({
     queryKey: ['heatmap', selectedPlantId],
     queryFn: () => getHeatMap(mapId, plantLayerId, shadeLayerId, selectedPlantId),
     cacheTime: 0,
@@ -25,20 +35,64 @@ export const HeatMapLayer = (props: HeatMapLayerProps) => {
     enabled: !!mapId && !!plantLayerId && !!shadeLayerId && !!selectedPlantId,
   });
 
+  const {
+    isLoading: mapIsLoading,
+    isError: mapIsError,
+    data: mapData,
+  } = useQuery({
+    queryKey: ['map', mapId],
+    queryFn: () => findMapById(mapId),
+    cacheTime: Infinity,
+    enabled: !!mapId,
+  });
+
   const image = useImageFromBlob({
-    isLoading,
-    isError,
-    data: data?.data,
+    isLoading: heatmapIsLoading,
+    isError: heatmapIsError,
+    data: heatmapData?.data,
     fallbackImageSource: '',
   });
 
-  if (selectedPlantId === undefined || data === undefined || isLoading || isError) {
+  if (
+    selectedPlantId === undefined ||
+    heatmapData === undefined ||
+    heatmapIsLoading ||
+    heatmapIsError ||
+    mapIsLoading ||
+    mapIsError ||
+    mapData === undefined
+  ) {
     return <Layer listening={false} />;
   }
 
+  // calculate map bounds
+  // we only need the first edge ring
+  const geometry = (mapData.geometry as Geomerty).rings[0];
+  let minX = geometry[0].x;
+  let maxX = geometry[0].x;
+  let minY = geometry[0].y;
+  let maxY = geometry[0].y;
+
+  for (const point of geometry) {
+    minX = Math.min(point.x, minX);
+    maxX = Math.max(point.x, maxX);
+    minY = Math.min(point.y, minY);
+    maxY = Math.max(point.y, maxY);
+  }
+
+  const width = Math.abs(maxX - minX);
+  const height = Math.abs(maxY - minY);
+
   return (
     <Layer {...layerProps} listening={false}>
-      <Image image={image} x={0} y={0} strokeWidth={1}></Image>
+      <Image
+        image={image}
+        x={-width / 2}
+        y={-height / 2}
+        width={width}
+        height={height}
+        strokeWidth={1}
+      ></Image>
     </Layer>
   );
 };
