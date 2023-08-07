de8a8651f56050d1c91adf3fb8f323e09f4d4f7c
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-08-07 22:39:41 +0200

    page title: add to storybook

---
+++ frontend/src/components/Header/PageTitle.stories.tsx
@@ -0,0 +1,17 @@
+import PageTitle from './PageTitle';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof PageTitle> = {
+  title: 'Components/Header/PageTitle',
+  component: PageTitle,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof PageTitle>;
+
+export const PageTitleStory: Story = {
+  args: {
+    title: 'Praise Permaculture!',
+  },
+};
