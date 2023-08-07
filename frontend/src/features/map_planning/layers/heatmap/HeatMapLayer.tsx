23faa989dbd96c8390c81ffe2ca343c42b2074c6
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-08-07 13:55:48 +0200

    heat map: refactor component.

--- frontend/src/features/map_planning/layers/heatmap/HeatMapLayer.tsx
+++ frontend/src/features/map_planning/layers/heatmap/HeatMapLayer.tsx
@@ -8,10 +8,91 @@ import { Layer, Image } from 'react-konva';
 
 type HeatMapLayerProps = Konva.LayerConfig;
 
-type Geomerty = {
+/**
+ * Represents an array of closed edge loops.
+ */
+export type Geometry = {
   rings: Array<Array<{ x: number; y: number }>>;
   srid: string;
 };
+
+/**
+ * Contains additional geometry properties that need to be derived in the frontend.
+ */
+export type GeometryStats = {
+  minX: number;
+  minY: number;
+  maxX: number;
+  maxY: number;
+  width: number;
+  height: number;
+};
+
+/**
+ * Derive GeometryStats from a Geometry object.
+ *
+ * @param geometry The object for which GeometryStats should be generated.
+ */
+export function calculateGeometryStats(geometry: Geometry): GeometryStats {
+  const firstEdgeRing = geometry.rings[0];
+  let minX = firstEdgeRing[0].x;
+  let maxX = firstEdgeRing[0].x;
+  let minY = firstEdgeRing[0].y;
+  let maxY = firstEdgeRing[0].y;
+
+  for (const point of firstEdgeRing) {
+    minX = Math.min(point.x, minX);
+    maxX = Math.max(point.x, maxX);
+    minY = Math.min(point.y, minY);
+    maxY = Math.max(point.y, maxY);
+  }
+
+  const width = Math.abs(maxX - minX);
+  const height = Math.abs(maxY - minY);
+
+  return {
+    minX,
+    minY,
+    maxX,
+    maxY,
+    width,
+    height,
+  };
+}
+
+export function useHeatmap(
+  mapId: number,
+  plantLayerId: number,
+  shadeLayerId: number,
+  selectedPlantId: number | undefined,
+) {
+  // cacheTime and staleTime are zero to force an image reload if any parameter changes.
+  // Caching is not worth it in this case because the heatmap is no longer up to date if the user modified the plant layer.
+  const {
+    isLoading,
+    isError,
+    data: heatmapData,
+  } = useQuery({
+    queryKey: ['heatmap', selectedPlantId],
+    queryFn: () => getHeatMap(mapId, plantLayerId, shadeLayerId, selectedPlantId),
+    cacheTime: 0,
+    staleTime: 0,
+    enabled: !!mapId && !!plantLayerId && !!shadeLayerId && !!selectedPlantId,
+  });
+
+  const image = useImageFromBlob({
+    isLoading,
+    isError,
+    data: heatmapData?.data,
+    fallbackImageSource: '',
+  });
+
+  return {
+    isLoading,
+    isError,
+    image,
+  };
+}
 
 export const HeatMapLayer = (props: HeatMapLayerProps) => {
   const { ...layerProps } = props;
@@ -24,18 +105,6 @@ export const HeatMapLayer = (props: HeatMapLayerProps) => {
   );
 
   const {
-    isLoading: heatmapIsLoading,
-    isError: heatmapIsError,
-    data: heatmapData,
-  } = useQuery({
-    queryKey: ['heatmap', selectedPlantId],
-    queryFn: () => getHeatMap(mapId, plantLayerId, shadeLayerId, selectedPlantId),
-    cacheTime: 0,
-    staleTime: 0,
-    enabled: !!mapId && !!plantLayerId && !!shadeLayerId && !!selectedPlantId,
-  });
-
-  const {
     isLoading: mapIsLoading,
     isError: mapIsError,
     data: mapData,
@@ -46,16 +115,14 @@ export const HeatMapLayer = (props: HeatMapLayerProps) => {
     enabled: !!mapId,
   });
 
-  const image = useImageFromBlob({
+  const {
     isLoading: heatmapIsLoading,
     isError: heatmapIsError,
-    data: heatmapData?.data,
-    fallbackImageSource: '',
-  });
+    image,
+  } = useHeatmap(mapId, plantLayerId, shadeLayerId, selectedPlantId);
 
   if (
     selectedPlantId === undefined ||
-    heatmapData === undefined ||
     heatmapIsLoading ||
     heatmapIsError ||
     mapIsLoading ||
@@ -65,27 +132,17 @@ export const HeatMapLayer = (props: HeatMapLayerProps) => {
     return <Layer listening={false} />;
   }
 
-  // calculate map bounds
-  // we only need the first edge ring
-  const geometry = (mapData.geometry as Geomerty).rings[0];
-  let minX = geometry[0].x;
-  let maxX = geometry[0].x;
-  let minY = geometry[0].y;
-  let maxY = geometry[0].y;
-
-  for (const point of geometry) {
-    minX = Math.min(point.x, minX);
-    maxX = Math.max(point.x, maxX);
-    minY = Math.min(point.y, minY);
-    maxY = Math.max(point.y, maxY);
-  }
-
-  const width = Math.abs(maxX - minX);
-  const height = Math.abs(maxY - minY);
+  const geometryStats = calculateGeometryStats(mapData.geometry as Geometry);
 
   return (
     <Layer {...layerProps} listening={false}>
-      <Image image={image} x={minX} y={minY} width={width} height={height}></Image>
+      <Image
+        image={image}
+        x={geometryStats.minX}
+        y={geometryStats.minY}
+        width={geometryStats.width}
+        height={geometryStats.height}
+      ></Image>
     </Layer>
   );
 };
