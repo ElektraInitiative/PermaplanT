499c128355b9642e028fd6b10be81c4fa003a29a
Samuel <absurd.turtl3@gmail.com>
2023-08-15 12:42:58 +0200

    removed timeout from map creation request

--- frontend/src/features/maps/api/createMap.ts
+++ frontend/src/features/maps/api/createMap.ts
@@ -4,7 +4,7 @@ import { createAPI } from '@/config/axios';
 export async function createMap(map: NewMapDto) {
   const http = createAPI();
   try {
-    const response = await http.post<MapDto>('/api/maps', map);
+    const response = await http.post<MapDto>('/api/maps', map, { timeout: 0 });
     return response.data;
   } catch (error) {
     throw error as Error;
