34830c1f26142418952d1a14a1358b9f624450db
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-08-25 15:22:58 +0200

    delete seed api: remove unnecessary url.

--- frontend/src/features/seeds/api/deleteSeed.ts
+++ frontend/src/features/seeds/api/deleteSeed.ts
@@ -1,11 +1,10 @@
-import { baseApiUrl } from '@/config';
 import { createAPI } from '@/config/axios';
 
 export const deleteSeed = async (id: number) => {
   const http = createAPI();
 
   try {
-    await http.delete(`${baseApiUrl}/api/seeds/${id}`);
+    await http.delete(`/api/seeds/${id}`);
   } catch (error) {
     throw error as Error;
   }
