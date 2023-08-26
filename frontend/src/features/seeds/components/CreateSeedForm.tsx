bbe58cff209ddcc275857666c1f89054cccff6b0
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-08-26 21:59:16 +0200

    create seed form: add default values to form data

--- frontend/src/features/seeds/components/CreateSeedForm.tsx
+++ frontend/src/features/seeds/components/CreateSeedForm.tsx
@@ -57,6 +57,12 @@ const CreateSeedForm = ({
   const [quantityOption, setQuantityOption] = useState<SelectOption | undefined>(quantity[0]);
   const [qualityOption, setQualityOption] = useState<SelectOption | undefined>(quality[0]);
 
+  // Makes sure that the default select values are stored in the form data..
+  const currentQuantity = quantityOption?.value as Quantity;
+  const currentQuality = qualityOption?.value as Quality;
+  setValue('quantity', currentQuantity);
+  setValue('quality', currentQuality);
+
   useEffect(
     () => {
       if (existingSeed) {
