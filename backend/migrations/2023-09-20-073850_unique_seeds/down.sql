6c9131f581c2fb6a7cfe5112f56c3ac85d82a507
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-09-20 12:47:10 +0200

    migrations: disallow - and ' characters in seed names.

--- backend/migrations/2023-09-20-073850_unique_seeds/down.sql
+++ backend/migrations/2023-09-20-073850_unique_seeds/down.sql
@@ -1,2 +1,5 @@
 ALTER TABLE seeds
 DROP CONSTRAINT unique_seeds;
+
+ALTER TABLE seeds
+DROP CONSTRAINT seeds_disalow_confusing_characters;
--- backend/migrations/2023-09-20-073850_unique_seeds/up.sql
+++ backend/migrations/2023-09-20-073850_unique_seeds/up.sql
@@ -1,3 +1,10 @@
+-- Users should not be able to have two seeds with the exact same name and plant.
 ALTER TABLE seeds
 ADD CONSTRAINT unique_seeds
 UNIQUE (owner_id, plant_id, name);
+
+-- Make sure the user can't enter names with - or ' in the name, as this could cause confusion
+-- when seeds are displayed with their full name.
+ALTER TABLE seeds
+ADD CONSTRAINT seeds_disalow_confusing_characters
+CHECK (name ~ '^(?!.*(\-|\'')).*$');
