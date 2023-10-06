f3e419fd0bb083013a88f5d351e020f41f3a71d5
Samuel <absurd.turtl3@gmail.com>
2023-10-06 15:13:28 +0200

    implemented full page nav for mobile

---
+++ frontend/src/components/Layout/FullPageNav.tsx
@@ -0,0 +1,85 @@
+import { ReactComponent as LogoSmallGraySVG } from '../../assets/permaplant-logo-gray.svg';
+import { ReactComponent as LogoSmallSVG } from '../../assets/permaplant-logo.svg';
+import { DarkModeSwitcher } from '../../features/dark_mode';
+import { useDarkModeStore } from '../../features/dark_mode';
+import LanguageSwitcher from '../../features/landing_page/components/LanguageSwitcher';
+import { ReactComponent as CloseIcon } from '../../icons/close.svg';
+import ButtonLink from '../Button/ButtonLink';
+import IconButton from '../Button/IconButton';
+import { LoginButton } from '@/features/auth/components/LoginButton';
+import { useSafeAuth } from '@/hooks/useSafeAuth';
+import { useTranslation } from 'react-i18next';
+
+interface NavpageProps {
+  close: () => void;
+}
+
+/**
+ * The navigation component that is fixed on the top.
+ *
+ */
+const Navpage = ({ close }: NavpageProps) => {
+  const darkMode = useDarkModeStore((state) => state.darkMode);
+  const { t } = useTranslation(['routes']);
+  const auth = useSafeAuth();
+  const backendVersion = sessionStorage.getItem('backend_version');
+  const navbarItems = (
+    <div className="flex items-center">
+      <ul className="pt-1">
+        <li>
+          <ButtonLink title={t('routes:maps')} to="/maps" className="pe-4 text-lg font-medium" />
+          <ButtonLink title={t('routes:seeds')} to="/seeds" className="text-lg font-medium" />
+        </li>
+      </ul>
+    </div>
+  );
+
+  return (
+    // z-index 1001 is needed because of leaflet GeoMap
+    <nav
+      className="
+      fixed
+      left-0
+      top-0
+      z-[1001]
+      h-full
+      w-full
+      items-center
+      bg-white
+      dark:bg-neutral-100-dark
+      "
+    >
+      <div className="flex flex-col gap-4 p-4">
+        <div className="flex justify-between">
+          <a href="/" className="flex items-center">
+            {darkMode ? (
+              <LogoSmallSVG className="h-12 w-12 pr-2" />
+            ) : (
+              <LogoSmallGraySVG className="h-12 w-12 pr-2" />
+            )}
+            <div className="flex flex-col">
+              <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+                PermaplanT
+              </span>
+              <span className="mt-auto text-xs text-gray-500 dark:text-gray-400">
+                {' '}
+                {backendVersion}
+              </span>
+            </div>
+          </a>
+          <IconButton onClick={close}>
+            <CloseIcon></CloseIcon>
+          </IconButton>
+        </div>
+        <ul>{auth.isAuthenticated && navbarItems}</ul>
+        <div className="flex items-center justify-center gap-4">
+          <DarkModeSwitcher />
+          <LanguageSwitcher />
+        </div>
+        <LoginButton></LoginButton>
+      </div>
+    </nav>
+  );
+};
+
+export default Navpage;
--- frontend/src/components/Layout/Navbar.tsx
+++ frontend/src/components/Layout/Navbar.tsx
@@ -7,6 +7,8 @@ import { useSafeAuth } from '@/hooks/useSafeAuth';
 import { ReactComponent as LogoSmallGraySVG } from '@/svg/permaplant-logo-gray.svg';
 import { ReactComponent as LogoSmallSVG } from '@/svg/permaplant-logo.svg';
 import { useTranslation } from 'react-i18next';
+import FullPageNav from './FullPageNav';
+import { useState } from 'react';
 
 /**
  * The navigation component that is fixed on the top.
@@ -17,6 +19,7 @@ const Navbar = () => {
   const { t } = useTranslation(['routes']);
   const auth = useSafeAuth();
   const backendVersion = sessionStorage.getItem('backend_version');
+  const [ showFullPageNav, setShowFullPageNav ] = useState(false);
   const navbarItems = (
     <div className="flex items-center">
       <ul className="pt-1">
@@ -28,9 +31,30 @@ const Navbar = () => {
     </div>
   );
 
+  const openFullPageNav = () => {
+    console.log(showFullPageNav)
+    setShowFullPageNav(true)
+    console.log(showFullPageNav)
+  }
+
   return (
     // z-index 1001 is needed because of leaflet GeoMap
-    <nav className="fixed left-0 top-0 z-[1001] h-16 w-full items-center border-b-[0.5px] border-neutral-200 bg-white dark:border-neutral-700 dark:bg-neutral-100-dark">
+    showFullPageNav ? <FullPageNav close={() => setShowFullPageNav(false)}></FullPageNav> : <nav
+      className="
+      fixed
+      left-0
+      top-0
+      z-[1001]
+      h-16
+      w-full
+      items-center
+      border-b-[0.5px]
+      border-neutral-200
+      bg-white
+      dark:border-neutral-700
+      dark:bg-neutral-100-dark
+      "
+    >
       <div className="mx-auto flex h-full max-w-screen-xl flex-wrap items-center justify-between">
         <div className="flex space-x-16">
           <div className="flex space-x-6">
@@ -41,7 +65,7 @@ const Navbar = () => {
                 <LogoSmallGraySVG className="h-12 w-12 pr-2" />
               )}
               <div className="flex flex-col">
-                <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+                <span className="text-2xl font-semibold dark:text-white">
                   PermaplanT
                 </span>
                 <span className="mt-auto text-xs text-gray-500 dark:text-gray-400">
@@ -54,33 +78,34 @@ const Navbar = () => {
           {auth.isAuthenticated && navbarItems}
         </div>
         <div className="flex md:order-2">
-          <div className="flex items-center space-x-4">
+          <div className="items-center space-x-4 hidden md:flex">
             <DarkModeSwitcher />
             <LanguageSwitcher />
-            <button
-              data-collapse-toggle="navbar-sticky"
-              type="button"
-              className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-              aria-controls="navbar-sticky"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="h-6 w-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
             <LoginButton></LoginButton>
           </div>
+          <button
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+            onClick={ openFullPageNav }
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="h-6 w-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
         </div>
       </div>
     </nav>
