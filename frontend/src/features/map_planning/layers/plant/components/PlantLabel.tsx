1b561facad12ac3090ac0492cb2aca13269af534
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-10-08 21:15:31 +0200

    plant label: use plant name utilities.

--- frontend/src/features/map_planning/layers/plant/components/PlantLabel.tsx
+++ frontend/src/features/map_planning/layers/plant/components/PlantLabel.tsx
@@ -1,6 +1,7 @@
 import { PlantingDto } from '@/bindings/definitions';
 import { useFindPlantById } from '@/features/map_planning/layers/plant/hooks/useFindPlantById';
 import { MapLabel } from '@/features/map_planning/utils/MapLabel';
+import { commonName } from '@/utils/plantName';
 import Konva from 'konva';
 import { useEffect, useRef, useState } from 'react';
 import { Label } from 'react-konva';
@@ -28,17 +29,15 @@ export const PlantLabel = ({ planting }: PlantLabelProps) => {
   const labelOffsetX = labelWidth / 2;
   const labelOffsetY = (planting.height / 2) * planting.scaleY * 1.1;
 
+  const plantName: string = commonName(plant);
+
   return (
     <MapLabel
       listening={false}
       ref={labelRef}
       x={planting.x - labelOffsetX}
       y={planting.y + labelOffsetY}
-      content={
-        plant.common_name_en === undefined
-          ? plant.unique_name
-          : plant.common_name_en[0].charAt(0).toUpperCase() + plant.common_name_en[0].slice(1)
-      }
+      content={plantName}
     />
   );
 };
