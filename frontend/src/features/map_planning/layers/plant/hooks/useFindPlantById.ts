844fcec65f6b3560d76e721e79b3ade38ced6fde
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-10-09 22:16:17 +0200

    plant & seed search: make sure selected seeds can be used for planting.

--- frontend/src/features/map_planning/layers/plant/hooks/useFindPlantById.ts
+++ frontend/src/features/map_planning/layers/plant/hooks/useFindPlantById.ts
@@ -1,18 +1,29 @@
 import { PlantsSummaryDto } from '@/bindings/definitions';
 import { findPlantById } from '@/features/seeds/api/findPlantById';
 import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 
-export function useFindPlantById(onSuccess?: (plant: PlantsSummaryDto) => void) {
+/**
+ * Request plants using their id
+ * @param afterPlantLoad
+ */
+export function useFindPlantById(afterPlantLoad: (plant: PlantsSummaryDto) => void) {
   const { t } = useTranslation(['plantings']);
   const [plantId, setPlantId] = useState<number>(0);
+  console.log(plantId);
   const { data, error } = useQuery(['plants/plant', plantId] as const, {
     queryFn: (context) => findPlantById(context.queryKey[1]),
     staleTime: Infinity,
-    onSuccess,
   });
+
+  // useQuery's onSuccess is deprecated and does not seem to work in this context
+  // for whatever reason.
+  useEffect(() => {
+    console.log('plant loaded');
+    if (data) afterPlantLoad(data);
+  }, [data, afterPlantLoad]);
 
   if (error) {
     toast.error(t('plantings:error_fetching_plant'), { autoClose: false });
