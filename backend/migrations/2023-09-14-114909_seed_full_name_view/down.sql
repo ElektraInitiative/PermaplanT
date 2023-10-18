7663ef43903eeceaf3b854fe8c2b6aa4027cbdfa
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-10-18 13:39:36 +0200

    migrations: remove unneeded migration

--- backend/migrations/2023-09-14-114909_seed_full_name_view/down.sql
+++
@@ -1,1 +0,0 @@
-DROP VIEW seeds_name_view;
--- backend/migrations/2023-09-14-114909_seed_full_name_view/up.sql
+++
@@ -1,9 +0,0 @@
--- returns all components of a seeds complete name along with its id and owner id.
-CREATE OR REPLACE VIEW seeds_name_view AS
-SELECT
-    seeds.id AS id,
-    plants.common_name_en AS common_name_en,
-    plants.common_name_de AS common_name_de,
-    plants.unique_name AS unique_name,
-    seeds.name AS additional_name
-FROM seeds INNER JOIN plants ON seeds.plant_id = plants.id;
