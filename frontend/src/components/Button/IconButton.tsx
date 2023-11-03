5a198313671e222eed3762909fb6e8054ac9f756
Christoph Nemeth <tschawax@gmail.com>
2023-11-03 16:16:27 +0100

    feat: add status indicative designs for toolbox icons
    Undo and redo icons are now greyed out and disabled when not available.
    Icons can now be declared as toolbox icons which adds toolbox specific design.
    Focus design is improved and less noisy now.
    
    Closes #1025, closes #657

--- frontend/src/components/Button/IconButton.tsx
+++ frontend/src/components/Button/IconButton.tsx
@@ -1,6 +1,10 @@
 interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   /** The variant specifies the look of the button. */
   variant?: ButtonVariant;
+  /** Active icons (e.g. Grid shown) are rendered in highlighted fashion. */
+  renderAsActive?: boolean;
+  /** Toolbox icons receive additional styling. */
+  isToolboxIcon?: boolean;
 }
 
 export enum ButtonVariant {
@@ -20,17 +24,31 @@ const variantStyles = {
  * @param props.variant The variant specifies the look of the button.
  * @param props All React props for buttons can be applied.
  */
-export default function IconButton({ variant = ButtonVariant.primary, ...props }: IconButtonProps) {
-  const className =
-    'inline-flex h-6 w-6 justify-center rounded-lg items-center text-sm font-medium focus:outline-none focus:ring-4 focus:ring-blue-300 stroke-neutral-800 dark:stroke-neutral-800-dark fill-neutral-800 dark:fill-neutral-800-dark' +
+export default function IconButton({
+  variant = ButtonVariant.primary,
+  renderAsActive = false,
+  isToolboxIcon = false,
+  ...props
+}: IconButtonProps) {
+  const defaultIconStyles =
+    'inline-flex h-6 w-6 justify-center rounded-lg items-center text-sm font-medium focus:outline-none focus:ring-1 focus:ring-secondary-100 dark:focus:ring-secondary-500 focus:border-0 dark:focus:border-0 stroke-neutral-800 dark:stroke-neutral-800-dark fill-neutral-800 dark:fill-neutral-800-dark' +
     ' disabled:stroke-neutral-500 dark:disabled:stroke-neutral-500-dark disabled:fill-neutral-500 dark:disabled:fill-neutral-500-dark disabled:cursor-not-allowed' +
     ' ' +
     variantStyles[variant];
+
+  const activeIcon = renderAsActive
+    ? 'fill-primary-500 dark:fill-primary-400 stroke-primary-500 dark:stroke-primary-400'
+    : '';
+  const toolboxIcon = isToolboxIcon
+    ? 'mx-1 my-2 first-of-type:ml-2 last-of-type:mr-2 h-8 w-8 p-1 border border-neutral-500 hover:bg-neutral-200 dark:hover:bg-neutral-800 dark:hover:stroke-primary-400 active:stroke-primary-400 active:fill-primary-400'
+    : '';
+  const additionalClasses = props.className ? props.className : '';
+
   return (
     <button
       type="button"
       {...props}
-      className={className + ' ' + (props.className ? props.className : '')}
+      className={activeIcon + ' ' + toolboxIcon + ' ' + defaultIconStyles + ' ' + additionalClasses}
     ></button>
   );
 }
