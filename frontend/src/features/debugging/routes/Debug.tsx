b27c54c39214214fc9488282b8b74dcd941badb4
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-11-10 15:40:58 +0100

    Document and refactor methods for spawning grouped toasts.

--- frontend/src/features/debugging/routes/Debug.tsx
+++ frontend/src/features/debugging/routes/Debug.tsx
@@ -1,16 +1,21 @@
 import SimpleButton from '@/components/Button/SimpleButton';
 import { WebdavTest } from '@/features/nextcloud_integration/components/webdav-test';
-import { error, info, success, warning } from '@/features/toasts/groupedToast';
+import {
+  errorToastGrouped,
+  infoToastGrouped,
+  successToastGrouped,
+  warningToastGrouped,
+} from '@/features/toasts/groupedToast';
 
 export function Debug() {
   return (
     <div className="grid grid-cols-3 gap-4 p-4">
       <div className="flex flex-col">
         <h2 className="mb-2 text-center">Create grouped Toasts</h2>
-        <SimpleButton onClick={() => error('Error')}>Error</SimpleButton>
-        <SimpleButton onClick={() => info('Info')}>Info</SimpleButton>
-        <SimpleButton onClick={() => warning('Warning')}>Warning</SimpleButton>
-        <SimpleButton onClick={() => success('Success')}>Success</SimpleButton>
+        <SimpleButton onClick={() => errorToastGrouped('Error')}>Error</SimpleButton>
+        <SimpleButton onClick={() => infoToastGrouped('Info')}>Info</SimpleButton>
+        <SimpleButton onClick={() => warningToastGrouped('Warning')}>Warning</SimpleButton>
+        <SimpleButton onClick={() => successToastGrouped('Success')}>Success</SimpleButton>
       </div>
       <div>
         <h2 className="mb-2 text-center">WebDAV Test</h2>
--- frontend/src/features/toasts/groupedToast.tsx
+++ frontend/src/features/toasts/groupedToast.tsx
@@ -6,28 +6,64 @@ const successToastData = new Set<ToastContent>();
 const warningToastData = new Set<ToastContent>();
 const errorToastData = new Set<ToastContent>();
 
-export function info(content: ToastContent, options?: ToastOptions<object> | undefined) {
+/**
+ * Allows only one info toast with the same content to appear on screen.
+ *
+ * @param content Toast content (see documentation of react-toastify)
+ * @param options Toast options (see documentation of react-toastify)
+ */
+export function infoToastGrouped(
+  content: ToastContent,
+  options?: ToastOptions<object> | undefined,
+) {
   if (!infoToastData.has(content)) {
     infoToastData.add(content);
     toast.info(content, options);
   }
 }
 
-export function success(content: ToastContent, options?: ToastOptions<object> | undefined) {
+/**
+ * Allows only one success toast with the same content to appear on screen.
+ *
+ * @param content Toast content (see documentation of react-toastify)
+ * @param options Toast options (see documentation of react-toastify)
+ */
+export function successToastGrouped(
+  content: ToastContent,
+  options?: ToastOptions<object> | undefined,
+) {
   if (!successToastData.has(content)) {
     successToastData.add(content);
     toast.success(content, options);
   }
 }
 
-export function warning(content: ToastContent, options?: ToastOptions<object> | undefined) {
+/**
+ * Allows only one warning toast with the same content to appear on screen.
+ *
+ * @param content Toast content (see documentation of react-toastify)
+ * @param options Toast options (see documentation of react-toastify)
+ */
+export function warningToastGrouped(
+  content: ToastContent,
+  options?: ToastOptions<object> | undefined,
+) {
   if (!warningToastData.has(content)) {
     warningToastData.add(content);
     toast.warning(content, options);
   }
 }
 
-export function error(content: ToastContent, options?: ToastOptions<object> | undefined) {
+/**
+ * Allows only one error toast with the same content to appear on screen.
+ *
+ * @param content Toast content (see documentation of react-toastify)
+ * @param options Toast options (see documentation of react-toastify)
+ */
+export function errorToastGrouped(
+  content: ToastContent,
+  options?: ToastOptions<object> | undefined,
+) {
   if (!errorToastData.has(content)) {
     errorToastData.add(content);
     toast.error(content, options);
