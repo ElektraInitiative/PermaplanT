8466a9653850e3611a7c46529dd314205d826c02
Samuel <absurd.turtl3@gmail.com>
2023-11-13 11:55:26 +0100

    fixed imports and link layout

--- frontend/src/components/Layout/FullPageNav.tsx
+++ frontend/src/components/Layout/FullPageNav.tsx
@@ -1,9 +1,9 @@
-import { ReactComponent as LogoSmallGraySVG } from '../../assets/permaplant-logo-gray.svg';
-import { ReactComponent as LogoSmallSVG } from '../../assets/permaplant-logo.svg';
 import { DarkModeSwitcher } from '../../features/dark_mode';
 import { useDarkModeStore } from '../../features/dark_mode';
 import LanguageSwitcher from '../../features/landing_page/components/LanguageSwitcher';
-import { ReactComponent as CloseIcon } from '../../icons/close.svg';
+import { ReactComponent as CloseIcon } from '../../svg/icons/close.svg';
+import { ReactComponent as LogoSmallGraySVG } from '../../svg/permaplant-logo-gray.svg';
+import { ReactComponent as LogoSmallSVG } from '../../svg/permaplant-logo.svg';
 import ButtonLink from '../Button/ButtonLink';
 import IconButton from '../Button/IconButton';
 import { LoginButton } from '@/features/auth/components/LoginButton';
@@ -24,14 +24,24 @@ const Navpage = ({ close }: NavpageProps) => {
   const auth = useSafeAuth();
   const backendVersion = sessionStorage.getItem('backend_version');
   const navbarItems = (
-    <div className="flex items-center">
-      <ul className="pt-1">
-        <li>
-          <ButtonLink title={t('routes:maps')} to="/maps" className="pe-4 text-lg font-medium" />
-          <ButtonLink title={t('routes:seeds')} to="/seeds" className="text-lg font-medium" />
-        </li>
-      </ul>
-    </div>
+    <ul className="flex flex-col items-center gap-4 pt-1">
+      <li className="text-center">
+        <ButtonLink
+          onClick={close}
+          title={t('routes:maps')}
+          to="/maps"
+          className="text-lg font-medium"
+        />
+      </li>
+      <li>
+        <ButtonLink
+          onClick={close}
+          title={t('routes:seeds')}
+          to="/seeds"
+          className="text-lg font-medium"
+        />
+      </li>
+    </ul>
   );
 
   return (
@@ -43,14 +53,14 @@ const Navpage = ({ close }: NavpageProps) => {
       top-0
       z-[1001]
       h-full
-      w-full
+      w-screen
       items-center
       bg-white
       dark:bg-neutral-100-dark
       "
     >
       <div className="flex flex-col gap-4 pl-4 pr-4">
-        <div className="flex justify-between h-16 items-center">
+        <div className="flex h-16 items-center justify-between">
           <a href="/" className="flex items-center">
             {darkMode ? (
               <LogoSmallSVG className="h-12 w-12 pr-2" />
@@ -72,6 +82,7 @@ const Navpage = ({ close }: NavpageProps) => {
           </IconButton>
         </div>
         <ul>{auth.isAuthenticated && navbarItems}</ul>
+        <div className="h-[3px] w-full bg-neutral-700" />
         <div className="flex items-center justify-center gap-4">
           <DarkModeSwitcher />
           <LanguageSwitcher />
