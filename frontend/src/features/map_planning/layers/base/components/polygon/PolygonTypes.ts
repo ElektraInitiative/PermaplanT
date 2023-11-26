70ce1a0d45a49851472d686ea16d66a871b1fa9d
Moritz Schalk <schalk.moritz@tutanota.de>
2023-11-26 15:13:14 +0100

    Define additional type for a point on a polygon.

--- frontend/src/features/map_planning/layers/base/components/polygon/PolygonTypes.ts
+++ frontend/src/features/map_planning/layers/base/components/polygon/PolygonTypes.ts
@@ -17,6 +17,24 @@ export type PolygonGeometry = {
  * Polygons can have multiple edge rings (i.e. multiple independent areas).
  * Each edge ring is assumed to be wound counterclockwise with lines connecting two neighboring points.
  */
-export type EdgeRing = Array<{ x: number; y: number; srid: number }>;
+export type EdgeRing = Array<PolygonPoint>;
+
+/**
+ * Point that forms an edge ring.
+ */
+export type PolygonPoint = {
+  /**
+   * Position in the horizontal axis.
+   */
+  x: number;
+  /**
+   * Position in the vertical axis.
+   */
+  y: number;
+  /**
+   * ID of the coordinate type used by the backend.
+   */
+  srid: number;
+};
 
 export const DEFAULT_SRID = 4326;
