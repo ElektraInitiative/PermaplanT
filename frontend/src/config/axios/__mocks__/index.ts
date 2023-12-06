310020605df966283db28925e4f5242db27f1f59
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-12-06 10:08:12 +0100

    Rewrite axios mock for vitest.

--- frontend/src/config/axios/__mocks__/index.ts
+++ frontend/src/config/axios/__mocks__/index.ts
@@ -4,15 +4,19 @@ import axios from 'axios';
  * Mock axios.create() to return a mock axios instance.
  * This is useful for writing tests that use axios.
  */
-vitest.mock('axios', () => ({
-  create: vi.fn(() => ({
-    get: vi.fn(() => Promise.resolve({ data: {} })),
-    post: vi.fn(() => Promise.resolve({ data: {} })),
-    put: vi.fn(() => Promise.resolve({ data: {} })),
-    patch: vi.fn(() => Promise.resolve({ data: {} })),
-    delete: vi.fn(() => Promise.resolve({ data: {} })),
-  })),
-}));
+vi.mock('axios', async () => {
+  const actual = await vi.importActual<typeof import('axios')>('axios');
+  return {
+    ...actual,
+    create: vi.fn(() => ({
+      get: vi.fn(() => Promise.resolve({ data: {} })),
+      post: vi.fn(() => Promise.resolve({ data: {} })),
+      put: vi.fn(() => Promise.resolve({ data: {} })),
+      patch: vi.fn(() => Promise.resolve({ data: {} })),
+      delete: vi.fn(() => Promise.resolve({ data: {} })),
+    })),
+  };
+});
 
 export function createAPI() {
   return axios.create();
