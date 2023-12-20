e9cc4020c74da900b9cfbd13fa4b872bbcbbd9b5
Moritz Schalk <e52005233@student.tuwien.ac.at>
2023-12-20 17:15:22 +0100

    Add shading right toolbar.

--- frontend/src/features/map_planning/layers/shade/components/ShadeLayerRightToolbar.tsx
+++ frontend/src/features/map_planning/layers/shade/components/ShadeLayerRightToolbar.tsx
@@ -1,3 +1,27 @@
+import SimpleButton from '@/components/Button/SimpleButton';
+import SelectMenu from '@/components/Form/SelectMenu';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+
 export function ShadeLayerRightToolbar() {
-  return <div></div>;
+  const { t } = useTranslation('shadeLayer');
+  const { control } = useForm();
+
+  return (
+    <div className="flex h-full flex-col gap-2 p-2">
+      <SelectMenu
+        id="shade-amount"
+        labelText={t('left_toolbar.shading_amount_select_title')}
+        className="w-64"
+        control={control}
+        options={[
+          { value: '', label: t('shading_amount.light_shade') },
+          { value: '', label: t('shading_amount.partial_shade') },
+          { value: '', label: t('shading_amount.permanent_shade') },
+          { value: '', label: t('shading_amount.permanent_deep_shade') },
+        ]}
+      />
+      <SimpleButton className="mb-32">{t('right_toolbar.add_shading')}</SimpleButton>
+    </div>
+  );
 }
