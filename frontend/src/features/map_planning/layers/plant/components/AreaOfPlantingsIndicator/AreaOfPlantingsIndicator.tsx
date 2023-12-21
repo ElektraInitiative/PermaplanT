08b396d0672416a4c8da64e69a871495f33fb18e
Bushuo <me@paulbuschmann.com>
2023-12-22 00:08:01 +0100

    feat: add adaptive unit and length formatting

--- frontend/src/features/map_planning/layers/plant/components/AreaOfPlantingsIndicator/AreaOfPlantingsIndicator.tsx
+++ frontend/src/features/map_planning/layers/plant/components/AreaOfPlantingsIndicator/AreaOfPlantingsIndicator.tsx
@@ -4,6 +4,7 @@ import style from './AreaOfPlantingsIndicator.module.css';
 import useMapStore from '@/features/map_planning/store/MapStore';
 import { colors } from '@/utils/colors';
 import { useEffect, useLayoutEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Html } from 'react-konva-utils';
 
 export function AreaOfPlantingsIndicator() {
@@ -15,9 +16,11 @@ export function AreaOfPlantingsIndicator() {
   const isVisible = selectedPlant !== null && selectionRectAttributes.isVisible;
 
   useEffect(() => {
-    if (!isVisible || !stage) return;
+    if (!stage) return;
 
     stage.on('mousemove.areaOfPlantingsIndicator', () => {
+      if (!isVisible) return;
+
       const pos = stage.getRelativePointerPosition();
       if (pos === null) return;
 
@@ -30,10 +33,11 @@ export function AreaOfPlantingsIndicator() {
   }, [isVisible, stage]);
 
   useLayoutEffect(() => {
+    // before showing the tooltip, place it where the selection rect is
     if (!isVisible) return;
 
     setPos({ x: selectionRectAttributes.x, y: selectionRectAttributes.y });
-    // we only want to run this if isVisible changes, then we are only interested in the latest value of selectionRectAttributes
+    // we only want to run this if isVisible changes
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isVisible]);
 
@@ -59,7 +63,7 @@ export function AreaOfPlantingsIndicator() {
         style: { backgroundColor: colors.secondary[200] },
       }}
     >
-      <InfoTextLabel
+      <PlacementInfoTooltip
         fieldWidth={selectionRectAttributes.width}
         fieldHeight={selectionRectAttributes.height}
         plantWidth={plantWidth}
@@ -68,7 +72,7 @@ export function AreaOfPlantingsIndicator() {
   );
 }
 
-function InfoTextLabel({
+function PlacementInfoTooltip({
   fieldWidth,
   fieldHeight,
   plantWidth,
@@ -77,22 +81,30 @@ function InfoTextLabel({
   fieldHeight: number;
   plantWidth: number;
 }) {
+  const { t } = useTranslation(['common', 'layers']);
   const { horizontalPlantCount, verticalPlantCount } = calculatePlantCount(
     plantWidth,
     fieldWidth,
     fieldHeight,
   );
 
+  const unitIsMeters = Math.min(fieldWidth, fieldHeight) > 100;
+  const unit = unitIsMeters ? t('meter_shorthand') : t('centimeter_shorthand');
+
   return (
     <>
-      <span>{fieldWidth.toFixed(0)}</span>
-      <span className="text-center">x</span>
-      <span className="text-right">{fieldHeight.toFixed(0)}</span>
-      <span className="font-bold">cm</span>
-      <span>{horizontalPlantCount}</span>
-      <span className="text-center">x</span>
-      <span className="text-right">{verticalPlantCount}</span>
-      <span className="font-bold">plants</span>
+      <span className="font-mono">{formatLength(fieldWidth, unitIsMeters)}</span>
+      <span className="text-center font-mono">&times;</span>
+      <span className="text-right font-mono">{formatLength(fieldHeight, unitIsMeters)}</span>
+      <span className="font-bold">{unit}</span>
+      <span className="font-mono">{horizontalPlantCount}</span>
+      <span className="text-center font-mono">&times;</span>
+      <span className="text-right font-mono">{verticalPlantCount}</span>
+      <span className="font-bold">{t('layers:plants')}</span>
     </>
   );
+}
+
+function formatLength(length: number, unitIsMeters: boolean) {
+  return unitIsMeters ? (length / 100).toFixed(2) : length.toFixed(0);
 }
