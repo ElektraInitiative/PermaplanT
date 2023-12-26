517b7855511965c35264b0a12b92c659fd042a0a
Moritz Schalk <schalk.moritz@tutanota.de>
2023-12-26 12:31:50 +0100

    Add buttons for shading selection.

--- frontend/src/features/map_planning/layers/shade/components/ShadeLayerRightToolbar.tsx
+++ frontend/src/features/map_planning/layers/shade/components/ShadeLayerRightToolbar.tsx
@@ -1,27 +1,52 @@
+import { Shade } from '@/api_types/definitions';
 import SimpleButton from '@/components/Button/SimpleButton';
-import SelectMenu from '@/components/Form/SelectMenu';
-import { useForm } from 'react-hook-form';
+import { StatusPanelContentWrapper } from '@/features/map_planning/components/statuspanel/StatusPanelContentWrapper';
+import useMapStore from '@/features/map_planning/store/MapStore';
 import { useTranslation } from 'react-i18next';
 
 export function ShadeLayerRightToolbar() {
   const { t } = useTranslation('shadeLayer');
-  const { control } = useForm();
+  const selectShadeForNewShading = useMapStore((state) => state.shadeLayerSelectShadeForNewShading);
+  const selectedShadeForNewShading = useMapStore(
+    (state) => state.untrackedState.layers.shade.selectedShadeForNewShading,
+  );
+  const setStatusPanelContent = useMapStore((state) => state.setStatusPanelContent);
+  const clearStatusPanelContent = useMapStore((state) => state.clearStatusPanelContent);
+
+  const removeShadeForShading = () => {
+    selectShadeForNewShading(null);
+    clearStatusPanelContent();
+  };
+
+  const toggleShadeForShading = (shade: Shade) => {
+    if (selectedShadeForNewShading === shade) {
+      removeShadeForShading();
+      return;
+    }
+
+    selectShadeForNewShading(shade);
+    setStatusPanelContent(
+      <StatusPanelContentWrapper
+        onClose={removeShadeForShading}
+        content={t('place_shadings_message')}
+      />,
+    );
+  };
 
   return (
     <div className="flex h-full flex-col gap-2 p-2">
-      <SelectMenu
-        id="shade-amount"
-        labelText={t('left_toolbar.shading_amount_select_title')}
-        className="w-64"
-        control={control}
-        options={[
-          { value: '', label: t('shading_amount.light_shade') },
-          { value: '', label: t('shading_amount.partial_shade') },
-          { value: '', label: t('shading_amount.permanent_shade') },
-          { value: '', label: t('shading_amount.permanent_deep_shade') },
-        ]}
-      />
-      <SimpleButton className="mb-32">{t('right_toolbar.add_shading')}</SimpleButton>
+      <SimpleButton onClick={() => toggleShadeForShading(Shade.LightShade)}>
+        {t('shading_amount.light_shade')}
+      </SimpleButton>
+      <SimpleButton onClick={() => toggleShadeForShading(Shade.PartialShade)}>
+        {t('shading_amount.partial_shade')}
+      </SimpleButton>
+      <SimpleButton onClick={() => toggleShadeForShading(Shade.PermanentDeepShade)}>
+        {t('shading_amount.permanent_shade')}
+      </SimpleButton>
+      <SimpleButton onClick={() => toggleShadeForShading(Shade.PermanentDeepShade)}>
+        {t('shading_amount.permanent_deep_shade')}
+      </SimpleButton>
     </div>
   );
 }
