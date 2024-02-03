67bd33b468108b17b64e39dd951c0fc9260b07f5
Bushuo <me@paulbuschmann.com>
2024-02-03 14:09:49 +0100

    fix: tests, drop default on add_date

--- backend/migrations/2024-01-24-184709_plantings_auditability/up.sql
+++ backend/migrations/2024-01-24-184709_plantings_auditability/up.sql
@@ -42,14 +42,12 @@ INNER JOIN maps ON layers.map_id = maps.id
 WHERE plantings.layer_id = layers.id;
 
 ALTER TABLE plantings
-ALTER COLUMN add_date SET DEFAULT now(),
 ALTER COLUMN created_at SET DEFAULT now(),
 ALTER COLUMN created_at SET NOT NULL,
 ALTER COLUMN modified_at SET DEFAULT now(),
 ALTER COLUMN modified_at SET NOT NULL,
 ALTER COLUMN created_by SET NOT NULL,
 ALTER COLUMN modified_by SET NOT NULL,
-ALTER COLUMN remove_date SET DEFAULT null,
 ALTER COLUMN notes SET DEFAULT '',
 ALTER COLUMN notes SET NOT NULL;
 
