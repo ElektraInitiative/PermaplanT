777afc97f4e34a0c4e93910a7c23bc30a5e29cb1
Jannis Adamek <jannis.adamek@gmail.com>
2024-04-16 21:27:16 +0200

    Fix migrations.

--- backend/migrations/2024-04-01-080848_alter_drawing_fill_pattern/down.sql
+++ backend/migrations/2024-04-01-080848_alter_drawing_fill_pattern/down.sql
@@ -1,3 +1,6 @@
-ALTER TABLE drawings ADD COLUMN fill_enabled boolean NOT NULL;
-UPDATE drawings SET fill_enabled = NOT coalesce(fill_pattern = 'none', false);
+ALTER TABLE drawings ADD COLUMN fill_enabled boolean;
+
+UPDATE drawings SET fill_enabled = (fill_pattern != 'none');
 ALTER TABLE drawings DROP COLUMN fill_pattern;
+
+ALTER TABLE drawings ALTER COLUMN fill_enabled SET NOT NULL;
--- backend/migrations/2024-04-01-191448_add_image_and_text/down.sql
+++ backend/migrations/2024-04-01-191448_add_image_and_text/down.sql
@@ -1,7 +1,28 @@
-ALTER TYPE drawing_shape_type DROP VALUE 'label text';
-ALTER TYPE drawing_shape_type DROP VALUE 'image';
+ALTER TYPE drawing_shape_type RENAME TO drawing_shape_type_old;
+
+CREATE TYPE drawing_shape_type AS ENUM (
+    'rectangle',
+    'ellipse',
+    'free line',
+    'bezier polygon'
+);
+
+ALTER TABLE drawings ALTER COLUMN shape_type TYPE drawing_shape_type USING shape_type::text::drawing_shape_type;
+
+DROP TYPE drawing_shape_type_old;
 
 ALTER TABLE drawings
-ADD COLUMN color varchar NOT NULL,
-ADD COLUMN fill_pattern boolean NOT NULL,
-ADD COLUMN stroke_width real NOT NULL;
+ADD COLUMN color varchar,
+ADD COLUMN fill_pattern varchar,
+ADD COLUMN stroke_width real;
+
+UPDATE drawings
+SET
+    color = '',
+    fill_pattern = false,
+    stroke_width = 1.0;
+
+
+ALTER TABLE drawings ALTER COLUMN color SET NOT NULL;
+ALTER TABLE drawings ALTER COLUMN fill_pattern SET NOT NULL;
+ALTER TABLE drawings ALTER COLUMN stroke_width SET NOT NULL;
